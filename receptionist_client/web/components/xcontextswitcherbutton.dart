12857afb3180eceea514e1e24657b5788d189d75
Thomas Løcke <tl@adaheads.com>
2013-04-12 15:33:26 +0200

    Finished contextswitcher and contextswitcherbutton components.

--- receptionist_client/web/components/xcontextswitcherbutton.dart
+++ receptionist_client/web/components/xcontextswitcherbutton.dart
@@ -7,36 +7,28 @@ import '../classes/context.dart';
 @observable
 class ContextSwitcherButton extends WebComponent {
   Context context;
-  String alertIcon = '../icons/contextalert.svg'; // ???? path here? yuk!
 
-  DivElement _alertDiv;
   ImageElement _alertImg;
   ButtonElement _button;
-  ImageElement _img;
-  String _iconActive;
-  String _iconPassive;
+  ImageElement _iconActive;
 
   String get alertMode => context.alertMode ? '' : 'hidden';
   bool get disabled => context.isActive;
-  String get icon => context.isActive ? _iconActive : _iconPassive;
+  String get hidden => context.isActive ? '' : 'hidden';
 
   void inserted() {
     _button = this.query('button');
-    _img = _button.query('img');
-    _alertDiv = _button.query('div');
-    _alertImg = _alertDiv.query('img');
-
-    _iconActive = '../icons/${context.id}_active.svg'; // ???? path here? yuk!
-    _iconPassive = '../icons/${context.id}.svg';       // ???? path here? yuk!
+    _iconActive = _button.query('[name="buttonactiveimage"]');
+    _alertImg = _button.query('[name="buttonalertimage"]');
 
     /*
      * We take advantage of the fact that disabled buttons ignore mouse-over/out
-     * events, so it's perfectly fine to just blindly set the src attribute
-     * of the button image element, as the currently active button (which is then
-     * disabled) does not emit any events.
+     * events, so it's perfectly fine to just toggle the hidden class on the
+     * iconActive element, as we can never remove nor add the hidden class on
+     * the currently active button, because it has also been disabled.
      */
-    _button.onMouseOver.listen((_) => _img.src = _iconActive);
-    _button.onMouseOut.listen((_) =>  _img.src = _iconPassive);
+    _button.onMouseOver.listen((_) => _iconActive.classes.remove('hidden'));
+    _button.onMouseOut.listen((_) => _iconActive.classes.add('hidden'));
 
     _resize();
     window.onResize.listen((_) => _resize());
@@ -46,11 +38,10 @@ class ContextSwitcherButton extends WebComponent {
     num newSize = _button.client.width / 2;
     num margin = newSize / 1.5;
 
+    _button.style.height = '${_button.client.width}px';
     _button.style.marginTop = '${margin}px';
     _button.style.marginBottom = '${margin}px';
 
-    _alertDiv.style.width = '${newSize}px';
-    _alertDiv.style.height = '${newSize}px';
     _alertImg.style.height = '${newSize}px';
     _alertImg.style.width = '${newSize}px';
   }
