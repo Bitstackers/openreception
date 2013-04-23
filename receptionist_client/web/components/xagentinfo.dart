38ebfe783317a1e037b1d371e399fd564d0c169e
Thomas Løcke <tl@adaheads.com>
2013-04-23 15:28:22 +0200

    Initial implementation of the agent info box.

--- receptionist_client/web/components/xagentinfo.dart
+++ receptionist_client/web/components/xagentinfo.dart
@@ -25,13 +25,13 @@ class AgentInfo extends WebComponent {
     divParent = this.query('[name="boxcontent"]');
 
     divTotal = this.query('[name="total"]');
-    imgTotal = divTotal.query('img');
+//    imgTotal = divTotal.query('img');
 
     divActive = this.query('[name="active"]');
-    imgActive = divActive.query('img');
+//    imgActive = divActive.query('img');
 
     divSleep = this.query('[name="sleep"]');
-    imgSleep = divSleep.query('img');
+//    imgSleep = divSleep.query('img');
 
     divFace = this.query('[name="face"]');
     imgFace = divFace.query('img');
@@ -53,21 +53,21 @@ class AgentInfo extends WebComponent {
     num partSize = (divParent.client.width - divFace.client.width) / 3;
 
     divTotal.style.width = '${partSize}px';
-    imgTotal.style.height = '${0.7 * divTotal.client.height}px';
-    imgTotal.style.width = '${imgTotal.client.height}px';
-    imgTotal.style.margin = '${0.15 * divTotal.client.height}px ${0.1 * partSize}px';
+//    imgTotal.style.height = '${0.7 * divTotal.client.height}px';
+//    imgTotal.style.width = '${imgTotal.client.height}px';
+//    imgTotal.style.margin = '${0.15 * divTotal.client.height}px ${0.1 * partSize}px';
 
     divActive.style.left = '${partSize}px';
     divActive.style.width = '${partSize}px';
-    imgActive.style.height = '${0.7 * divActive.client.height}px';
-    imgActive.style.width = '${imgActive.client.height}px';
-    imgActive.style.margin = '${0.15 * divActive.client.height}px';
+//    imgActive.style.height = '${0.7 * divActive.client.height}px';
+//    imgActive.style.width = '${imgActive.client.height}px';
+//    imgActive.style.margin = '${0.15 * divActive.client.height}px';
 
     divSleep.style.left = '${partSize * 2}px';
     divSleep.style.width = '${partSize}px';
-    imgSleep.style.height = '${0.7 * divSleep.client.height}px';
-    imgSleep.style.width = '${imgSleep.client.height}px';
-    imgSleep.style.margin = '${0.15 * divSleep.client.height}px';
+//    imgSleep.style.height = '${0.7 * divSleep.client.height}px';
+//    imgSleep.style.width = '${imgSleep.client.height}px';
+//    imgSleep.style.margin = '${0.15 * divSleep.client.height}px';
   }
 
   void _initialSetup() {
