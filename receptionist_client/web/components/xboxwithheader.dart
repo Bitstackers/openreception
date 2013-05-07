8a536d776b9676e345568583cb4ef15794077d90
Thomas Løcke <tl@adaheads.com>
2013-05-07 13:55:39 +0200

    Removed the chrome option from xboxwithheader.
    It was clumsy and made little sense in the new design.

--- receptionist_client/web/components/xboxwithheader.dart
+++ receptionist_client/web/components/xboxwithheader.dart
@@ -4,14 +4,10 @@ import 'package:web_ui/web_ui.dart';
 
 class BoxWithHeader extends WebComponent {
   DivElement body;
-  bool chrome = true;
   HeadingElement header;
   String headerfontsize = '1.0em';
   String headerpadding = '5px 10px';
   DivElement outer;
-
-  String get headlineChrome => chrome ? _css['.headline_chrome'] : _css['.headline_nochrome'];
-  String get outerboxChrome => chrome ? '' : 'hideborder';
 
   void inserted() {
     _queryElements();
