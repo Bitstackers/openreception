2d836f62b41092843aae7142fab96e4f086220b6
Thomas Løcke <tl@adaheads.com>
2013-05-07 13:39:48 +0200

    Moved placing CSS into scoped styling for the individual components.

--- receptionist_client/web/components/xcompanyinfo.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:html';
-
-import 'package:web_ui/web_ui.dart';
-
-import '../classes/environment.dart' as environment;
-import '../classes/model.dart' as model;
-
-class CompanyInfo extends WebComponent {}
