41dd6608b0a01411b0ec68b4d777f4cc4dcebe87
Thomas Løcke <tl@adaheads.com>
2013-05-08 13:01:34 +0200

    Added very early and simple company Event list to environment

--- receptionist_client/web/components/xcompanyevents.dart
+++ receptionist_client/web/components/xcompanyevents.dart
@@ -2,4 +2,7 @@ import 'dart:html';
 
 import 'package:web_ui/web_ui.dart';
 
+import '../classes/environment.dart' as environment;
+import '../classes/model.dart' as model;
+
 class CompanyEvents extends WebComponent {}
