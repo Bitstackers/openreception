af14b99d4e9d14031e7630722f6d46f5a94106c8
Thomas Løcke <tl@adaheads.com>
2013-05-21 14:08:52 +0200

    Moved title into component class.

--- receptionist_client/web/components/xcompanyevents.dart
+++ receptionist_client/web/components/xcompanyevents.dart
@@ -5,4 +5,6 @@ import 'package:web_ui/web_ui.dart';
 import '../classes/environment.dart' as environment;
 import '../classes/model.dart' as model;
 
-class CompanyEvents extends WebComponent {}
+class CompanyEvents extends WebComponent {
+  String title = 'Kalender';
+}
