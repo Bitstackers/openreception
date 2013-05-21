6064bd43fb0c0445ea9a95d9dc49d172b7832751
Thomas Løcke <tl@adaheads.com>
2013-05-21 16:57:33 +0200

    Removed Map<String, String> killChrome field from Box class.

--- receptionist_client/web/components/xbox.dart
+++ receptionist_client/web/components/xbox.dart
@@ -3,8 +3,5 @@ import 'package:web_ui/web_ui.dart';
 
 class Box extends WebComponent {
   bool chrome = true;
-
-  Map<String, String> killChrome = {'border' : '0px', 'border-radius' : '0px'};
-
-  Map get noChrome => chrome ? {} : killChrome;
+  Map get noChrome => chrome ? {} : {'border' : '0px', 'border-radius' : '0px'};
 }
