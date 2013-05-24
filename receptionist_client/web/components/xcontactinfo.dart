24106d8d56fe6065465025809779098697803163
Thomas Løcke <tl@adaheads.com>
2013-05-24 12:40:19 +0200

    Initial HTML/CSS for the contact info widget.

--- receptionist_client/web/components/xcontactinfo.dart
+++ receptionist_client/web/components/xcontactinfo.dart
@@ -5,4 +5,26 @@ import 'package:web_ui/web_ui.dart';
 import '../classes/environment.dart' as environment;
 import '../classes/model.dart' as model;
 
-class ContactInfo extends WebComponent {}
+class ContactInfo extends WebComponent {
+  String calendarTitle = 'Kalender';
+  String placeholder = 'søg...';
+  String title = 'Medarbejdere';
+
+  void inserted() {
+    //_queryElements();
+    //_registerEventListeners();
+    //_resize();
+  }
+
+  void _queryElements() {
+
+  }
+
+  void _registerEventListeners() {
+    window.onResize.listen((_) => _resize());
+  }
+
+  void _resize() {
+    //this.query('[name="foo"]').style.height = '70%';
+  }
+}
