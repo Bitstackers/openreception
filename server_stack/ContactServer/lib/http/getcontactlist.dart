f3329cf4cf8b6059f24e1d770b1e4a53fca3c2f4
Thomas Pedersen <tp@adaheads.com>
2013-12-19 12:40:17 +0100

    Forgot error handler for contact list.

--- server_stack/ContactServer/lib/http/getcontactlist.dart
+++ server_stack/ContactServer/lib/http/getcontactlist.dart
@@ -5,5 +5,5 @@ void getOrgList(HttpRequest request) {
 
   db.getOrganizationList(orgId).then((Map value) {
     writeAndClose(request, JSON.encode(value));
-  });
+  }).catchError((error) => serverError(request, error.toString()));
 }
