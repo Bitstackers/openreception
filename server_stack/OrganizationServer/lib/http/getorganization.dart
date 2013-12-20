d1fb84987db2dc8922c6d4e78f888ce75caeed0f
Thomas Pedersen <tp@adaheads.com>
2013-12-20 17:13:41 +0100

    Made a faster cache reader.

--- server_stack/OrganizationServer/lib/http/getorganization.dart
+++ server_stack/OrganizationServer/lib/http/getorganization.dart
@@ -1,6 +1,6 @@
 part of http;
 
-void getOrg(HttpRequest request) {
+void getOrgSlow(HttpRequest request) {
   int id = int.parse(request.uri.pathSegments.elementAt(1));
   cache.loadOrganization(id).then((String org) {
     if(org != null) {
@@ -19,5 +19,28 @@ void getOrg(HttpRequest request) {
         }
       }).catchError((error) => serverError(request, error.toString()));
     }
+  });
+}
+
+void getOrg(HttpRequest request) {
+  int id = int.parse(request.uri.pathSegments.elementAt(1));
+  String path = '${config.cache}org/$id.json';
+
+  File file = new File(path);
+  file.openRead()
+    .handleError((error) {log('Cache miss: $path. $error');})
+    .pipe(request.response)
+    .catchError((_) {
+        db.getOrganization(id).then((Map value) {
+          String org = JSON.encode(value);
+    
+          if(value.isEmpty) {
+            request.response.statusCode = 404;
+            writeAndClose(request, org);
+          } else {
+            cache.saveOrganization(id, org)
+              .then((_) => writeAndClose(request, org));
+          }
+        }).catchError((error) => serverError(request, error.toString()));
   });
 }
