5c942fcd711cc8e529a5ff1c258aa5a5c2f73738
Thomas Pedersen <tp@adaheads.com>
2014-01-03 23:58:23 +0100

    Added authentication and calendar interface.

--- server_stack/OrganizationServer/lib/router.dart
+++ server_stack/OrganizationServer/lib/router.dart
@@ -9,28 +9,32 @@ import 'db.dart' as db;
 import '../../Shared/httpserver.dart';
 
 import 'package:route/server.dart';
+import 'package:route/pattern.dart';
 
 part 'router/getorganization.dart';
+part 'router/getcalendar.dart';
 part 'router/updateorganization.dart';
 part 'router/createorganization.dart';
 part 'router/deleteorganization.dart';
 part 'router/getorganizationlist.dart';
 part 'router/invalidateorganization.dart';
 
-final Pattern getOrganizationUrl        = new UrlPattern(r'/organization/(\d)*');
-final Pattern deleteOrganizationUrl     = new UrlPattern(r'/organization/(\d)*');
+final Pattern organizationUrl           = new UrlPattern(r'/organization/(\d)*');
 final Pattern createOrganizationUrl     = new UrlPattern('/organization');
-final Pattern updateOrganizationUrl     = new UrlPattern(r'/organization/(\d)*');
-final Pattern getOrganizationListUrl    = new UrlPattern('/organization/list');
+final Pattern organizationListUrl       = new UrlPattern('/organization/list');
 final Pattern invalidateOrganizationUrl = new UrlPattern(r'/organization/(\d)*/invalidate');
+final Pattern getOrganizationCalendarListUrl  = new UrlPattern(r'/organization/(\d)*/calendar');
+final List<Pattern> allUniqueUrls = [organizationUrl, organizationListUrl, invalidateOrganizationUrl, getOrganizationCalendarListUrl];
 
 void setup(HttpServer server) {
   Router router = new Router(server)
-    ..serve(getOrganizationUrl, method: 'GET').listen(getOrg)
-    ..serve(deleteOrganizationUrl, method: 'DELETE').listen(deleteOrg)
+    ..filter(matchAny(allUniqueUrls), authFilter)
+    ..serve(organizationUrl, method: 'GET').listen(getOrg)
+    ..serve(organizationUrl, method: 'DELETE').listen(deleteOrg)
     ..serve(createOrganizationUrl, method: 'POST').listen(createOrg)
-    ..serve(updateOrganizationUrl, method: 'PUT').listen(updateOrg)
-    ..serve(getOrganizationListUrl, method: 'GET').listen(getOrgList)
+    ..serve(organizationUrl, method: 'PUT').listen(updateOrg)
+    ..serve(organizationListUrl, method: 'GET').listen(getOrgList)
     ..serve(invalidateOrganizationUrl, method: 'POST').listen(invalidateOrg)
+    ..serve(getOrganizationCalendarListUrl, method: 'GET').listen(getOrganizationCalendar)
     ..defaultStream.listen(page404);
 }
