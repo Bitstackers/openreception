f26608d19fcb205b8f4d75df02f31d778d9d25c6
Thomas Pedersen <tp@adaheads.com>
2014-01-08 16:59:57 +0100

    Update error message, to tell which function it came from.

--- server_stack/ContactServer/lib/router/getcontactlist.dart
+++ server_stack/ContactServer/lib/router/getcontactlist.dart
@@ -5,5 +5,5 @@ void getOrgList(HttpRequest request) {
   
   db.getOrganizationContactList(orgId).then((Map value) {
     writeAndClose(request, JSON.encode(value));
-  }).catchError((error) => serverError(request, error.toString()));
+  }).catchError((error) => serverError(request, 'getOrgList. db.getOrganizationContactList returned error: $error'));
 }
