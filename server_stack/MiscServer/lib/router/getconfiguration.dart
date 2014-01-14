c69bb89f6b31040e39558c05427dc4e2dc4f54c6
Thomas Pedersen <tp@adaheads.com>
2014-01-14 11:25:56 +0100

    If the bobconfig was not found, the request would hang.

--- server_stack/MiscServer/lib/router/getconfiguration.dart
+++ server_stack/MiscServer/lib/router/getconfiguration.dart
@@ -3,10 +3,10 @@ part of miscserver.router;
 void getBobConfig(HttpRequest request) {
   String path = config.bobConfigfile;
   File file = new File(path);
-
+  
   file.readAsString().then((String text) {
     writeAndClose(request, text);
   }).catchError((error) {
-    log(error.toString());
+    serverError(request, error.toString());
   });
 }
