d5e61141cb40b6fba805fbfee5bb594d5726916a
Thomas Pedersen <tp@adaheads.com>
2014-02-14 14:07:26 +0100

    Wrap it in try-catch.

--- server_stack/AuthServer/lib/router/oauthcallback.dart
+++ server_stack/AuthServer/lib/router/oauthcallback.dart
@@ -1,52 +1,56 @@
 part of authenticationserver.router;
 
 void oauthCallback(HttpRequest request) {
-  Uri returnUrl = Uri.parse(queryParameter(request.uri, 'state'));
-  Map postBody = 
-    {
-      "grant_type": "authorization_code",
-      "code": request.uri.queryParameters['code'],
-      "redirect_uri": config.redirectUri.toString(),
-      "client_id": config.clientId,
-      "client_secret": config.clientSecret
-    };
-  
-  String body = mapToUrlFormEncodedPostBody(postBody);
-  http.post(tokenEndpoint, headers: {'content-type':'application/x-www-form-urlencoded'}, body: body).then((http.Response response) {
-    Map json = JSON.decode(response.body);
+  try {
+    Uri returnUrl = Uri.parse(queryParameter(request.uri, 'state'));
+    Map postBody = 
+      {
+        "grant_type": "authorization_code",
+        "code": request.uri.queryParameters['code'],
+        "redirect_uri": config.redirectUri.toString(),
+        "client_id": config.clientId,
+        "client_secret": config.clientSecret
+      };
     
-    if(json.containsKey('error')) {
-      serverError(request, 'Authtication failed. ${json}');
+    String body = mapToUrlFormEncodedPostBody(postBody);
+    http.post(tokenEndpoint, headers: {'content-type':'application/x-www-form-urlencoded'}, body: body).then((http.Response response) {
+      Map json = JSON.decode(response.body);
       
-    } else {
-      //TODO How long should a token be valid for? configuration?
-      json['expiresAt'] = dateTimeToJson(new DateTime.now().add(new Duration(hours: 1)));
-      return getUserInfo(json['access_token']).then((Map userData) {
-        if(userData == null || userData.isEmpty) {
+      if(json.containsKey('error')) {
+        serverError(request, 'Authtication failed. ${json}');
+        
+      } else {
+        //TODO How long should a token be valid for? configuration?
+        json['expiresAt'] = dateTimeToJson(new DateTime.now().add(new Duration(hours: 1)));
+        return getUserInfo(json['access_token']).then((Map userData) {
+          if(userData == null || userData.isEmpty) {
+            request.response.statusCode = 403;
+            writeAndClose(request, JSON.encode({'status': 'Forbidden!'}));
+            
+          } else {
+            json['identity'] = userData;
+  
+            String cacheObject = JSON.encode(json);
+            String hash = Sha256Token(cacheObject);
+            
+            return cache.saveToken(hash, cacheObject).then((_) {
+              Map queryParameters = {'settoken' : hash};
+              request.response.redirect(new Uri(scheme: returnUrl.scheme, userInfo: returnUrl.userInfo, host: returnUrl.host, port: returnUrl.port, path: returnUrl.path, queryParameters: queryParameters));
+            }).catchError((error) {
+              serverError(request, error.toString());
+            });
+          }
+        }).catchError((error) {
+          log(error);
           request.response.statusCode = 403;
-          writeAndClose(request, JSON.encode({'status': 'Forbidden!'}));
-          
-        } else {
-          json['identity'] = userData;
-
-          String cacheObject = JSON.encode(json);
-          String hash = Sha256Token(cacheObject);
-          
-          return cache.saveToken(hash, cacheObject).then((_) {
-            Map queryParameters = {'settoken' : hash};
-            request.response.redirect(new Uri(scheme: returnUrl.scheme, userInfo: returnUrl.userInfo, host: returnUrl.host, port: returnUrl.port, path: returnUrl.path, queryParameters: queryParameters));
-          }).catchError((error) {
-            serverError(request, error.toString());
-          });
-        }
-      }).catchError((error) {
-        log(error);
-        request.response.statusCode = 403;
-        writeAndClose(request, JSON.encode({'status': 'Forbidden.'}));
-      });
-    }
-    
-  }).catchError((error) => serverError(request, error.toString()));
+          writeAndClose(request, JSON.encode({'status': 'Forbidden.'}));
+        });
+      }
+      
+    }).catchError((error) => serverError(request, error.toString()));
+  } catch(e) {
+    serverError(request, 'authenticationserver.router.oauthCallback() error ${e} Url ${request.uri}');
+  }
 }
 
 
