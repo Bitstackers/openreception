f7cbbb29c8f794b6484a34f4624cc1da54816863
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-04-30 15:08:54 +0200

    Now also starting the MessageDispatcher. Fixed a minor issue with Iterrupt handler: The parent process did not catch the SIGINT or SIGTERM - and thus, did not terminate on ^C.

--- server_stack/spawner.dart
+++ server_stack/spawner.dart
@@ -9,9 +9,26 @@ void main() {
                              'ContactServer'      : {'path' : 'ContactServer/bin/contactserver.dart'},
                              'LogServer'          : {'path' : 'LogServer/bin/logserver.dart'},
                              'MessageServer'      : {'path' : 'MessageServer/bin/messageserver.dart'},
+                             'MessageDispatcher'  : {'path' : 'MessageDispatcher/bin/messagedispacher.dart'},
                              'NotificationServer' : {'path' : 'NotificationServer/bin/notificationserver.dart'},
                              'ReceptionServer'    : {'path' : 'ReceptionServer/bin/receptionserver.dart'}};
-
+ 
+  ProcessSignal.SIGINT.watch().listen((_) {
+    Servers.forEach((String serverName, Map server) {
+      print ('Sending SIGINT to instance of ${serverName}');
+      (server['process'] as Process).kill(ProcessSignal.SIGINT);
+    });
+    exit(0);
+  });
+    
+    ProcessSignal.SIGTERM.watch().listen((_) {
+      Servers.forEach((String serverName, Map server) {
+        print ('Sending SIGTERM to instance of ${serverName}');
+        (server['process'] as Process).kill(ProcessSignal.SIGINT);
+      });
+      exit(0);
+    });
+    
   Servers.forEach((String serverName, Map server) {
     print ('Starting ${serverName}..');
     Process.start('dart', [server['path']]).then((process) {
@@ -34,15 +51,4 @@ void main() {
     });
   });
   
-  ProcessSignal.SIGINT.watch().listen((_) {
-    Servers.forEach((String serverName, Map server) {
-      (server['process'] as Process).kill(ProcessSignal.SIGINT);
-    });
-  });
-    
-    ProcessSignal.SIGTERM.watch().listen((_) {
-      Servers.forEach((String serverName, Map server) {
-        (server['process'] as Process).kill(ProcessSignal.SIGINT);
-      });
-    });
 }
