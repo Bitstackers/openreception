7fdc284eff1c9fe30b20c1ffc20b0bc645bc6a61
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-05-06 21:20:25 +0200

    Moved to the view folder.

---
+++ receptionist_client/web/view/view.dart
@@ -0,0 +1,13 @@
+library view;
+
+import 'dart:async';
+import 'dart:html';
+import '../classes/events.dart' as event;
+import '../model/model.dart' as model;
+import '../constants.dart' as constant;
+import '../service/service.dart' as Service;
+
+part 'call-management.dart';
+part 'view-notification.dart';
+
+const String libraryName = "view";
