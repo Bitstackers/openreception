1eddfa6b7694b1e4a070617e59dbbfc7195ab5cf
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-05-08 11:04:19 +0200

    Additional dependencies.

--- receptionist_client/web/classes/controller.dart
+++ receptionist_client/web/classes/controller.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 
 import '../protocol/protocol.dart' as protocol;
 import 'events.dart' as event;
-import '../model/model.dart';
+import '../service/service.dart' as Service;
+import '../model/model.dart' as model;
 
 part 'controller-call.dart';
