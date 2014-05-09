af2d6853da4661513dc684993e3a64cfa17b0eb9
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-05-09 10:37:39 +0200

    Moved the controller files to a seperate folder.

---
+++ receptionist_client/web/controller/controller.dart
@@ -0,0 +1,15 @@
+library controller;
+
+import 'dart:async';
+
+import '../protocol/protocol.dart' as protocol;
+import '../classes/events.dart' as event;
+import '../service/service.dart' as Service;
+import '../model/model.dart' as model;
+import '../classes/context.dart' as UIContext;
+import '../classes/location.dart' as nav;
+
+
+
+part '../classes/controller-call.dart';
+part '../classes/controller-context.dart';
