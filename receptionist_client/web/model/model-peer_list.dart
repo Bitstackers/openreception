b014e0402fae7484e683c89b40b50606097cc834
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-05-09 13:24:25 +0200

    Minor bugfix.

--- receptionist_client/web/model/model-peer_list.dart
+++ receptionist_client/web/model/model-peer_list.dart
@@ -19,7 +19,7 @@ class PeerList extends IterableBase<Peer> {
 
   /* A set would have been a better fit here, but it makes the code
    * read terribly. */
-  Map<String,Peer> _map = new Map<String,Peer>();
+  Map<String, Peer> _map = new Map<String, Peer>();
 
   /**
    * Iterator. This merely forwards the values from within the internal map.
@@ -28,7 +28,36 @@ class PeerList extends IterableBase<Peer> {
    */
   Iterator<Peer> get iterator => this._map.values.iterator;
 
-   void update (Peer peer) {
-     this._map[peer.ID].update(peer);
-   }
+  /**
+   * Default constructor.
+   */
+  PeerList();
+  
+  void update(Peer peer) {
+    if (this._map.containsKey(peer.ID)) {
+      this._map[peer.ID].update(peer);
+    } else {
+      this._map[peer.ID] = peer;
+    }
+  }
+  
+  /**
+   * TODO
+   */
+  PeerList.fromList (List<Map> peerMaps) {
+    peerMaps.forEach((Map peerMap) {
+      this.update(new Peer.fromMap(peerMap));
+    });
+  }
+  
+  /**
+   * 
+   */
+  Future<PeerList> reloadFromServer() {
+    return Service.Peer.list().then ((PeerList peerList){
+      this._map = peerList._map;
+      
+      return this;
+    });
+  }
 }
