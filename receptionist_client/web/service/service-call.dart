3249782918b00d8e0b0c2943180222533886d9a1
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-05-20 13:36:09 +0200

    Reflecting rename.

--- receptionist_client/web/service/service-call.dart
+++ receptionist_client/web/service/service-call.dart
@@ -77,7 +77,7 @@ abstract class Call {
       ..onLoad.listen((_) {
       switch (request.status) {
         case 200:
-          completer.complete(new model.Call.fromMap(JSON.decode(request.responseText)));
+          completer.complete(new model.CallList.fromJson(JSON.decode(request.responseText), 'calls'));
           break;
 
         case 400:
