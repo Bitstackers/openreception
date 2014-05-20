1878751043042108250d2df2f0d0efc53a87bc67
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-05-20 13:23:28 +0200

    Import changes.

--- receptionist_client/web/view/view.dart
+++ receptionist_client/web/view/view.dart
@@ -7,9 +7,10 @@ import '../model/model.dart' as model;
 import '../constants.dart' as constant;
 import '../service/service.dart' as Service;
 import '../storage/storage.dart' as Storage;
-import '../components.dart';
+import '../components.dart' as Component;
 import '../classes/id.dart' as id;
 import '../classes/location.dart' as nav;
+import '../controller/controller.dart' as Controller;
 
 
 import '../classes/context.dart';
