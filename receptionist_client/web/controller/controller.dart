05acedf7e8acd2b20292ccd7098a1d6db1a02e85
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-05-28 09:31:20 +0200

    Comment and style.

--- receptionist_client/web/controller/controller.dart
+++ receptionist_client/web/controller/controller.dart
@@ -1,3 +1,12 @@
+
+/**
+ * Controllers are a centralized point, from where all actions are performed.
+ * Any UI componentes should only listen to Command Events, and update their own
+ * state/presentation accordingly.
+ * Every component, such as a button, that is able to send commands will inject a
+ * command into the command stream (currently event stream, separation will follow).
+ */
+
 library controller;
 
 import 'dart:async';
@@ -8,8 +17,6 @@ import '../service/service.dart' as Service;
 import '../model/model.dart' as model;
 import '../classes/context.dart' as UIContext;
 import '../classes/location.dart' as nav;
-
-
 
 part 'controller-call.dart';
 part 'controller-context.dart';
