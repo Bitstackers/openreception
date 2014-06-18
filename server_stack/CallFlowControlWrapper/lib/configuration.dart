dd77d7e03f9e32e595de100cec78424c75f53da6
Thomas Pedersen <tp@adaheads.com>
2014-06-18 11:49:31 +0200

    Added ServerToken to dist file, and fixed configuration parsing bug.

--- server_stack/CallFlowControlWrapper/lib/configuration.dart
+++ server_stack/CallFlowControlWrapper/lib/configuration.dart
@@ -75,21 +75,21 @@ class Configuration {
       if(config.containsKey('authurl')) {
         _authUrl = Uri.parse(config['authurl']);
       }
-      
-      
+
+
       if(config.containsKey('httpport')) {
         _httpport = config['httpport'];
       }
-      
+
       if(config.containsKey('serverToken')) {
-        _httpport = config['serverToken'];
+        _serverToken = config['serverToken'];
       }
 
       if(config.containsKey('notificationServer')) {
         _notificationServer = Uri.parse(config['notificationServer']);
       }
-      
-      
+
+
       if(config.containsKey('callFlowHost')) {
         _callFlowHost = config['callFlowHost'];
       }
@@ -109,7 +109,7 @@ class Configuration {
       if(hasArgument('authurl')) {
         _authUrl = Uri.parse(_args['authurl']);
       }
-      
+
       if(hasArgument('httpport')) {
         _httpport = int.parse(_args['httpport']);
       }
