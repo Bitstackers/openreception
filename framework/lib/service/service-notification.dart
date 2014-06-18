6e7fa0b780a36b32acb1385ffe4b86866585a54a
Thomas Pedersen <tp@adaheads.com>
2014-06-18 11:02:52 +0200

    EndWiths now uses the build in function.

--- framework/lib/service/service-notification.dart
+++ framework/lib/service/service-notification.dart
@@ -5,32 +5,31 @@ abstract class Protocol {
 }
 
 abstract class Notification {
-  
-  static final String className = '${libraryName}.Notification'; 
+
+  static final String className = '${libraryName}.Notification';
 
   static HttpClient client = new HttpClient();
-  
+
   /**
    * Performs a broadcat via the notification server.
    */
   static Future broadcast(Map map, Uri host, String serverToken) {
     final String context = '${className}.broadcast';
-    
+
     if (!_UriEndsWithSlash(host)) {
       host = Uri.parse (host.toString() + '/');
     }
-    
+
     host = Uri.parse('${host}${Protocol.BROADCAST_RESOURCE}?token=${serverToken}');
-    
+
     return client.postUrl(host)
       .then(( HttpClientRequest req ) {
         req.headers.contentType = new ContentType( "application", "json", charset: "utf-8" );
         //req.headers.add( HttpHeaders.CONNECTION, "keep-alive");
         req.write( JSON.encode( map ));
         return req.close();
-      }).catchError((error, StackTrace) => logger.errorContext('${error} : ${StackTrace}' , context));    
+      }).catchError((error, StackTrace) => logger.errorContext('${error} : ${StackTrace}' , context));
   }
-  
-  static bool _UriEndsWithSlash (Uri uri) => uri.toString()[uri.toString().length-1] == '/'; 
-}
 
+  static bool _UriEndsWithSlash (Uri uri) => uri.toString().endsWith('/');
+}
