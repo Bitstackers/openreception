8f0113607052bb85ae0dd8229350ed979f783af9
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-07-15 21:40:38 +0200

    Additional fields used by the messageServer.

--- framework/lib/model/model-user.dart
+++ framework/lib/model/model-user.dart
@@ -5,9 +5,11 @@ class User {
   static int nullID = 0;
 
   Map _map = {};
-  String       get peer   => this._map['extension'];
-  int          get ID     => this._map['id'];
-  List<String> get groups => this._map['groups'];
+  String       get peer    => this._map['extension'];
+  String       get name    => this._map['name'];
+  String       get address => this._map['address'];
+  int          get ID      => this._map['id'];
+  List<String> get groups  => this._map['groups'];
 
   static Future<User> load (String identity) {
     return UserDatabase.getUser(identity).then((Map userMap) {
@@ -20,6 +22,12 @@ class User {
       return new User.fromMap (userMap);
     });
   }
+  
+  Map get asSender => 
+      { 'name'    : this.name,
+        'id'      : this.ID,
+        'address' : this.address
+      };
 
   User.fromMap (Map userMap) {
     this._map = userMap;
