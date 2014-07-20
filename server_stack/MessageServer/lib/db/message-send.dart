cf6b0953b357c2f6e80d7f8ed95143ceb57c8de2
Thomas Pedersen <tp@adaheads.com>
2014-07-20 15:14:23 +0200

    Minor code style update. Using String interpolation, not concat.

--- server_stack/MessageServer/lib/db/message-send.dart
+++ server_stack/MessageServer/lib/db/message-send.dart
@@ -35,7 +35,7 @@ Future<Map> createSendMessage(String message, MessageRecipient messageContext, M
                     'taken_by_agent'         : takenByAgentID,
                     'flags'                  : JSON.encode(flags)
                     };
-  
+
   return database.query(_pool, sql, parameters).then((rows) {
     Map data = {};
     if (rows.length == 1) {
@@ -48,16 +48,16 @@ Future<Map> createSendMessage(String message, MessageRecipient messageContext, M
 /**
  * [sqlRecipients] is expected to be a string in SQL row format e.g. ('name'), ('othername).
  * an empty list is denoted by ()
- * 
+ *
  */
 Future<Map> addRecipientsToSendMessage(String sqlRecipients) {
-  assert (sqlRecipients != ""); 
-  
+  assert (sqlRecipients != "");
+
   String sql = '''
     INSERT INTO message_recipients (contact_id, contact_name, reception_id, reception_name, message_id, recipient_role)
     VALUES $sqlRecipients''';
-  
-  
+
+
   print (sql);
   return database.execute(_pool, sql).then((int rowsAffected) {
     return {'rowsAffected': rowsAffected};
@@ -65,9 +65,9 @@ Future<Map> addRecipientsToSendMessage(String sqlRecipients) {
 }
 
 Future<Map> enqueue(Message message) {
-  
+
   final String context = '${packageName}.enqueue';
-  
+
   String sql = '''INSERT INTO message_queue (message_id) VALUES (${message.ID})''';
 
   return database.execute(_pool, sql).then((rowsAffected) {
@@ -82,9 +82,9 @@ Future<Map> enqueue(Message message) {
 
 
 Future<Map> populateQueue(Message message) {
-  
+
   final String context = "messageserver.database.populateQueue";
-  
+
   int id = message.ID;
   String sql = '''
 INSERT INTO message_queue (message_id, endpoint_id, recipient_role)
@@ -97,7 +97,7 @@ messages AS msg
   WHERE message_id = $id''';
 
   return database.execute(_pool, sql).then((rowsAffected) {
-    logger.debugContext("Inserted " + rowsAffected.toString() + " in queue for message with ID: " + message.ID.toString(), context);
+    logger.debugContext("Inserted ${rowsAffected} in queue for message with ID: ${message.ID}", context);
     return rowsAffected;
   }).catchError((error) {
     log(sql);
@@ -117,9 +117,9 @@ Future<Map> getSendMessageContacts(List<String> receptionContacts) {
           int contactId = int.parse(simpleDivide[0]);
           int receptionId = int.parse(simpleDivide[1]);
           return'($contactId, $receptionId)';
-        }) 
+        })
       .join(','); //Transform ["1@2", "3@4"] into "(1,2),(3,4)"
-  
+
   String sql = '''
     SELECT 
       mep.contact_id, 
@@ -136,7 +136,7 @@ Future<Map> getSendMessageContacts(List<String> receptionContacts) {
       JOIN messaging_addresses ma ON mep.address_id = ma.id
       JOIN reception_contacts rc ON mep.contact_id = rc.contact_id AND mep.reception_id = rc.reception_id
       JOIN contacts c ON rc.contact_id = c.id;''';
-  
+
   return database.query(_pool, sql).then((rows) {
     List contacts = new List();
     for(var row in rows) {
@@ -150,9 +150,9 @@ Future<Map> getSendMessageContacts(List<String> receptionContacts) {
          'wants_messages'  : row.wants_messages};
       contacts.add(contact);
     }
-    
+
     Map data = {'contacts': contacts};
-    
+
     return data;
   }).catchError((error) {
     log(sql);
