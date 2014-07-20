4e3085ef46b20d54b41febcd5f0b58403ef5373a
Thomas Pedersen <tp@adaheads.com>
2014-07-20 11:41:00 +0200

    The type of log message is relaxed to any type.

--- framework/lib/common.dart
+++ framework/lib/common.dart
@@ -22,43 +22,43 @@ Future activateSyslog(String hostname) {
 
 
 class BasicLogger {
-  
+
   static final int DEBUG    = 255;
   static final int INFO     = 10;
   static final int ERROR    = 5;
   static final int CRITICAL = 0;
-  
+
   int loglevel = DEBUG;
-  
-  void debugContext(String message, String context) => (this.loglevel >= DEBUG ? 
+
+  void debugContext(message, String context) => (this.loglevel >= DEBUG ?
                                                        print('[DEBUG]  ${new DateTime.now()} - $context - $message') : null);
-  void infoContext(String message, String context)  => print('[INFO]   ${new DateTime.now()} - $context - $message');
-  void errorContext(String message, String context) => print('[ERROR]  ${new DateTime.now()} - $context - $message');
-  void access(String message)                       => print('[ACCESS] ${new DateTime.now()} - $message');
-  void debug(String message) => print('[DEBUG] $message');
-  void error(String message) => print('[ERROR] $message');
-  void critical(String message) => print('[CRITICAL] $message');
+  void infoContext(message, String context)  => print('[INFO]   ${new DateTime.now()} - $context - $message');
+  void errorContext(message, String context) => print('[ERROR]  ${new DateTime.now()} - $context - $message');
+  void access(message)                       => print('[ACCESS] ${new DateTime.now()} - $message');
+  void debug(message) => print('[DEBUG] $message');
+  void error(message) => print('[ERROR] $message');
+  void critical(message) => print('[CRITICAL] $message');
 }
 
 class _SysLogger extends BasicLogger {
   Syslog _syslog;
-  
+
   _SysLogger(Syslog this._syslog);
-  
+
   static Future open(String hostname) => Syslog.open(hostname).then((Syslog syslog) => new _SysLogger(syslog));
-  
-  void debug(String message) => _syslog.log(Facility.user, Severity.Debug, message);  
-  void error(String message) => _syslog.log(Facility.user, Severity.Error, message);  
-  void critical(String message) => _syslog.log(Facility.user, Severity.Critical, message);
+
+  void debug(message) => _syslog.log(Facility.user, Severity.Debug, message);
+  void error(message) => _syslog.log(Facility.user, Severity.Error, message);
+  void critical(message) => _syslog.log(Facility.user, Severity.Critical, message);
 }
 
 int dateTimeToUnixTimestamp(DateTime time) {
-  return time.millisecondsSinceEpoch~/1000; 
+  return time.millisecondsSinceEpoch~/1000;
 }
 
 String dateTimeToJson(DateTime time) {
   //TODO We should find a format.
-  return time.toString(); 
+  return time.toString();
 }
 
 DateTime JsonToDateTime(String json) {
