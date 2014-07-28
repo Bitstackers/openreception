0e4daacbfb124aaa1419f2e29e5443a9505a41ca
Thomas Pedersen <tp@adaheads.com>
2014-07-28 14:04:41 +0200

    Added Messaging Endpoints to ReceptionContacts documents.

--- server_stack/ContactServer/lib/router/getcontact.dart
+++ server_stack/ContactServer/lib/router/getcontact.dart
@@ -3,23 +3,23 @@ part of contactserver.router;
 void getContact(HttpRequest request) {
   int contactId  = pathParameter(request.uri, 'contact');
   int receptionId = pathParameter(request.uri, 'reception');
-  
+
   cache.loadContact(receptionId, contactId).then((String reception) {
     writeAndClose(request, reception);
-      
+
   }).catchError((_) {
-    _fetchAndCacheContact(receptionId, contactId, request);
+    return _fetchAndCacheContact(receptionId, contactId, request);
   });
 }
 
 Future _fetchAndCacheContact(int receptionId, int contactId, HttpRequest request) {
   return db.getContact(receptionId, contactId).then((Map value) {
     String contact = JSON.encode(value);
-    
+
     if(value.isEmpty) {
       request.response.statusCode = HttpStatus.NOT_FOUND;
       writeAndClose(request, contact);
-      
+
     } else {
       writeAndClose(request, contact);
       return cache.saveContact(receptionId, contactId, contact)
