d1a6f2700a303f70867925292a2ef7812431da61
Kim Rostgaard Christensen <krc@retrospekt.dk>
2014-08-11 13:27:00 +0200

    Experiment with SQL statements in the (probably obsolecent) UserServer.

--- server_stack/UserServer/lib/database/database.dart
+++ server_stack/UserServer/lib/database/database.dart
@@ -5,8 +5,8 @@ import 'dart:async';
 import '../configuration.dart';
 import 'package:OpenReceptionFramework/database.dart' as database;
 
-database.Database db;
+database.Connection db;
 
 Future startDatabase() =>
-    database.Database.connect('postgres://${config.dbuser}:${config.dbpassword}@${config.dbhost}:${config.dbport}/${config.dbname}').then((database.Database
+    database.Connection.connect('postgres://${config.dbuser}:${config.dbpassword}@${config.dbhost}:${config.dbport}/${config.dbname}').then((database.Connection
         databaseHandle) {db = databaseHandle;});
