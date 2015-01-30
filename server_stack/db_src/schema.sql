0d048046fb398152735226981535de55f3707a29
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-01-30 10:33:23 +0100

    Localextension in messages.

--- server_stack/db_src/schema.sql
+++ server_stack/db_src/schema.sql
@@ -156,6 +156,7 @@ CREATE TABLE messages (
    taken_from_company        TEXT      NOT NULL DEFAULT '',
    taken_from_phone          TEXT      NOT NULL DEFAULT '',
    taken_from_cellphone      TEXT      NOT NULL DEFAULT '',
+   taken_from_localexten     TEXT      NOT NULL DEFAULT '',
    taken_by_agent            INTEGER   NOT NULL REFERENCES users (id),
    flags                     JSON      NOT NULL DEFAULT '[]',
    created_at                TIMESTAMP NOT NULL DEFAULT NOW()
