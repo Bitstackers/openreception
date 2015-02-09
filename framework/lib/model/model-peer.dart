82c01f53f75401df4ef89ec8f37c3e7fee33de35
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-02-09 20:29:26 +0100

    Active channel count to JSON representation.

--- framework/lib/model/model-peer.dart
+++ framework/lib/model/model-peer.dart
@@ -3,23 +3,27 @@ part of openreception.model;
 abstract class PeerJSONKey {
   static const ID         = 'id';
   static const REGISTERED = 'registered';
+  static const CHAN_COUNT = 'activeChannels';
 }
 
 class Peer {
   final String ID;
+  final int    channelCount;
 
   bool registered;
 
-  Peer (this.ID);
+  Peer (this.ID, this.channelCount);
 
   Map get asMap => {
     PeerJSONKey.ID         : this.ID,
-    PeerJSONKey.REGISTERED : this.registered
+    PeerJSONKey.REGISTERED : this.registered,
+    PeerJSONKey.CHAN_COUNT : this.channelCount
   };
 
   Peer.fromMap (Map map) :
-    this.ID         = map[PeerJSONKey.ID],
-    this.registered = map[PeerJSONKey.REGISTERED];
+    this.ID           = map[PeerJSONKey.ID],
+    this.registered   = map[PeerJSONKey.REGISTERED],
+    this.channelCount = map[PeerJSONKey.CHAN_COUNT];
 
   Map toJson() => this.asMap;
 
