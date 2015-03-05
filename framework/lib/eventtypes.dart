81226dda97b871ed6558b0869a04c34d08825c4b
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-03-05 15:59:01 +0100

    Removing string constants package as it was mere a duplicated of the EventJSONKey string constants found within lib/model-event.dart.

--- framework/lib/eventtypes.dart
+++
@@ -1,9 +0,0 @@
-library openreception.eventtypes;
-
-abstract class EventType {
-  static const String Call_Offer  = 'call_offer';
-  static const String Call_Lock   = 'call_lock';
-  static const String Call_Unlock = 'call_unlock';
-
-}
-
