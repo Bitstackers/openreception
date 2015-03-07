ac88c44db9c0c21bfdf8fa4c404a587e1f107bd9
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-03-07 20:53:55 +0100

    Changed to correct types.

---
+++ integration_tests/lib/config_pool.dart
@@ -0,0 +1,54 @@
+part of config;
+
+/**
+ * Helper functions for keeping track how config resource usage.
+ */
+abstract class ConfigPool {
+
+  static int aquiredLocalSIPAccounts = 0;
+  static int aquiredExternalSipAccounts = 0;
+  static int aquiredSnomHosts = 0;
+  static int aquiredPjsuaPortPorts = 0;
+
+  /**
+   * Request the next available local SIP account from the config.
+   */
+  static SIPAccount requestLocalSipAccount() {
+    SIPAccount account =
+        Config.localSipAccounts.skip(aquiredLocalSIPAccounts).first;
+    aquiredLocalSIPAccounts++;
+
+    return account;
+  }
+
+  /**
+   * Request the next available external SIP account from the config.
+   */
+  static SIPAccount requestExternalSIPAccount() {
+    SIPAccount account =
+        Config.externalSipAccounts.skip(aquiredExternalSipAccounts).first;
+    aquiredExternalSipAccounts++;
+
+    return account;
+  }
+
+  /**
+   * Request the next available Snom hostname from the config.
+   */
+  static String requestSNOMHost() {
+    String snomHost = Config.snomHosts.skip(aquiredSnomHosts).first;
+    aquiredSnomHosts++;
+
+    return snomHost;
+  }
+
+  /**
+   * Request the next available pjsua UDP port from the config.
+   */
+  static int requestPjsuaPort() {
+    int port = Config.pjsuaPortAvailablePorts.skip(aquiredPjsuaPortPorts).first;
+    aquiredPjsuaPortPorts++;
+
+    return port;
+  }
+}
