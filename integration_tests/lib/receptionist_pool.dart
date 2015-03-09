94ea503514b394e547671d753d9171c47bbb094e
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-03-09 11:38:05 +0100

    Method for waiting until every element in the pool is ready.

--- integration_tests/lib/receptionist_pool.dart
+++ integration_tests/lib/receptionist_pool.dart
@@ -5,5 +5,8 @@ class ReceptionistPool extends Pool<Receptionist>{
   static ReceptionistPool instance = null;
 
   ReceptionistPool(Iterable<Receptionist> elements) : super (elements);
+
+  Future initialized () =>
+      Future.forEach(this.elements, (Receptionist r) => r.ready());
 }
 
