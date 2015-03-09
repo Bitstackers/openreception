beab5226650f84339428b96b4334d139b504096d
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-03-09 14:32:19 +0100

    Intial server tests.

---
+++ integration_tests/bin/servertests.dart
@@ -0,0 +1,33 @@
+import 'package:openreception_framework/service.dart' as Service;
+import 'package:openreception_framework/model.dart' as Model;
+import 'package:openreception_framework/service-io.dart' as Transport;
+import 'package:openreception_framework/storage.dart' as Storage;
+
+import '../lib/or_test_fw.dart';
+
+import 'package:logging/logging.dart';
+import 'package:unittest/unittest.dart';
+
+import '../lib/config.dart';
+import 'data/testdata.dart' as Test_Data;
+
+void main() {
+  Logger.root.level = Level.INFO;
+  Logger.root.onRecord.listen(print);
+
+  group('service.ResourceMessage', () {
+    Storage.Message messageStore =
+        new Service.RESTMessageStore(Config.messageServerUri,
+            Config.serverToken, new Transport.Client());
+
+   test('${Service.RESTMessageStore.className}.list (non-filtered)', () =>
+       RESTMessageStore.messageList(messageStore));
+   test('${Service.RESTMessageStore.className}.get (non-existing ID)', () =>
+       RESTMessageStore.messageNotExists(messageStore));
+   test('${Service.RESTMessageStore.className}.get (existence)', () =>
+       RESTMessageStore.messageExists(messageStore, 1));
+   test('${Service.RESTMessageStore.className}.get (equality)', () =>
+       RESTMessageStore.messageMapEquality(messageStore, 1,
+           new Model.Message.fromMap(Test_Data.testMessage_1_Map)));
+  });
+}
