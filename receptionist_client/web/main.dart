de1a7c19909d664786f107609d502f4d1243de13
Thomas Løcke <thomas@12boo.net>
2015-03-16 13:44:37 +0100

    More or less got message-compose under control

--- receptionist_client/web/main.dart
+++ receptionist_client/web/main.dart
@@ -6,4 +6,8 @@ void main() {
 
   button.onClick.listen((_) => div.classes.toggle('focus'));
 //  button.onClick.listen((_) => querySelector('#context-home').classes.toggle('none'));
+
+  SpanElement showRecipients = querySelector('#message-compose .show-recipients');
+  showRecipients.onMouseOver.listen((_) => querySelector('#message-compose .recipients').classes.toggle('recipients-hidden', false));
+  showRecipients.onMouseOut.listen((_) => querySelector('#message-compose .recipients').classes.toggle('recipients-hidden', true));
 }
