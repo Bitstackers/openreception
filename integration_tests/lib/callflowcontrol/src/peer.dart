f9054c0968dc9e9b886ab6554c3101614c18ba7b
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-03-16 19:37:29 +0100

    Additional test. Peer listing.

--- integration_tests/lib/callflowcontrol/src/peer.dart
+++ integration_tests/lib/callflowcontrol/src/peer.dart
@@ -47,47 +47,7 @@ abstract class Peer {
       });
   }
 
-  static Future list () {
-
-  }
-//
-//  def test_event_and_list_presence(self):
-//
-//          self.log.info ("Unregistering agent to assert that we get a registration event.")
-//          receptionist.sip_phone.Unregister()
-//
-//
-//          self.log.info ("Flushing event stack.")
-//          receptionist.event_stack.flush() # Purge any registration events.
-//
-//          self.log.info ("Registering receptionst sip agent.")
-//          receptionist.sip_phone.Register()
-//          self.log.info ("Expecting peer_state event.")
-//          receptionist.event_stack.WaitFor(event_type="peer_state")
-//
-//          self.log.info ("Event received, looking up receptionist in peerlist.")
-//          peer = receptionist.call_control.peerList().locatePeer(receptionist.username)
-//          if not peer ['registered']:
-//              self.fail (receptionist.username + " expected to be in peer list at this point")
-//
-//          self.log.info ("Flushing event stack.")
-//          receptionist.event_stack.flush() # Purge any registration events.
-//
-//          self.log.info ("Unregistering agent again to complete cycle.")
-//          receptionist.sip_phone.Unregister()
-//
-//          self.log.info ("Expecting peer_state event.")
-//          receptionist.event_stack.WaitFor(event_type="peer_state")
-//
-//          self.log.info ("Event received, looking up receptionist in peerlist.")
-//          peer = receptionist.call_control.peerList().locatePeer(receptionist.username)
-//          if peer ['registered']:
-//              self.fail("Peer is still registered: " + str (peer))
-//
-//          self.log.info ("Peer is no longer registered, run complete.")
-//          receptionist.release()
-//
-//      except:
-//          receptionist.release()
-//          raise
+  static Future list (Service.CallFlowControl callFlowControl) =>
+    callFlowControl.peerList().then((Iterable<Model.Peer> peers) =>
+      expect (peers.length, isPositive));
 }
