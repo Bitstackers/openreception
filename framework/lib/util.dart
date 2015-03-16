ab5cebe51f938b26e223f80ab017a1a8ca1d74e4
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-03-16 21:21:25 +0100

    Removed explicit UTC code as they didn't play well with our serialization format.

--- framework/lib/util.dart
+++ framework/lib/util.dart
@@ -1,11 +1,11 @@
 library openreception.utilities;
 
 int dateTimeToUnixTimestamp(DateTime time) {
-  return time.toUtc().millisecondsSinceEpoch~/1000;
+  return time.millisecondsSinceEpoch~/1000;
 }
 
 DateTime unixTimestampToDateTime(int secondsSinceEpoch) {
-  return new DateTime.fromMillisecondsSinceEpoch(secondsSinceEpoch*1000, isUtc: true);
+  return new DateTime.fromMillisecondsSinceEpoch(secondsSinceEpoch*1000);
 }
 
 String removeTailingSlashes (Uri host) {
