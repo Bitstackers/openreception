6ba85ceebb1f86528f449cac2d67ff72cce41440
Thomas Pedersen <tp@adaheads.com>
2015-03-30 16:49:58 +0200

    Contact objects id field is now define from the framework.

--- server_stack/lib/management_server/view/contact.dart
+++ server_stack/lib/management_server/view/contact.dart
@@ -2,23 +2,25 @@ library adaheads.server.view.contact;
 
 import 'dart:convert';
 
+import 'package:openreception_framework/model.dart' as ORF;
+
 import '../model.dart';
 
 String contactAsJson(Contact contact) => JSON.encode(_contactAsJsonMap(contact));
 
 String listContactAsJson(List<Contact> contacts) =>
-    JSON.encode({'contacts':_listContactAsJsonList(contacts)});
+    JSON.encode({ORF.ContactJSONKey.Contact_LIST:_listContactAsJsonList(contacts)});
 
-String contactIdAsJson(int id) => JSON.encode({'id': id});
+String contactIdAsJson(int id) => JSON.encode({ORF.ContactJSONKey.contactID: id});
 
 String contactTypesAsJson(List<String> types) => JSON.encode({'contacttypes': types});
 String addressTypesAsJson(List<String> types) => JSON.encode({'addresstypes': types});
 
 Map _contactAsJsonMap(Contact contact) => contact == null ? {} :
-    {'id': contact.id,
-     'full_name': contact.fullName,
-     'contact_type': contact.contactType,
-     'enabled': contact.enabled};
+    {ORF.ContactJSONKey.contactID: contact.id,
+     ORF.ContactJSONKey.fullName: contact.fullName,
+     ORF.ContactJSONKey.contactType: contact.contactType,
+     ORF.ContactJSONKey.enabled: contact.enabled};
 
 List _listContactAsJsonList(List<Contact> contacts) =>
     contacts.map(_contactAsJsonMap).toList();
