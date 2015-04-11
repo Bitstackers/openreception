47b7d1b151c7eb826ab2c991ef8cb61653affb7d
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-04-11 13:15:43 +0200

    Data model imports.

--- receptionist_client/web/model/model.dart
+++ receptionist_client/web/model/model.dart
@@ -1,7 +1,34 @@
 library model;
 
 import 'dart:async';
+import 'dart:collection';
 import 'dart:html';
+
+import '../controller/controller.dart' as Controller;
+import '../classes/events.dart' as event;
+import '../service/service.dart' as Service;
+import '../storage/storage.dart' as storage;
+import 'package:event_bus/event_bus.dart';
+import 'package:openreception_framework/model.dart' as ORModel;
+
+part 'model-call.dart';
+part 'model-call-list.dart';
+part 'model-contact.dart';
+part 'model-contact-list.dart';
+part 'model-message.dart';
+part 'model-message-filter.dart';
+part 'model-message-list.dart';
+part 'model-notification.dart';
+part 'model-notification-list.dart';
+part 'model-origination-request.dart';
+part 'model-peer.dart';
+part 'model-peer-list.dart';
+part 'model-phone-number.dart';
+part 'model-reception.dart';
+part 'model-recipient.dart';
+part 'model-transfer-request.dart';
+part 'model-user.dart';
+part 'model-user-status.dart';
 
 part 'model-ui-agent-info.dart';
 part 'model-ui-calendar-editor.dart';
@@ -12,6 +39,8 @@ part 'model-ui-contexts.dart';
 part 'model-ui-message-compose.dart';
 part 'model-ui-reception-calendar.dart';
 part 'model-ui-reception-commands.dart';
+
+const String libraryName = "model";
 
 enum AgentState {Busy, Idle, Pause, Unknown}
 enum AlertState {Off, On}
