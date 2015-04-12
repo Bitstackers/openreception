38b46d85560a43491ab70b25b6e987f0949cb8ae
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-04-12 21:53:11 +0200

    Interface methos for validation method.

--- framework/lib/service/service-auth.dart
+++ framework/lib/service/service-auth.dart
@@ -12,10 +12,24 @@ class Authentication {
   Authentication (Uri this._host, String this._token, this._backed);
 
   /**
-   * Performs a lookup of the user on the notification server via the supplied token.
+   * Performs a lookup of the user on the notification server from the
+   * supplied token.
    */
-  Future<Model.User> userOf(String token) =>
-      this._backed.get (Resource.Authentication.tokenToUser(this._host, token))
+  Future<Model.User> userOf(String token) {
+    Uri uri = Resource.Authentication.tokenToUser(this._host, token);
+
+    return this._backed.get (uri)
       .then((String response)
         => new Model.User.fromMap(JSON.decode(response)));
+  }
+
+  /**
+   * Validate [token]. Throws [NotFound] exception if the token is not valid.
+   */
+  Future validate(String token) {
+    Uri uri = Resource.Authentication.validate(this._host, token);
+
+    return this._backed.get (uri);
+  }
+
 }
