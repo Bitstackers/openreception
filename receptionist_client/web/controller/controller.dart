458e58ea138b0d03be67f3191bad98567356db5c
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-04-15 10:20:41 +0200

    Missing imports and parts.

--- receptionist_client/web/controller/controller.dart
+++ receptionist_client/web/controller/controller.dart
@@ -5,9 +5,16 @@ import 'dart:html';
 
 import 'package:okeyee/okeyee.dart';
 import 'package:openreception_framework/bus.dart';
+import 'package:logging/logging.dart';
+import '../model/model.dart' as Model;
+import '../service/service.dart' as Service;
 
+part 'controller-call.dart';
+part 'controller-context.dart';
 part 'controller-hotkeys.dart';
 part 'controller-navigation.dart';
+
+const String libraryName = "Controller";
 
 enum Context {Home,
               Homeplus,
