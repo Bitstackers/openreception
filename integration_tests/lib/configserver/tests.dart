7963e5e6b300682cab302bee1375b826793b6fca
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-04-16 11:11:22 +0200

    Additional config tests.

--- integration_tests/lib/configserver/tests.dart
+++ integration_tests/lib/configserver/tests.dart
@@ -3,6 +3,7 @@ part of or_test_fw;
 void runConfigServerTests() {
   group ('RESTConfigService', () {
     Transport.Client transport = null;
+    Service.RESTConfiguration configServer;
 
     setUp (() {
       transport = new Transport.Client();
@@ -17,6 +18,23 @@ void runConfigServerTests() {
 
     test ('Non-existing path',
         () => ConfigService.nonExistingPath(transport.client));
+
+    setUp (() {
+      transport = new Transport.Client();
+      configServer = new Service.RESTConfiguration
+          (Config.configServerUri, transport);
+    });
+
+    tearDown (() {
+      transport.client.close(force : true);
+      configServer = null;
+    });
+
+    test ('Get',
+          () => configServer.clientConfig()
+            .then((Model.ClientConfiguration configuration) {
+              expect (configuration, isNotNull);
+            }));
   });
 }
 
