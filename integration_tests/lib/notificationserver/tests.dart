8e0c0c2c9ebdac688bae1cec41531726071f550f
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-04-19 18:48:21 +0200

    Outlined Notification server tests.

---
+++ integration_tests/lib/notificationserver/tests.dart
@@ -0,0 +1,16 @@
+part of or_test_fw;
+
+/**
+ * TODO: Add tests for both broadcast, send and FIFO message ordering.
+ */
+void runNotificatioTests() {
+  group('NotificationService', () {
+
+    setUp (() {
+    });
+
+    tearDown (() {
+    });
+
+  });
+}
