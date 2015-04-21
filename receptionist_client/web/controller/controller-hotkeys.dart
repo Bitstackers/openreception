6d08cc4a4e3f8d06a45f528564a58ab6c9755961
Thomas Løcke <thomas@12boo.net>
2015-04-21 11:14:23 +0200

    Prettyfied controller.hotkeys

--- receptionist_client/web/controller/controller-hotkeys.dart
+++ receptionist_client/web/controller/controller-hotkeys.dart
@@ -1,48 +1,44 @@
 part of controller;
 
 /**
- *
+ * TODO (TL): Comment
  */
 class HotKeys {
   static final HotKeys _singleton = new HotKeys._internal();
   factory HotKeys() => _singleton;
 
+  final Keyboard _keyDown = new Keyboard();
+
+  final Bus<KeyboardEvent> _altA = new Bus<KeyboardEvent>();
+  final Bus<KeyboardEvent> _altB = new Bus<KeyboardEvent>();
+  final Bus<KeyboardEvent> _altE = new Bus<KeyboardEvent>();
+  final Bus<KeyboardEvent> _altH = new Bus<KeyboardEvent>();
+  final Bus<KeyboardEvent> _altI = new Bus<KeyboardEvent>();
+  final Bus<KeyboardEvent> _altK = new Bus<KeyboardEvent>();
+  final Bus<KeyboardEvent> _altQ = new Bus<KeyboardEvent>();
+  final Bus<KeyboardEvent> _altS = new Bus<KeyboardEvent>();
+  final Bus<KeyboardEvent> _altT = new Bus<KeyboardEvent>();
+  final Bus<KeyboardEvent> _altV = new Bus<KeyboardEvent>();
+  final Bus<KeyboardEvent> _altW = new Bus<KeyboardEvent>();
+  final Bus<KeyboardEvent> _f1   = new Bus<KeyboardEvent>();
+
+  Stream<KeyboardEvent> get onAltA => _altA.stream;
+  Stream<KeyboardEvent> get onAltB => _altB.stream;
+  Stream<KeyboardEvent> get onAltE => _altE.stream;
+  Stream<KeyboardEvent> get onAltH => _altH.stream;
+  Stream<KeyboardEvent> get onAltI => _altI.stream;
+  Stream<KeyboardEvent> get onAltK => _altK.stream;
+  Stream<KeyboardEvent> get onAltQ => _altQ.stream;
+  Stream<KeyboardEvent> get onAltS => _altS.stream;
+  Stream<KeyboardEvent> get onAltT => _altT.stream;
+  Stream<KeyboardEvent> get onAltV => _altV.stream;
+  Stream<KeyboardEvent> get onAltW => _altW.stream;
+  Stream<KeyboardEvent> get onF1   => _f1.stream;
+
   /**
    * Internal constructor.
    */
   HotKeys._internal() {
-    _initialize();
-  }
-
-  final Keyboard _keyDown = new Keyboard();
-
-  final Bus<KeyboardEvent> _altA     = new Bus<KeyboardEvent>();
-  final Bus<KeyboardEvent> _altB     = new Bus<KeyboardEvent>();
-  final Bus<KeyboardEvent> _altE     = new Bus<KeyboardEvent>();
-  final Bus<KeyboardEvent> _altH     = new Bus<KeyboardEvent>();
-  final Bus<KeyboardEvent> _altI     = new Bus<KeyboardEvent>();
-  final Bus<KeyboardEvent> _altK     = new Bus<KeyboardEvent>();
-  final Bus<KeyboardEvent> _altQ     = new Bus<KeyboardEvent>();
-  final Bus<KeyboardEvent> _altS     = new Bus<KeyboardEvent>();
-  final Bus<KeyboardEvent> _altT     = new Bus<KeyboardEvent>();
-  final Bus<KeyboardEvent> _altV     = new Bus<KeyboardEvent>();
-  final Bus<KeyboardEvent> _altW     = new Bus<KeyboardEvent>();
-  final Bus<KeyboardEvent> _f1       = new Bus<KeyboardEvent>();
-
-  Stream<KeyboardEvent> get onAltA     => _altA.stream;
-  Stream<KeyboardEvent> get onAltB     => _altB.stream;
-  Stream<KeyboardEvent> get onAltE     => _altE.stream;
-  Stream<KeyboardEvent> get onAltH     => _altH.stream;
-  Stream<KeyboardEvent> get onAltI     => _altI.stream;
-  Stream<KeyboardEvent> get onAltK     => _altK.stream;
-  Stream<KeyboardEvent> get onAltQ     => _altQ.stream;
-  Stream<KeyboardEvent> get onAltS     => _altS.stream;
-  Stream<KeyboardEvent> get onAltT     => _altT.stream;
-  Stream<KeyboardEvent> get onAltV     => _altV.stream;
-  Stream<KeyboardEvent> get onAltW     => _altW.stream;
-  Stream<KeyboardEvent> get onF1       => _f1.stream;
-
-  void _initialize() {
     window.document.onKeyDown.listen(_keyDown.press);
 
     final Map<String, EventListener> preventDefaultBindings =
