2324f062d9beb47bc451a1badc08e4a247a49b06
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-04-22 07:14:48 +0200

    Moved to Logging package and performed general cleanup.

--- server_stack/lib/callflowcontrol/model/model-pbx_client.dart
+++ server_stack/lib/callflowcontrol/model/model-pbx_client.dart
@@ -1,19 +1,15 @@
 part of callflowcontrol.model;
 
 abstract class PBXClient {
-  
-  static const String className = '${libraryName}.PBXClient'; 
 
-   static ESL.Connection instance = null;
-   
-   static Future<ESL.Response> api (String command) {
-     const String context = '${className}.api';
-     
-     
-     return instance.api(command).then((ESL.Response response) {
-       logger.debugContext('$command => ${response.rawBody}' , context);
-       return response;
-     });
-   }
-   
+  static final Logger log = new Logger ('${libraryName}.PBXClient');
+
+  static ESL.Connection instance = null;
+
+  static Future<ESL.Response> api (String command) {
+    return instance.api(command).then((ESL.Response response) {
+      log.finest('$command => ${response.rawBody}');
+      return response;
+    });
+  }
 }
