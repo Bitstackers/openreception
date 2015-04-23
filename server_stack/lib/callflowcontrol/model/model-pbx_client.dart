17f593ac6827a4876a611deaa7fac4e1e3050c11
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-04-23 20:22:03 +0200

    Addition of bgapi command.

--- server_stack/lib/callflowcontrol/model/model-pbx_client.dart
+++ server_stack/lib/callflowcontrol/model/model-pbx_client.dart
@@ -8,8 +8,16 @@ abstract class PBXClient {
 
   static Future<ESL.Response> api (String command) {
     return instance.api(command).then((ESL.Response response) {
-      log.finest('$command => ${response.rawBody}');
+      log.finest('api $command => ${response.rawBody}');
       return response;
     });
   }
+
+  static Future<ESL.Reply> bgapi (String command) {
+    return instance.bgapi(command).then((ESL.Reply response) {
+      log.finest('bgapi $command => ${response.content}');
+      return response;
+    });
+  }
+
 }
