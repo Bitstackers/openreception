b9fd77db37a289c298669991f9effeb66e22585a
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-05-02 13:37:28 +0200

    Changed from singleton instance to real instance.

--- receptionist_client/web/controller/controller-call.dart
+++ receptionist_client/web/controller/controller-call.dart
@@ -18,77 +18,96 @@ enum CallCommand {
   TRANSFERFAILURE
 }
 
-abstract class Call {
+class Call {
   static final Logger log = new Logger ('${libraryName}.Call');
 
-  static Bus<CallCommand> _command = new Bus<CallCommand>();
-  static Stream<CallCommand> get commandStream => _command.stream;
+  Bus<CallCommand> _command = new Bus<CallCommand>();
+  Stream<CallCommand> get commandStream => _command.stream;
 
-  static Future dial(String extension, Model.Reception reception, Model.Contact contact) {
+  final ORService.CallFlowControl _service;
+
+  Call(this._service);
+
+  Future dial(String extension, Model.Reception reception, Model.Contact contact) {
     log.info('Dialing $extension.');
 
     _command.fire(CallCommand.DIAL);
-//    return Service.Call.instance.originate(contact, reception, extension)
-    return _call.originate(contact, reception, extension)
-      .then((_) => _command.fire(CallCommand.DIALSUCCESS))
+    return this._service.originate(extension, contact.ID, reception.ID)
+      .then((ORModel.Call orCall) {
+        _command.fire(CallCommand.DIALSUCCESS);
+        return new Model.Call.fromORModel(orCall);
+      })
       .catchError((error, stackTrace) {
         log.severe(error, stackTrace);
         _command.fire(CallCommand.DIALFAILURE);
+
+        return new Future.error(new ControllerError(error.toString()));
       });
   }
 
-  static Future<Model.Call> pickupParked (Model.Call call) => pickup (call);
-
+  Future<Model.Call> pickupParked (Model.Call call) => pickup (call);
 
   /**
    * Make the service layer perform a pickup request to the
    * call-flow-control server.
    */
-  static Future<Model.Call> pickup(Model.Call call) {
+  Future<Model.Call> pickup(Model.Call call) {
     log.info('Picking up $call.');
 
     _command.fire(CallCommand.PICKUP);
-//    return Service.Call.instance.pickup(call)
-    return _call.pickup(call)
-      .then((_) => _command.fire(CallCommand.PICKUPSUCCESS))
+    return this._service.pickupMap(call.ID)
+      .then((Map callMap) {
+        _command.fire(CallCommand.PICKUPSUCCESS);
+        return new Model.Call.fromMap(callMap);
+      })
       .catchError((error, stackTrace) {
         log.severe(error, stackTrace);
         _command.fire(CallCommand.PICKUPFAILURE);
+
+        return new Future.error(new ControllerError(error.toString()));
       });
   }
 
-  static Future hangup(Model.Call call) {
+  Future hangup(Model.Call call) {
     log.info('Hanging up $call.');
 
     _command.fire(CallCommand.HANGUP);
-//    return Service.Call.instance.hangup(call)
-    return _call.hangup(call)
+    return this._service.hangup(call.ID)
       .then((_) => _command.fire(CallCommand.HANGUPSUCCESS))
       .catchError((error, stackTrace) {
         log.severe(error, stackTrace);
         _command.fire(CallCommand.HANGUPFAILURE);
+
+        return new Future.error(new ControllerError(error.toString()));
       });
   }
 
-  static Future park(Model.Call call) {
+  Future park(Model.Call call) {
     _command.fire(CallCommand.PARK);
-//    return Service.Call.instance.park(call)
-    return _call.park(call)
-      .then((_) => _command.fire(CallCommand.PARKSUCCESS))
+
+    return this._service.park(call.ID)
+      .then((ORModel.Call orCall) {
+        _command.fire(CallCommand.PARKSUCCESS);
+        return new Model.Call.fromORModel(orCall);
+      })
       .catchError((error, stackTrace) {
         log.severe(error, stackTrace);
         _command.fire(CallCommand.PARKFAILURE);
+
+        return new Future.error(new ControllerError(error.toString()));
       });
   }
 
-  static Future transfer(Model.Call source, Model.Call destination) {
+  Future transfer(Model.Call source, Model.Call destination) {
     _command.fire(CallCommand.TRANSFER);
-//    return Service.Call.instance.transfer(source, destination)
-    return _call.transfer(source, destination)
+
+    return this._service.transfer(source.ID, destination.ID)
       .then((_) => _command.fire(CallCommand.TRANSFERSUCCESS))
       .catchError((error, stackTrace) {
         log.severe(error, stackTrace);
         _command.fire(CallCommand.TRANSFERFAILURE);
+
+        return new Future.error(new ControllerError(error.toString()));
       });
     }
 }
