6b0b6af1ac02a750e99d1d565949b3d5147e5f7e
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-05-11 12:31:07 +0200

    Enabled notification server tests.

--- integration_tests/lib/notificationserver/tests.dart
+++ integration_tests/lib/notificationserver/tests.dart
@@ -10,11 +10,16 @@ void runNotificationTests() {
     Receptionist receptionist1;
     Receptionist receptionist2;
     Receptionist receptionist3;
+    Service.NotificationService notificationService;
+    Transport.Client transport;
 
     setUp (() {
       receptionist1 = ReceptionistPool.instance.aquire();
       receptionist2 = ReceptionistPool.instance.aquire();
       receptionist3 = ReceptionistPool.instance.aquire();
+      transport = new Transport.Client();
+      notificationService = new Service.NotificationService
+          (Config.notificationServiceUri, Config.serverToken, transport);
 
       return Future.wait([receptionist1.initialize(),
                           receptionist2.initialize(),
@@ -25,6 +30,8 @@ void runNotificationTests() {
       ReceptionistPool.instance.release(receptionist1);
       ReceptionistPool.instance.release(receptionist2);
       ReceptionistPool.instance.release(receptionist3);
+      transport.client.close(force : true);
+      notificationService = null;
 
       return Future.wait([receptionist1.teardown(),
                           receptionist2.teardown(),
@@ -34,5 +41,24 @@ void runNotificationTests() {
     test('Event broadcast', () =>
       NotificationService.eventBroadcast
         ([receptionist1, receptionist2, receptionist3]));
+
+    test('ConnectionState listing', () =>
+      NotificationService.connectionStateList
+        ([receptionist1, receptionist2, receptionist3], notificationService));
+
+    test('ConnectionState get', () =>
+      NotificationService.connectionState
+        ([receptionist1, receptionist2, receptionist3], notificationService));
+
+    //TODO: Implement these tests.
+//    test('Event clientConnectionState', () =>
+//      NotificationService.clientConnectionState
+//        ([receptionist1, receptionist2, receptionist3]));
+
+    //    test('Event send', () =>
+//      NotificationService.eventSend
+//        ([receptionist1, receptionist2, receptionist3],
+//            notificationService));
+
   });
 }
