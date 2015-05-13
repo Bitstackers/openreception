af3048dd3785abb4dce81234b4418d721e6088da
Thomas Løcke <tl@bitstackers.dk>
2015-05-13 13:30:10 +0200

    Tightened the code up and added some comments

--- receptionist_client/web/service/service-notification.dart
+++ receptionist_client/web/service/service-notification.dart
@@ -10,50 +10,66 @@
   You should have received a copy of the GNU General Public License along with
   this program; see the file COPYING3. If not, see http://www.gnu.org/licenses.
 */
+
 part of service;
 
+/**
+ * Contains a bunch of notification streams for various events.
+ */
 class Notification {
+  Bus<Model.UserStatus>            _agentStateChange      = new Bus<Model.UserStatus>();
+  Bus<OREvent.CalendarChange>      _calendarChange        = new Bus<OREvent.CalendarChange>();
+  Bus<Model.Call>                  _callStateChange       = new Bus<Model.Call>();
+  Bus<Model.ClientConnectionState> _clientConnectionState = new Bus<Model.ClientConnectionState>();
+  final Logger                     _log                   = new Logger('$libraryName.Notification');
+  ORService.NotificationSocket     _socket                = null;
 
-  Logger log = new Logger('$libraryName.Notification');
-
-
-  ORService.NotificationSocket _socket = null;
-
+  /**
+   * Constructor.
+   */
   Notification (this._socket) {
-    _socket.eventStream.listen(_dispatch);
+    _observers();
   }
 
-  /// Agent state change
-  Bus<Model.UserStatus> _agentStateChange = new Bus<Model.UserStatus>();
-  Stream<Model.UserStatus> get onAgentStateChange => _agentStateChange.stream;
-
-  /// Call state change
-  Bus<Model.Call> _callStateChange = new Bus<Model.Call>();
-  Stream<Model.Call> get onAnyCallStateChange => _callStateChange.stream;
-
-  /// Client connection state change
-  Bus<Model.ClientConnectionState> _clientConnectionState = new Bus<Model.ClientConnectionState>();
-  Stream<Model.ClientConnectionState> get onClientConnectionStateChange => _clientConnectionState.stream;
-
-  /// Calendar Event changes
-  Bus<OREvent.CalendarChange> _calendarChange = new Bus<OREvent.CalendarChange>();
-  Stream<OREvent.CalendarChange> get onCalendarChange => _calendarChange.stream;
-
+  /**
+   * Fire [event] on relevant bus.
+   */
   void _dispatch (OREvent.Event event) {
-    if (event is OREvent.CallEvent) {
-      _dispatchCall(event);
-    }
-    else if(event is OREvent.CalendarChange) {
-      this._calendarChange.fire(event);
-    }
-    else if(event is OREvent.UserState) {
+    if(event is OREvent.CallEvent) {
+      _callStateChange.fire(new Model.Call.fromORModel(event.call));
+    } else if(event is OREvent.CalendarChange) {
+      _calendarChange.fire(event);
+    } else if(event is OREvent.UserState) {
       _agentStateChange.fire(new Model.UserStatus.fromMap(event.asMap));
     } else {
-      log.severe('Failed to dispatch event ${event}');
+      _log.severe('Failed to dispatch event ${event}');
     }
   }
 
-  void _dispatchCall (OREvent.CallEvent event) {
-    this._callStateChange.fire(new Model.Call.fromORModel(event.call));
+  /**
+   * Agent state change stream.
+   */
+  Stream<Model.UserStatus> get onAgentStateChange => _agentStateChange.stream;
+
+  /**
+   * Call state change stream.
+   */
+  Stream<Model.Call> get onAnyCallStateChange => _callStateChange.stream;
+
+  /**
+   * Calendar Event changes stream.
+   */
+  Stream<OREvent.CalendarChange> get onCalendarChange => _calendarChange.stream;
+
+  /**
+   * Client connection state change stream.
+   */
+  Stream<Model.ClientConnectionState> get onClientConnectionStateChange => _clientConnectionState.stream;
+
+  /**
+   * Observers.
+   */
+  void _observers() {
+    _socket.eventStream.listen(_dispatch);
   }
 }
