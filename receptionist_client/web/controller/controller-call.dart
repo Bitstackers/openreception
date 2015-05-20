c17d1c3152cc5a78c3e48396534cbc3eca4a4fb7
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-05-20 12:41:21 +0200

    Additional call manipulation mechanisms.

--- receptionist_client/web/controller/controller-call.dart
+++ receptionist_client/web/controller/controller-call.dart
@@ -58,7 +58,9 @@ class Call {
     return this._service.pickupMap(call.ID)
       .then((Map callMap) {
         _command.fire(CallCommand.PICKUPSUCCESS);
-        return new Model.Call.fromMap(callMap);
+        Model.Call.activeCall = new Model.Call.fromMap(callMap);
+
+        return Model.Call.activeCall;
       })
       .catchError((error, stackTrace) {
         log.severe(error, stackTrace);
@@ -66,6 +68,42 @@ class Call {
 
         return new Future.error(new ControllerError(error.toString()));
       });
+  }
+
+  /**
+   *
+   */
+  Future<Model.Call> pickupNext() {
+
+    bool availableForPickup (ORModel.Call call) =>
+      call.assignedTo == Model.User.noID && !call.locked;
+
+    return this._service.callList().then((Iterable<ORModel.Call> calls) {
+      ORModel.Call foundCall = calls.firstWhere
+        (availableForPickup, orElse: () => null);
+
+      if (foundCall != null) {
+        return pickup(new Model.Call.fromORModel(foundCall));
+      }
+      else {
+        return Model.Call.noCall;
+      }
+    });
+  }
+
+  /**
+   *
+   */
+  Future<Model.Call> pickupFirstParkedCall() {
+    return this._service.callList().then((Iterable<ORModel.Call> calls) {
+      ORModel.Call parkedCall = calls.firstWhere((ORModel.Call call) =>
+      call.assignedTo == Model.User.currentUser.ID &&
+      call.state == ORModel.CallState.Parked, orElse: null);
+
+      if (parkedCall != null) {
+        this.pickup(new Model.Call.fromORModel(parkedCall));
+      }
+    });
   }
 
   Future hangup(Model.Call call) {
@@ -83,12 +121,16 @@ class Call {
   }
 
   Future park(Model.Call call) {
+    if (call == Model.Call.noCall) {
+      return new Future.value(Model.Call.noCall);
+    }
+
     _command.fire(CallCommand.PARK);
 
     return this._service.park(call.ID)
-      .then((ORModel.Call orCall) {
+      .then((String stuff) {
         _command.fire(CallCommand.PARKSUCCESS);
-        return new Model.Call.fromORModel(orCall);
+        return new Model.Call.fromMap(JSON.decode(stuff));
       })
       .catchError((error, stackTrace) {
         log.severe(error, stackTrace);
