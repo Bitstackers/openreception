502741f5f52b36d05a2ad7097be1a3e43ad7586d
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-05-27 11:51:06 +0200

    Username field for calendar entry changes.

--- framework/lib/model/model-calendar_entry_change.dart
+++ framework/lib/model/model-calendar_entry_change.dart
@@ -3,23 +3,27 @@ part of openreception.model;
 abstract class CalendarEntryChangeKey {
   static const String userID = 'uid';
   static const String updatedAt = 'updated';
+  static const String username = 'username';
 }
 
 class CalendarEntryChange {
 
   int userID = User.nullID;
   DateTime changedAt;
+  String username;
 
   CalendarEntryChange();
 
   CalendarEntryChange.fromMap(Map map) {
     this.userID = map[CalendarEntryChangeKey.userID];
     this.changedAt = Util.unixTimestampToDateTime(map[CalendarEntryChangeKey.updatedAt]);
+    this.username = map[CalendarEntryChangeKey.username];
   }
 
   Map get asMap => {
     CalendarEntryChangeKey.userID : this.userID,
-    CalendarEntryChangeKey.updatedAt : Util. dateTimeToUnixTimestamp(changedAt)
+    CalendarEntryChangeKey.updatedAt : Util.dateTimeToUnixTimestamp(changedAt),
+    CalendarEntryChangeKey.username : this.username
   };
 
   Map toJson() => this.asMap;
