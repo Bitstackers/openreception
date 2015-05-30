1fb43474677a22720232f224f5eaf5aaac958c26
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-05-30 14:47:27 +0200

    Intial partial commit.

---
+++ integration_tests/bin/on_demand_service.dart
@@ -0,0 +1,92 @@
+library receptionserver.router;
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io' as IO;
+
+import 'package:logging/logging.dart';
+
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_route/shelf_route.dart' as shelf_route;
+import 'package:shelf_cors/shelf_cors.dart' as shelf_cors;
+
+import '../lib/or_test_fw.dart';
+import '../lib/on_demand_handlers.dart' as handler;
+
+const String libraryName = 'support_tools.on_demand_service';
+final Logger log = new Logger (libraryName);
+
+shelf.Middleware checkAuthentication =
+  shelf.createMiddleware(requestHandler: _lookupToken, responseHandler: null);
+
+
+shelf.Response _lookupToken(shelf.Request request) {
+  var token = request.requestedUri.queryParameters['token'];
+
+  if (token != 'magic') {
+      return new shelf.Response.forbidden('Invalid token');
+  }
+
+  return null;
+}
+
+
+/// Simple access logging.
+void _accessLogger(String msg, bool isError) {
+  if (isError) {
+    log.severe(msg);
+  } else {
+    log.finest(msg);
+  }
+}
+
+
+Future<IO.HttpServer> start(
+    handler.Receptionist receptionistHandler,
+    handler.Customer customerHandler,
+    {String hostname : '0.0.0.0', int port : 4224}) {
+  var router = shelf_route.router()
+    ..get('/resource/receptionist/aquire', receptionistHandler.aquire)
+    ..get('/resource/receptionist/{rid}/release', receptionistHandler.release)
+    ..get('/resource/receptionist/{rid}', receptionistHandler.get)
+    ..get('/resource/customer/aquire', customerHandler.aquire)
+    ..get('/resource/customer/{cid}/release', customerHandler.release)
+    ..get('/resource/customer/{cid}/dial/{extension}', customerHandler.dial)
+    ..get('/resource/customer/{cid}/hangupAll', customerHandler.hangup)
+    ..get('/resource/customer/{cid}', customerHandler.get);
+
+  var handler = const shelf.Pipeline()
+      .addMiddleware(shelf_cors.createCorsHeadersMiddleware())
+      .addMiddleware(checkAuthentication)
+      .addMiddleware(shelf.logRequests(logger : _accessLogger))
+      .addHandler(router.handler);
+
+  log.fine('Serving interfaces:');
+  shelf_route.printRoutes(router, printer : log.fine);
+
+  return shelf_io.serve(handler, hostname, port);
+}
+
+
+void main ()  {
+
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen(print);
+
+  SupportTools st;
+  handler.Receptionist receptionistHandler;
+  handler.Customer customerHandler;
+
+  void setupHandlers() {
+    receptionistHandler = new handler.Receptionist(ReceptionistPool.instance);
+    customerHandler = new handler.Customer(CustomerPool.instance);
+  }
+
+  SupportTools.instance
+    .then((SupportTools init) => st = init)
+    .then((_) => print(st))
+    .then((_) => setupHandlers())
+    .then((_) => start(receptionistHandler,customerHandler));
+
+}
