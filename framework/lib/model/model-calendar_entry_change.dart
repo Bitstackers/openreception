9d9de0cc7abe565d25466bfe649c26e161dd2b6d
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-06-04 09:28:46 +0200

    Documentation.

--- framework/lib/model/model-calendar_entry_change.dart
+++ framework/lib/model/model-calendar_entry_change.dart
@@ -6,25 +6,41 @@ abstract class CalendarEntryChangeKey {
   static const String username = 'username';
 }
 
+/**
+ * Class representing a historic change, by a [User] in a [CalendarEntry].
+ */
 class CalendarEntryChange {
 
   int userID = User.nullID;
   DateTime changedAt;
   String username;
 
+  /**
+   * Default constructor.
+   */
   CalendarEntryChange();
 
+  /**
+   * Deserializing constructor.
+   */
   CalendarEntryChange.fromMap(Map map) {
     this.userID = map[CalendarEntryChangeKey.userID];
     this.changedAt = Util.unixTimestampToDateTime(map[CalendarEntryChangeKey.updatedAt]);
     this.username = map[CalendarEntryChangeKey.username];
   }
 
+  /**
+   * Returns a map representation of the object.
+   * Suitable for serialization.
+   */
   Map get asMap => {
     CalendarEntryChangeKey.userID : this.userID,
     CalendarEntryChangeKey.updatedAt : Util.dateTimeToUnixTimestamp(changedAt),
     CalendarEntryChangeKey.username : this.username
   };
 
+  /**
+   * Serialization function.
+   */
   Map toJson() => this.asMap;
 }
