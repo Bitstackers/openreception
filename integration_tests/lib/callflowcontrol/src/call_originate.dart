7681e95d39eae7f8d3e817895f95e0c9556a89da
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-06-08 18:53:28 +0200

    Two additional call origination tests; one that checks that the correct error occurs when an agent phone rejects a call, and one that checks what happens when an agent channel is not created in time.

--- integration_tests/lib/callflowcontrol/src/call_originate.dart
+++ integration_tests/lib/callflowcontrol/src/call_originate.dart
@@ -7,33 +7,76 @@ abstract class Originate {
   //static Future  originationToLookedUNumber()
 
   static Future originationToHostedNumber(Receptionist receptionist) {
-
     String receptionNumber = '12340005';
     int contactID = 4;
     int receptionID = 1;
 
-    return receptionist.originate(receptionNumber, contactID, receptionID)
-        .then((_) => receptionist.waitFor(eventType: Event.Key.callOffer)
-          .then((Event.CallOffer event) {
-            //TODO: Assert that callerID is the correct one
-            expect (event.call.inbound, isTrue);
+    return receptionist.originate(receptionNumber, contactID, receptionID).then(
+        (_) => receptionist
+            .waitFor(eventType: Event.Key.callOffer)
+            .then((Event.CallOffer event) {
+      //TODO: Assert that callerID is the correct one
+      expect(event.call.inbound, isTrue);
     }));
   }
-  static void originationToForbiddenNumber(Receptionist receptionist) {
 
+  static Future originationOnAgentCallRejected(Receptionist receptionist) {
+    String originationNumber = '12340005';
+    int contactID = 4;
+    int receptionID = 1;
+
+    Completer callRejectExpectation = new Completer();
+
+    return receptionist.autoAnswer(false).then((_) {
+      /// Asynchronous origination.
+      receptionist
+          .originate(originationNumber, contactID, receptionID)
+          .then((_) => callRejectExpectation.complete())
+          .catchError(callRejectExpectation.completeError);
+    })
+        .then((_) => receptionist.waitForInboundCall())
+        .then((_) => log.info('Receptionist $receptionist rejects the call'))
+        .then((_) => receptionist._phone.hangupAll())
+        .then((_) => expect(callRejectExpectation.future,
+            throwsA(new isInstanceOf<Storage.ClientError>())));
+  }
+
+  static Future originationOnAgentAutoAnswer(Receptionist receptionist) {
+    String originationNumber = '12340005';
+    int contactID = 4;
+    int receptionID = 1;
+
+    Completer callRejectExpectation = new Completer();
+
+    return receptionist.autoAnswer(false).then((_) {
+      /// Asynchronous origination.
+      receptionist
+          .originate(originationNumber, contactID, receptionID)
+          .then((_) => callRejectExpectation.complete())
+          .catchError(callRejectExpectation.completeError);
+    })
+        .then((_) => receptionist.waitForInboundCall())
+        .then((_) => log.info('Receptionist $receptionist ignores the call'))
+        .then((_) => expect(callRejectExpectation.future,
+            throwsA(new isInstanceOf<Storage.ClientError>())));
+  }
+
+  static void originationToForbiddenNumber(Receptionist receptionist) {
     String receptionNumber = 'X';
     int contactID = 4;
     int receptionID = 1;
 
-    return expect(receptionist.originate(receptionNumber, contactID, receptionID),
-          throwsA(new isInstanceOf<Storage.ClientError>()));
+    return expect(
+        receptionist.originate(receptionNumber, contactID, receptionID),
+        throwsA(new isInstanceOf<Storage.ClientError>()));
   }
 
   static Future originationToPeer(Receptionist receptionist, String peerUri) {
     int contactID = 4;
     int receptionID = 1;
 
-    return receptionist.originate(peerUri, contactID, receptionID)
+    return receptionist
+        .originate(peerUri, contactID, receptionID)
         .then((_) => receptionist.waitForInboundCall());
   }
 }
