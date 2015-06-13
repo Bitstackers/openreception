1210db292866828a9f801ee75322988eb7f6fe5e
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-06-13 19:51:37 +0200

    Addtional methods for the organization interface.

--- framework/lib/storage/storage-organization.dart
+++ framework/lib/storage/storage-organization.dart
@@ -2,11 +2,17 @@ part of openreception.storage;
 
 abstract class Organization {
 
+  Future<Iterable<Model.BaseContact>> contacts(int organizationID);
+
+  Future<Model.Organization> create(Model.Organization organization);
+
   Future<Model.Organization> get(int organizationID);
 
   Future<Iterable<Model.Organization>> list();
 
-  Future<Model.Organization> remove(Model.Organization organization);
+  Future<Model.Organization> remove(int organizationID);
 
-  Future<Model.Organization> save(Model.Organization organization);
+  Future<Model.Organization> update(Model.Organization organization);
+
+  Future<Iterable<Model.Reception>> receptions(int organizationID);
 }
