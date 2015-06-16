1dc2b123004d0ae915b9dfd991f9fb55d7f49b8c
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-06-17 00:40:18 +0200

    Storage interface for CDR.

--- framework/lib/storage/storage-cdr.dart
+++ framework/lib/storage/storage-cdr.dart
@@ -2,9 +2,9 @@ part of openreception.storage;
 
 abstract class CDR {
 
-  Future<Iterable<Model.CDREntry>>listEntries();
+  Future<Iterable<Model.CDREntry>>listEntries(DateTime from, DateTime to);
 
   Future<Iterable<Model.CDRCheckpoint>> checkpoints();
 
-  Future<Model.CDRCheckpoint> createCheckpoint();
+  Future<Model.CDRCheckpoint> createCheckpoint(Model.CDRCheckpoint checkpoint);
 }
