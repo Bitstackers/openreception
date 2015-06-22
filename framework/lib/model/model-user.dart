be324a236565b77527b8afeec7cb0689c02ebe72
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-06-22 11:53:16 +0200

    Google email sending credentials for users.

--- framework/lib/model/model-user.dart
+++ framework/lib/model/model-user.dart
@@ -7,6 +7,8 @@ abstract class UserKey {
   static const String identites = 'identites';
   static const String name = 'name';
   static const String extension = 'extension';
+  static const String googleUsername = 'google_username';
+  static const String googleAppcode ='google_appcode';
 }
 
 /**
@@ -21,6 +23,8 @@ class User {
   static const int noID      = 0;
   String           peer;
   String           portrait = '';
+  String           googleUsername = '';
+  String           googleAppcode = '';
 
   /**
    * Constructor for creating an empty object.
@@ -38,6 +42,15 @@ class User {
     name       = userMap[UserKey.name];
     peer       = userMap[UserKey.extension];
 
+    /// Google egmila sending credentials.
+    if (userMap.containsKey(UserKey.googleUsername)) {
+      googleUsername = userMap[UserKey.googleUsername];
+    }
+    if (userMap.containsKey(UserKey.googleAppcode)) {
+      googleAppcode  = userMap[UserKey.googleAppcode];
+    }
+
+    /// Remote attributes from Google account.
     if(userMap.containsKey('remote_attributes')) {
       if((userMap['remote_attributes'] as Map).containsKey('picture')) {
         portrait = userMap['remote_attributes']['picture'];
@@ -56,13 +69,14 @@ class User {
    *
    */
   Map get asMap => {
-    UserKey.id        : ID,
-    UserKey.name      : name,
-    UserKey.address   : address,
-    UserKey.groups    : groups,
-    UserKey.identites : identities,
-    UserKey.extension : peer
-
+    UserKey.id             : ID,
+    UserKey.name           : name,
+    UserKey.address        : address,
+    UserKey.groups         : groups,
+    UserKey.identites      : identities,
+    UserKey.extension      : peer,
+    UserKey.googleUsername : googleUsername,
+    UserKey.googleAppcode  : googleAppcode
   };
 
   Map toJson() => this.asMap;
