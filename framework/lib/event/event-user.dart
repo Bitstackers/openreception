2f08abd7101e387a7a2098862b84edf6f7d22fc1
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-06-23 09:55:15 +0200

    Refined object with named constuctors and nicer toString.

--- framework/lib/event/event-user.dart
+++ framework/lib/event/event-user.dart
@@ -15,16 +15,27 @@ class UserChange implements Event {
   final int userID;
   final String state;
 
-  UserChange (this.userID, this.state) :
+  UserChange._internal (this.userID, this.state) :
     timestamp = new DateTime.now();
 
+  factory UserChange.created (int userID) =>
+    new UserChange._internal(userID, UserObjectState.CREATED);
+
+  factory UserChange.updated (int userID) =>
+    new UserChange._internal(userID, UserObjectState.UPDATED);
+
+  factory UserChange.deleted (int userID) =>
+    new UserChange._internal(userID, UserObjectState.DELETED);
+
   Map toJson() => this.asMap;
-  String toString() => this.asMap.toString();
+
+  @override
+  String toString() => 'UserChange, uid:$userID, state:$state';
 
   Map get asMap {
-    Map template = EventTemplate._rootElement(this);
+    final Map template = EventTemplate._rootElement(this);
 
-    Map body = {Key.userID : userID,
+    final Map body = {Key.userID : userID,
                 Key.state  : state};
 
     template[this.eventName] = body;
