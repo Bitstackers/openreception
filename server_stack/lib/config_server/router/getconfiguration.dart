5fbe5a715c654f272012bd027b78edb346a6ed34
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-06-26 13:43:37 +0200

    Removed deprecated log fields.

--- server_stack/lib/config_server/router/getconfiguration.dart
+++ server_stack/lib/config_server/router/getconfiguration.dart
@@ -14,18 +14,8 @@ final Map client_config =
       "notificationSocket": {
           "interface": config.notificationSocketUri.toString(),
           "reconnectInterval": 2000
-      },
-
-      "serverLog": {
-          "level": "info",
-          "interface": {
-              "critical": "/log/critical",
-              "error": "/log/error",
-              "info": "/log/info"
-          }
       }
   };
-
 
 shelf.Response getBobConfig(shelf.Request request) =>
   new shelf.Response.ok(JSON.encode(client_config));
