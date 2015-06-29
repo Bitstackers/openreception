19eac708b455644c3de53b0a6bcaf2086ba156ba
Thomas Løcke <tl@bitstackers.dk>
2015-06-29 08:13:00 +0200

    List to Iterable and some tightening up

--- framework/lib/model/model-template_email.dart
+++ framework/lib/model/model-template_email.dart
@@ -2,30 +2,32 @@ part of openreception.model;
 
 abstract class Label {
   static const String URGENT = 'Haster';
-
-
 }
 
 class TemplateEmail extends Template {
+  final DateFormat      _dateFormat = new DateFormat("dd-MM-yyyy' kl. 'HH:mm:ss");
+  List<MessageEndpoint> _endpoints;
+  final Message         _message;
 
-  /* The actual dateformat use for the concrete email. */
-  DateFormat dateFormat = new DateFormat("dd-MM-yyyy' kl. 'HH:mm:ss"); // 11-04-2014 kl. 13:37:22
+  /**
+   * Constructor.
+   */
+  TemplateEmail(Message this._message, List<MessageEndpoint> this._endpoints);
 
-  Message               _message;
-  List<MessageEndpoint> endpoints;
+  /**
+   *
+   */
+  String _renderEmailAddress(MessageEndpoint endpoint) =>
+      '"${endpoint.recipient.contactName}" <${endpoint.address}>';
 
-  TemplateEmail (this._message);
-
-  static String _renderEmail (MessageEndpoint endpoint) => '"${endpoint.recipient.contactName}" <${endpoint.address}>';
-
-  static Iterable<MessageEndpoint> _filterRole (List<MessageEndpoint> endpoints, String role)
+  Iterable<MessageEndpoint> _filterRole (List<MessageEndpoint> endpoints, String role)
      => endpoints.where((MessageEndpoint endpoint) => endpoint.recipient.role == role);
 
-  List<String> get toRecipients => _filterRole(this.endpoints, Role.TO).map(_renderEmail).toList();
+  Iterable<String> get toRecipients => _filterRole(_endpoints, Role.TO).map(_renderEmailAddress);
 
-  List<String> get ccRecipients => _filterRole(this.endpoints, Role.CC).map(_renderEmail).toList();
+  Iterable<String> get ccRecipients => _filterRole(_endpoints, Role.CC).map(_renderEmailAddress);
 
-  List<String> get bccRecipients => _filterRole(this.endpoints, Role.BCC).map(_renderEmail).toList();
+  Iterable<String> get bccRecipients => _filterRole(_endpoints, Role.BCC).map(_renderEmailAddress);
 
 
   /**
@@ -39,8 +41,7 @@ class TemplateEmail extends Template {
       '${this._message.urgent ? '(X) ${Label.URGENT}' : ''}';
 
 
-  String _renderTime(DateTime time) =>
-      dateFormat.format(time);
+  String _renderTime(DateTime time) => _dateFormat.format(time);
 
 /**
  * This is the actual "template". It uses a lot of iternal formatting
@@ -66,18 +67,12 @@ ${this._message.sender.name}
 Responsum K/S
 ''';
 
-  Map toJson() {
-    return {
-      Role.TO        : this.toRecipients,
-      Role.CC        : this.ccRecipients,
-
-      Role.BCC       : this.bccRecipients,
-      'message_body' : this._renderedBody,
-      'from'         : this._message.sender.address,
-      'subject'      : this._renderSubject()
-    };
-  }
-
+  Map toJson() => {Role.TO        : this.toRecipients,
+                   Role.CC        : this.ccRecipients,
+                   Role.BCC       : this.bccRecipients,
+                   'message_body' : this._renderedBody,
+                   'from'         : this._message.sender.address,
+                   'subject'      : this._renderSubject()};
 
   /**
    * Renders the email for Dart:mailer. TODO!
