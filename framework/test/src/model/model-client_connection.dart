d752ad73756f28e3348c12e735de23d1dcd975c9
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-07-13 09:27:46 +0200

    Serialization test for client connection model.

--- framework/test/src/model/model-client_connection.dart
+++ framework/test/src/model/model-client_connection.dart
@@ -3,10 +3,20 @@ part of openreception.test;
 void testModelClientConnection() {
   group('Model.ClientConnection', () {
     test('buildObject', ModelClientConnection.buildObject);
+    test('serialization', ModelClientConnection.serialization);
   });
 }
 
 abstract class ModelClientConnection {
+
+  /**
+   * Merely asserts that no exceptions arise.
+   */
+  static void serialization() {
+    Model.ClientConnection builtObject = buildObject();
+
+    expect(() => JSON.encode(builtObject), returnsNormally);
+  }
 
   static Model.ClientConnection buildObject () {
     final int userId = 2;
