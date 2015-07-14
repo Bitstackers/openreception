f4a186a3286085e6322d2007316386031b6e197f
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-07-15 00:14:27 +0200

    Deserialization test of object.

--- framework/test/src/model/model-client_connection.dart
+++ framework/test/src/model/model-client_connection.dart
@@ -4,10 +4,24 @@ void testModelClientConnection() {
   group('Model.ClientConnection', () {
     test('buildObject', ModelClientConnection.buildObject);
     test('serialization', ModelClientConnection.serialization);
+    test('deserialization', ModelClientConnection.deserialization);
   });
 }
 
 abstract class ModelClientConnection {
+
+  static void deserialization() {
+    Model.ClientConnection builtObject = buildObject();
+    String serializedObject = JSON.encode(builtObject);
+    Model.ClientConnection decodedCall =
+        new Model.ClientConnection.fromMap (JSON.decode(serializedObject));
+
+    expect(builtObject.connectionCount, equals(decodedCall.connectionCount));
+    expect(builtObject.userID, equals(decodedCall.userID));
+
+    expect(builtObject.toJson(), equals(decodedCall.toJson()));
+
+  }
 
   /**
    * Merely asserts that no exceptions arise.
