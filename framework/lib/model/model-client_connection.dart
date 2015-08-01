79e0d6a261b7ba1c22bbf1f4e707040a9dc7e437
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-08-01 09:19:12 +0200

    Deprecated the standard constructor in favour of the explicit empty() constructor. Added additional documentation.

--- framework/lib/model/model-client_connection.dart
+++ framework/lib/model/model-client_connection.dart
@@ -1,21 +1,41 @@
 part of openreception.model;
 
+/**
+ * Model class representing a client connection. A client connection object is
+ * an information object that reveals information about how many open
+ * push notification connections a user (of id [userID]) currently has.
+ */
 class ClientConnection {
   int userID;
   int connectionCount;
 
-  ClientConnection();
 
+  @deprecated
+  factory ClientConnection() => new ClientConnection.empty();
+
+  /**
+   * Default emtpy constructor.
+   */
+  ClientConnection.empty();
+
+  /**
+   * Deserializing constructor.
+   */
   ClientConnection.fromMap(Map map) {
     userID = map[_Key.userID];
     connectionCount = map[_Key.connectionCount];
   }
 
+  /**
+   * JSON encoding function.
+   */
   Map toJson() => this.asMap;
 
+  /**
+   * Returns a map representation of this object.
+   */
   Map get asMap => {
     _Key.userID : userID,
     _Key.connectionCount : connectionCount
   };
-
 }
