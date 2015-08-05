8af390c5f2ce4dfd6c7f45dfda7cea0f143e5dc6
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-08-05 13:59:59 +0200

    Copyright headers.

--- framework/lib/common.dart
+++ framework/lib/common.dart
@@ -1,3 +1,16 @@
+/*                  This file is part of OpenReception
+                   Copyright (C) 2014-, BitStackers K/S
+
+  This is free software;  you can redistribute it and/or modify it
+  under terms of the  GNU General Public License  as published by the
+  Free Software  Foundation;  either version 3,  or (at your  option) any
+  later version. This software is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  You should have received a copy of the GNU General Public License along with
+  this program; see the file COPYING3. If not, see http://www.gnu.org/licenses.
+*/
+
 library utilities.common;
 
 void access(String message) => logger.access(message);
