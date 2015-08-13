e911d089352941f90f9b10f78c1fcb8523f18cc2
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-08-13 13:37:59 +0200

    Autoformatting.

--- integration_tests/lib/benchmark/all_tests.dart
+++ integration_tests/lib/benchmark/all_tests.dart
@@ -1,46 +1,43 @@
 part of or_test_fw;
 
-void runBenchmarkTests () {
-  
+void runBenchmarkTests() {
   group('Benchmark', () {
     Set<Receptionist> receptionists;
     Set<Customer> customers;
 
-    setUp (() {
+    setUp(() {
       receptionists = new Set();
-      customers  = new Set();
-      
+      customers = new Set();
+
       while (ReceptionistPool.instance.available.length > 0) {
         receptionists.add(ReceptionistPool.instance.aquire());
       }
-      
+
       while (CustomerPool.instance.available.length > 0) {
         customers.add(CustomerPool.instance.aquire());
       }
-      
-      return Future.wait(receptionists.map
-        ((Receptionist receptionist) => receptionist.initialize()))
-        .then((_) => 
-            Future.wait(customers.map
-          ((Customer customer) => customer.initialize())));
+
+      return Future
+          .wait(receptionists
+              .map((Receptionist receptionist) => receptionist.initialize()))
+          .then((_) => Future.wait(
+              customers.map((Customer customer) => customer.initialize())));
     });
 
-    tearDown (() {
+    tearDown(() {
       receptionists.forEach(((Receptionist receptionist) =>
-        ReceptionistPool.instance.release (receptionist)));
+          ReceptionistPool.instance.release(receptionist)));
 
-      customers.forEach(((Customer customer) =>
-          CustomerPool.instance.release (customer)));
+      customers.forEach(
+          ((Customer customer) => CustomerPool.instance.release(customer)));
 
-      return Future.wait(receptionists.map
-        ((Receptionist receptionist) => receptionist.teardown()))
-        .then((_) => 
-            Future.wait(customers.map
-          ((Customer customer) => customer.teardown())));
+      return Future
+          .wait(receptionists
+              .map((Receptionist receptionist) => receptionist.teardown()))
+          .then((_) => Future
+              .wait(customers.map((Customer customer) => customer.teardown())));
     });
 
-    test('callRush', () =>
-      Benchmark.callRush (receptionists, customers));
+    test('callRush', () => Benchmark.callRush(receptionists, customers));
   });
-  
 }
