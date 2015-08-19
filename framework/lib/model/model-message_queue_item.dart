22e8310998eb1f1408a1cad5eb8818eaaa257043
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-08-19 22:34:15 +0200

    Deprecating and cleaning up.

--- framework/lib/model/model-message_queue_item.dart
+++ framework/lib/model/model-message_queue_item.dart
@@ -17,47 +17,29 @@ class MessageQueueItem {
   int ID;
   int tries = 0;
   int messageID = Message.noID;
-  List<MessageEndpoint> unhandledEndpoints = [];
+  List<MessageRecipient> handledRecipients = [];
+  List<MessageRecipient> unhandledRecipients = [];
 
   /**
    * Default constructor.
    */
+  @deprecated
   MessageQueueItem();
 
   /**
+   * Default empty constructor.
+   */
+  MessageQueueItem.empty();
+
+  /**
    * Creates a message from the information given in [map].
-   * The expected map format is:
-   *     {
-   *       int       queue_id,
-   *       int       message_id,
-   *       int       tries,
-   *       List<Map> unhandledEndpoints
-   *     }
    */
   MessageQueueItem.fromMap(Map map) {
-    this.ID = map['id'];
-    this.messageID = map['message_id'];
-    this.tries = map['tries'];
-    if (tries > 0) {
-      this.unhandledEndpoints.addAll(map['unhandled_endpoints']
-          .map((Map endpointMap) => new MessageEndpoint.fromMap(endpointMap)));
-    }
+    throw new UnimplementedError();
   }
 
   /**
    * Serialization function
    */
-  Map toJson() => {'id': ID, 'message_id': messageID, 'tries': tries};
-
-  /**
-   * Persistenly stores the message in the [messageStore] passed.
-   */
-  Future save(Storage.MessageQueue messageQeueStore) =>
-      messageQeueStore.save(this);
-
-  /**
-   * Persistenly archives the message from the [messageStore] passed.
-   */
-  Future archive(Storage.MessageQueue messageQeueStore) =>
-      messageQeueStore.archive(this);
+  Map toJson() => throw new UnimplementedError();
 }
