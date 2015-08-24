1a513527d13d99d7bea621b07116caae4fc1f8a0
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-08-24 14:45:35 +0200

    Endpoint controller.

--- management_client/web/lib/controller.dart
+++ management_client/web/lib/controller.dart
@@ -7,6 +7,8 @@ import 'package:openreception_framework/model.dart' as ORModel;
 part 'controller/controller-calendar.dart';
 part 'controller/controller-cdr.dart';
 part 'controller/controller-contact.dart';
+part 'controller/controller-distribution_list.dart';
+part 'controller/controller-endpoint.dart';
 part 'controller/controller-organization.dart';
 part 'controller/controller-reception.dart';
 part 'controller/controller-user.dart';
---
+++ management_client/web/lib/controller/controller-endpoint.dart
@@ -0,0 +1,19 @@
+part of openreception.managementclient.controller;
+
+class Endpoint {
+  final ORService.RESTEndpointStore _service;
+
+  Endpoint(this._service);
+
+  Future<Iterable<ORModel.MessageEndpoint>> list(int receptionId, int contactId) =>
+      _service.list(receptionId, contactId);
+
+  Future<ORModel.MessageEndpoint> create(int receptionId, int contactId, ORModel.MessageEndpoint ep) =>
+      _service.create(receptionId, contactId, ep);
+
+  Future remove(int endpointId) =>
+      _service.remove(endpointId);
+
+  Future<ORModel.MessageEndpoint> update(ORModel.MessageEndpoint ep) =>
+      _service.update(ep);
+}
