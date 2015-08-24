98300955d54e0e446b2c1cf3ee258794a86010f1
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-08-24 12:39:16 +0200

    Extra controllers.

--- management_client/web/lib/controller/controller-contact.dart
+++ management_client/web/lib/controller/controller-contact.dart
@@ -18,6 +18,15 @@ class Contact {
 
   Future<ORModel.BaseContact> get(int contactID) => _service.get(contactID);
 
+  Future<ORModel.BaseContact> update(ORModel.BaseContact contact) =>
+      _service.update(contact);
+
+  Future<ORModel.BaseContact> create(ORModel.BaseContact contact) =>
+      _service.create(contact);
+
+  Future remove(int contactId) =>
+      _service.remove(contactId);
+
   Future<Iterable<ORModel.MessageEndpoint>> endpoints(
           int contactID, int receptionID) =>
       _service.endpoints(contactID, receptionID);
@@ -25,6 +34,22 @@ class Contact {
   Future<Iterable<int>> receptions(int contactID) =>
       _service.receptions(contactID);
 
-  Future<Iterable<ORModel.BaseContact>> colleagues(int contactId) =>
-      _service.colleagues(contactId);
+  Future<ORModel.Contact> addToReception(ORModel.Contact contact, int receptionId) =>
+      _service.addToReception(contact, receptionId);
+
+  Future removeFromReception(int contactId, int receptionId) =>
+      _service.removeFromReception(contactId, receptionId);
+
+  Future<ORModel.Contact> updateInReception(ORModel.Contact contact) =>
+      _service.updateInReception(contact);
+
+  Future<Iterable<ORModel.Contact>> colleagues(int contactId) {
+    List<ORModel.Contact> foundColleagues = [];
+
+    return _service.receptions(contactId)
+       .then((Iterable<int> receptionIds) =>
+        Future.forEach(receptionIds, (int receptionId) =>
+          _service.listByReception(receptionId)
+            .then(foundColleagues.addAll))).then((_) => foundColleagues);
+  }
 }
