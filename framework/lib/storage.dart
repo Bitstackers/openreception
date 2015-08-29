8a90ee76c4aa52860cc01003a16912e30907ad26
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-08-29 10:38:37 +0200

    Calendar storage interface #16.

--- framework/lib/storage.dart
+++ framework/lib/storage.dart
@@ -3,6 +3,7 @@ library openreception.storage;
 import 'dart:async';
 import 'model.dart' as Model;
 
+part 'storage/storage-calendar.dart';
 part 'storage/storage-cdr.dart';
 part 'storage/storage-contact.dart';
 part 'storage/storage-distribution_list.dart';
