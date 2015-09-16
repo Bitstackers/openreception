0a8e440c3ba77e9eca87c09051732a44681c1d28
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-09-16 20:16:21 +0200

    Rewrote the CallRush test to - hopefully - make it more reliable.

--- integration_tests/lib/benchmark/src/tests.dart
+++ integration_tests/lib/benchmark/src/tests.dart
@@ -13,31 +13,41 @@ abstract class Benchmark {
 
     return r.callFlowControl.callList().then((Iterable<Model.Call> calls) {
       if (calls.isEmpty) {
+        log.info('No more calls. Aborting.');
         return false;
       }
 
       Model.Call nextCall = calls.firstWhere(callAvailable, orElse: () => null);
 
       log.info('$r found $nextCall available');
-      log.info('${calls.map((var call) => call.toJson())}');
 
       if (nextCall == null) {
         return new Future.delayed(new Duration(milliseconds: 100),
             () => _receptionistRequestsCall(r));
       } else {
+        log.info('$r picks up $nextCall');
         return r
             .pickup(nextCall)
+            .then((_) => log.info('$r got $nextCall, hangin it up after 100ms'))
             .then((_) => new Future.delayed(
                 new Duration(milliseconds: 100), () => r.hangUp(nextCall)))
             .catchError((error, stackTrace) {
           if (error is Storage.Conflict) {
-            log.info('$nextCall is already assigned, trying the next one.');
+            log.info('$nextCall is locked, trying again in 100ms.');
             return new Future.delayed(
-                new Duration(milliseconds: 100), () => r.hangUp(nextCall));
+                   new Duration(milliseconds: 100), () => _receptionistRequestsCall(r));
+
           } else if (error is Storage.NotFound) {
+            log.info('$nextCall is already assigned, trying the next one.');
+            return _receptionistRequestsCall(r);
+
+          } else if (error is Storage.Forbidden) {
             log.info('$nextCall is hung up, trying the next one.');
-            return new Future.delayed(
-                new Duration(milliseconds: 100), () => r.hangUp(nextCall));
+            return _receptionistRequestsCall(r);
+
+          } else {
+            log.shout(error, stackTrace);
+            return _receptionistRequestsCall(r);
           }
         });
       }
@@ -53,33 +63,35 @@ abstract class Benchmark {
       Iterable<Receptionist> receptionists, Iterable<Customer> customers) {
     Receptionist callWaiter = receptionists.first;
 
+
+    Future waitForListToEmpty() =>
+      Future.doWhile((() => new Future.delayed(
+        new Duration(milliseconds: 1000), () =>
+            callWaiter.callFlowControl
+              .callList()
+              .then((Iterable<Model.Call> calls) => calls.length != 0))));
+
+
+
     // Each customer spawns a call
-    return Future.forEach(customers, (Customer customer) {
-      return customer.dial('12340003');
-    }).then((_) {
+    return Future.wait
+      (customers.map((Customer customer) => customer.dial('12340003')))
+    .then((_) {
       log.info('Waiting for call list to fill');
-      return Future.doWhile((() => new Future.delayed(
-          new Duration(milliseconds: 10), () => callWaiter.callFlowControl
+      return Future.doWhile((() =>
+       new Future.delayed(
+          new Duration(milliseconds: 100), () => callWaiter.callFlowControl
               .callList()
               .then((Iterable<Model.Call> calls) {
         return calls.length != customers.length;
       }))));
-    }).then((_) {
-      return Future.forEach(receptionists.take(customers.length),
-          (Receptionist r) {
-        log.info('$r finds next call');
-
-        return _receptionistRequestsCall(r);
-      });
     })
-        .then((_) => receptionists.first.callFlowControl
-            .callList()
-            .then((Iterable<Model.Call> calls) {
+    .then((_) => log.info('Call list filled, starting to handle the calls'))
 
-      return new Future.delayed(new Duration(milliseconds : 100), () {
-        expect(calls, isEmpty);
-      });
-
-    })).then((_) => log.info('Test done'));
+    .then((_) => Future.wait(receptionists.map((Receptionist r) =>
+        _receptionistRequestsCall(r))))
+    .then((_) => log.info('Wait for call list to empty'))
+    .then((_) => waitForListToEmpty().timeout(new Duration(seconds: 10)))
+    .then((_) => log.info('Test done'));
   }
 }
