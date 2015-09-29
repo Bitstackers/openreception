f6afe74c26c0f29c90bf02185dd56949d69c43aa
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-09-29 11:22:34 +0200

    Increasing the timeout for receiving a reply from the PBX.

--- server_stack/lib/callflowcontrol/model/model-pbx_client.dart
+++ server_stack/lib/callflowcontrol/model/model-pbx_client.dart
@@ -20,7 +20,8 @@ abstract class PBXClient {
   static ESL.Connection instance = null;
 
   static Future<ESL.Response> api (String command) {
-    return instance.api(command).then((ESL.Response response) {
+    return instance.api(command, timeoutSeconds: 20)
+        .then((ESL.Response response) {
       log.finest('api $command => ${response.rawBody}');
       return response;
     });
