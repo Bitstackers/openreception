ce69ff69cd1752a3f617d7b06898c3c625e4b844
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-10-28 21:59:47 +0100

    Resource for fetching the extension of a reception identified by id.

--- framework/lib/resource/resource-reception.dart
+++ framework/lib/resource/resource-reception.dart
@@ -24,6 +24,9 @@ abstract class Reception {
   static Uri single(Uri host, int receptionID) =>
     Uri.parse('${root(host)}/${receptionID}');
 
+  static Uri extensionOf(Uri host, int receptionID) =>
+    Uri.parse('${root(host)}/${receptionID}/extension');
+
   static Uri byExtension(Uri host, String extension) =>
     Uri.parse('${root(host)}/extension/${extension}');
 
