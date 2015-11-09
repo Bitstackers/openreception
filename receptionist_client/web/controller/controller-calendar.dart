fed0b4c3c84b3fcd647c00271a11f3987614bfbe
Thomas Løcke <tl@bitstackers.dk>
2015-11-09 15:18:32 +0100

    Added comments and re-arranged a few properties

--- receptionist_client/web/controller/controller-calendar.dart
+++ receptionist_client/web/controller/controller-calendar.dart
@@ -13,6 +13,9 @@
 
 part of controller;
 
+/**
+ * Exposes methods for calendar CRUD operations.
+ */
 class Calendar {
   final ORService.RESTCalendarStore _calendarStore;
 
@@ -22,51 +25,43 @@ class Calendar {
   Calendar(this._calendarStore);
 
   /**
-   * Return the latest entry change information for the [entryId]
-   * calendar entry.
+   * Return the latest entry change information for the [entryId] calendar entry.
    */
-  Future<Iterable<ORModel.CalendarEntryChange>> calendarEntryChanges
-    (ORModel.CalendarEntry entry) => _calendarStore.changes(entry.ID);
+  Future<Iterable<ORModel.CalendarEntryChange>> calendarEntryChanges(ORModel.CalendarEntry entry) =>
+      _calendarStore.changes(entry.ID);
 
   /**
-   * Return the latest entry change information for the [entryId]
-   * calendar entry.
+   * Return the latest entry change information for the [entryId] calendar entry.
    */
-  Future<ORModel.CalendarEntryChange> calendarEntryLatestChange
-    (ORModel.CalendarEntry entry) => _calendarStore.latestChange(entry.ID);
+  Future<ORModel.CalendarEntryChange> calendarEntryLatestChange(ORModel.CalendarEntry entry) =>
+      _calendarStore.latestChange(entry.ID);
 
   /**
    * Save [entry] to the database.
    */
-  Future createCalendarEvent(ORModel.CalendarEntry entry) =>
-      _calendarStore.create(entry);
+  Future createCalendarEvent(ORModel.CalendarEntry entry) => _calendarStore.create(entry);
 
   /**
    * Delete [entry] from the database.
    */
-  Future deleteCalendarEvent(ORModel.CalendarEntry entry) =>
-      _calendarStore.removeEntry(entry);
+  Future deleteCalendarEvent(ORModel.CalendarEntry entry) => _calendarStore.removeEntry(entry);
 
   /**
-   * Return all the [ORModel.CalendarEntry]'s of a [reception].
+   * Return all the [ORModel.CalendarEntry]s of a [reception].
    */
-  Future<Iterable<ORModel.CalendarEntry>> receptionCalendar
-    (ORModel.Reception reception) =>
-        _calendarStore.list(new ORModel.Owner.reception(reception.ID));
+  Future<Iterable<ORModel.CalendarEntry>> receptionCalendar(ORModel.Reception reception) =>
+      _calendarStore.list(new ORModel.Owner.reception(reception.ID));
 
   /**
-   * Return all the [contact] [ORModel.CalendarEntry]'s.
+   * Return all the [contact] [ORModel.CalendarEntry]s.
    */
-  Future<Iterable<ORModel.CalendarEntry>> contactCalendar
-    (ORModel.Contact contact) =>
-        _calendarStore.list
-          (new ORModel.Owner.contact(contact.ID, contact.receptionID));
+  Future<Iterable<ORModel.CalendarEntry>> contactCalendar(ORModel.Contact contact) =>
+      _calendarStore.list(new ORModel.Owner.contact(contact.ID, contact.receptionID));
 
   /**
    * Save [entry] to the database.
    */
-  Future saveCalendarEvent(ORModel.CalendarEntry entry) =>
-      entry.ID == ORModel.CalendarEntry.noID
+  Future saveCalendarEvent(ORModel.CalendarEntry entry) => entry.ID == ORModel.CalendarEntry.noID
       ? createCalendarEvent(entry)
       : _calendarStore.update(entry);
 }
