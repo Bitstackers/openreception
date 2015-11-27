35b1e7741da18249c9a50e1e4d7430f29ae86bd7
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-11-27 10:27:35 +0100

    HourAction model tests.

--- framework/test/src/model/dialplan/model-hour_action.dart
+++ framework/test/src/model/dialplan/model-hour_action.dart
@@ -13,3 +13,86 @@
 
 part of openreception.test;
 
+/**
+ *
+ */
+void testModelHourAction() {
+  group('Model.HourAction', () {
+    test('deserialization', HourAction.deserialization);
+
+    test('serialization', HourAction.serialization);
+
+    test('buildObject', HourAction.buildObject);
+    test('parse', HourAction.parse);
+  });
+}
+
+/**
+ *
+ */
+abstract class HourAction {
+  /**
+   *
+   */
+  static void serialization() {
+    Model.HourAction builtObject = buildObject();
+    String serializedObject = JSON.encode(builtObject);
+
+    expect(serializedObject, isNotNull);
+    expect(serializedObject, isNotEmpty);
+  }
+
+  /**
+   *
+   */
+  static void deserialization() {
+    Model.HourAction builtObject = buildObject();
+
+    Model.HourAction deserializedObject =
+        Model.HourAction.parse(JSON.decode(JSON.encode(builtObject)));
+
+    expect(builtObject.hours, equals(deserializedObject.hours));
+    expect(builtObject.actions, equals(deserializedObject.actions));
+    expect(builtObject.toString(), isNotEmpty);
+  }
+
+  /**
+   *
+   */
+  static Model.HourAction buildObject() {
+
+    print(JSON.encode(new Model.ReceptionDialplan()));
+
+    final List<Model.OpeningHour> openHours = [
+      new Model.OpeningHour.empty()
+        ..fromDay = Model.WeekDay.mon
+        ..fromHour = 8
+        ..fromMinute = 30
+        ..toDay = Model.WeekDay.thur
+        ..toHour = 17,
+      new Model.OpeningHour.empty()
+        ..fromDay = Model.WeekDay.fri
+        ..fromHour = 8
+        ..fromMinute = 30
+        ..toDay = Model.WeekDay.fri
+        ..toHour = 16
+    ];
+    final List<Model.Action> actions = [ModelPlayback.buildObject()];
+
+    Model.HourAction builtObject = new Model.HourAction()
+      ..hours = openHours
+      ..actions = actions;
+
+    expect(builtObject.hours, equals(openHours));
+    expect(builtObject.actions, equals(actions));
+    expect(builtObject.toString(), isNotEmpty);
+
+    return builtObject;
+  }
+
+  /**
+   *
+   */
+  static void parse() {
+  }
+}
