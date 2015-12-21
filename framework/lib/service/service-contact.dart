9060fafa845b3790c0ce2288acc92ca13694df05
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-12-21 10:55:53 +0100

    Removing deprecated calender API. For a replacement, see service.calendar.

--- framework/lib/service/service-contact.dart
+++ framework/lib/service/service-contact.dart
@@ -26,21 +26,6 @@ class RESTContactStore implements Storage.Contact {
 
   RESTContactStore(Uri this._host, String this._token, this._backend);
 
-  Future<Iterable<Map>> calendarMap(int contactID, int receptionID) {
-    Uri url = Resource.Contact.calendar(this._host, contactID, receptionID);
-    url = appendToken(url, this._token);
-
-    return this._backend.get(url).then((String response) {
-      var decodedData = JSON.decode(response);
-
-      if (decodedData is Map) {
-        return (JSON.decode(response)['CalendarEvents'] as Iterable);
-      } else {
-        return (JSON.decode(response) as Iterable);
-      }
-    });
-  }
-
   Future<Iterable<int>> receptions(int contactID) {
     Uri url = Resource.Contact.receptions(this._host, contactID);
     url = appendToken(url, this._token);
@@ -123,62 +108,6 @@ class RESTContactStore implements Storage.Contact {
     return this._backend.get(url).then(
         (String response) => (JSON.decode(response) as Iterable)
             .map((Map map) => new Model.Contact.fromMap(map)));
-  }
-
-  @deprecated
-  Future<Iterable<Model.CalendarEntry>> calendar(
-      int contactID, int receptionID) => calendarMap(contactID, receptionID)
-      .then((Iterable<Map> maps) =>
-          maps.map((Map map) => new Model.CalendarEntry.fromMap(map)));
-
-  Future<Model.CalendarEntry> calendarEvent(
-      int receptionID, int contactID, int eventID) {
-    Uri url = Resource.Contact.calendarEvent(
-        this._host, contactID, receptionID, eventID);
-    url = appendToken(url, this._token);
-
-    return this._backend.get(url).then((String response) {
-      Map responseMap = null;
-      try {
-        responseMap = JSON.decode(response);
-      } catch (error) {
-        return new Future.error(new ArgumentError(
-            'Failed to response parse as JSON string : `$response`'));
-      }
-
-      return new Model.CalendarEntry.fromMap(responseMap);
-    });
-  }
-
-  @deprecated
-  Future<Model.CalendarEntry> calendarEventCreate(Model.CalendarEntry event) {
-    Uri url = Resource.Contact.calendar(
-        this._host, event.contactID, event.receptionID);
-    url = appendToken(url, this._token);
-
-    String data = JSON.encode(event);
-    return this._backend.post(url, data).then((String response) =>
-        new Model.CalendarEntry.fromMap(JSON.decode(response)));
-  }
-
-  @deprecated
-  Future<Model.CalendarEntry> calendarEventUpdate(Model.CalendarEntry event) {
-    Uri url = Resource.Contact.calendarEvent(
-        this._host, event.contactID, event.receptionID, event.ID);
-    url = appendToken(url, this._token);
-
-    String data = JSON.encode(event);
-    return this._backend.put(url, data).then((String response) =>
-        new Model.CalendarEntry.fromMap(JSON.decode(response)));
-  }
-
-  @deprecated
-  Future calendarEventRemove(Model.CalendarEntry event) {
-    Uri url = Resource.Contact.calendarEvent(
-        this._host, event.contactID, event.receptionID, event.ID);
-    url = appendToken(url, this._token);
-
-    return this._backend.delete(url);
   }
 
   Future<Iterable<Map>> endpointsMap(int contactID, int receptionID) {
