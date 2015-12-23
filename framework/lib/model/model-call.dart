d4fc89f08c587c8f95dcc95fa0da709e7a31bcc0
Thomas Løcke <tl@bitstackers.dk>
2015-12-23 12:47:32 +0100

    Removed deprecated PbxKeys and started using the new or:: namespace

--- framework/lib/model/model-call.dart
+++ framework/lib/model/model-call.dart
@@ -17,16 +17,16 @@ part of openreception.model;
  * Enumeration type for call states.
  */
 abstract class CallState {
-   static const String Unknown      = 'UNKNOWN';
-   static const String Created      = 'CREATED';
-   static const String Ringing      = 'RINGING';
-   static const String Queued       = 'QUEUED';
-   static const String Unparked     = 'UNPARKED';
-   static const String Hungup       = 'HUNGUP';
-   static const String Transferring = 'TRANSFERRING';
-   static const String Transferred  = 'TRANSFERRED';
-   static const String Speaking     = 'SPEAKING';
-   static const String Parked       = 'PARKED';
+  static const String Unknown = 'UNKNOWN';
+  static const String Created = 'CREATED';
+  static const String Ringing = 'RINGING';
+  static const String Queued = 'QUEUED';
+  static const String Unparked = 'UNPARKED';
+  static const String Hungup = 'HUNGUP';
+  static const String Transferring = 'TRANSFERRING';
+  static const String Transferred = 'TRANSFERRED';
+  static const String Speaking = 'SPEAKING';
+  static const String Parked = 'PARKED';
 }
 
 /**
@@ -34,30 +34,29 @@ abstract class CallState {
  * unique id (uuid) that may be used as a handle for REST service method calls.
  */
 class Call {
+  static const String className = '${libraryName}.Call';
+  static final Logger _log = new Logger(Call.className);
 
-  static const String    className       = '${libraryName}.Call';
-  static final Logger    _log            = new Logger (Call.className);
+  static final Call noCall = new Call.empty(noID);
+  static final String noID = '';
 
-  static final Call      noCall          = new Call.empty(noID);
-  static final String    noID            = '';
+  final Bus<String> _callState = new Bus<String>();
+  final Bus<Event.CallEvent> _eventBus = new Bus<Event.CallEvent>();
 
-  final Bus<String>          _callState  = new Bus<String>();
-  final Bus<Event.CallEvent> _eventBus   = new Bus<Event.CallEvent>();
-
-  DateTime               arrived         = new DateTime.now();
-  DateTime               answeredAt      = Util.never;
-  int                    assignedTo      = User.noID;
-  String                 b_Leg           = null;
-  String                 callerID        = '';
-  int                    contactID       = Contact.noID;
-  String                 destination     = '';
-  bool                   greetingPlayed  = false;
-  String                 _ID             = noID;
-  bool                   inbound         = null;
-  bool                   _locked         = false;
-  int                    receptionID     = Reception.noID;
-  String                 _state          = CallState.Unknown;
-  String                 hangupCause     = '';
+  DateTime arrived = new DateTime.now();
+  DateTime answeredAt = Util.never;
+  int assignedTo = User.noID;
+  String b_Leg = null;
+  String callerID = '';
+  int contactID = Contact.noID;
+  String destination = '';
+  bool greetingPlayed = false;
+  String _ID = noID;
+  bool inbound = null;
+  bool _locked = false;
+  int receptionID = Reception.noID;
+  String _state = CallState.Unknown;
+  String hangupCause = '';
 
   /**
    * Constructor.
@@ -68,25 +67,24 @@ class Call {
    * Constructor.
    */
   factory Call.fromMap(Map map) => new Call.empty(map[Key.ID])
-    .._state         = map[Key.state]
-    ..answeredAt     = Util.unixTimestampToDateTime (map[Key.answeredAt])
-    ..b_Leg          = map[Key.bLeg]
-    .._locked        = map[Key.locked]
-    ..inbound        = map[Key.inbound]
-    ..destination    = map[Key.destination]
-    ..callerID       = map[Key.callerID]
+    .._state = map[PbxKey.state]
+    ..answeredAt = Util.unixTimestampToDateTime(map[Key.answeredAt])
+    ..b_Leg = map[Key.bLeg]
+    .._locked = map[Key.locked]
+    ..inbound = map[Key.inbound]
+    ..destination = map[Key.destination]
+    ..callerID = map[Key.callerID]
     ..greetingPlayed = map[Key.greetingPlayed]
-    ..receptionID    = map[Key.receptionID]
-    ..contactID      = map[Key.contactID]
-    ..assignedTo     = map[Key.assignedTo]
-    ..arrived        = Util.unixTimestampToDateTime (map[Key.arrivalTime]);
-
+    ..receptionID = map[Key.receptionID]
+    ..contactID = map[Key.contactID]
+    ..assignedTo = map[Key.assignedTo]
+    ..arrived = Util.unixTimestampToDateTime(map[Key.arrivalTime]);
 
   /**
    *
    */
   @override
-  operator == (Call other) => _ID == other._ID;
+  operator ==(Call other) => _ID == other._ID;
 
   /**
    *
@@ -110,51 +108,51 @@ class Call {
 
     _log.finest('UUID: ${_ID}: ${lastState} => ${newState}');
 
-    if(lastState == CallState.Queued) {
+    if (lastState == CallState.Queued) {
       notifyEvent(new Event.QueueLeave(this));
-    } else if(lastState == CallState.Parked) {
+    } else if (lastState == CallState.Parked) {
       notifyEvent(new Event.CallUnpark(this));
     }
 
-    switch(newState) {
-      case(CallState.Created):
+    switch (newState) {
+      case (CallState.Created):
         notifyEvent(new Event.CallOffer(this));
         break;
 
-      case(CallState.Parked):
+      case (CallState.Parked):
         notifyEvent(new Event.CallPark(this));
         break;
 
-      case(CallState.Unparked):
+      case (CallState.Unparked):
         notifyEvent(new Event.CallUnpark(this));
         break;
 
-      case(CallState.Queued):
+      case (CallState.Queued):
         notifyEvent(new Event.QueueJoin(this));
         break;
 
-      case(CallState.Hungup):
-        notifyEvent (new Event.CallHangup(this, hangupCause: hangupCause));
+      case (CallState.Hungup):
+        notifyEvent(new Event.CallHangup(this, hangupCause: hangupCause));
         break;
 
-      case(CallState.Speaking):
+      case (CallState.Speaking):
         notifyEvent(new Event.CallPickup(this));
         break;
 
-      case(CallState.Transferred):
+      case (CallState.Transferred):
         notifyEvent(new Event.CallTransfer(this));
         break;
 
-      case(CallState.Ringing):
+      case (CallState.Ringing):
         notifyEvent(new Event.CallStateChanged(this));
         break;
 
-      case(CallState.Transferring):
-         break;
+      case (CallState.Transferring):
+        break;
 
       default:
         _log.severe('Changing call ${this} to Unkown state!');
-      break;
+        break;
     }
   }
 
@@ -172,7 +170,7 @@ class Call {
   /**
    *
    */
-  String get state  => _state;
+  String get state => _state;
 
   /**
    *
@@ -202,9 +200,9 @@ class Call {
    *
    */
   void link(Call other) {
-    if(locked) locked = false;
+    if (locked) locked = false;
 
-    b_Leg  = other._ID;
+    b_Leg = other._ID;
     other.b_Leg = _ID;
   }
 
@@ -216,12 +214,12 @@ class Call {
   /**
    *
    */
-  void set locked(bool lock)   {
+  void set locked(bool lock) {
     _locked = lock;
 
-    if(lock) {
+    if (lock) {
       notifyEvent(new Event.CallLock((this)));
-    }else {
+    } else {
       notifyEvent(new Event.CallUnlock(this));
     }
   }
@@ -243,33 +241,33 @@ class Call {
    */
   @override
   String toString() =>
-      this == noCall
-      ? 'no Call'
-      : 'Call ID: ${_ID}, state: ${_state}, destination: ${destination}';
+      this == noCall ? 'no Call' : 'Call ID: ${_ID}, state: ${_state}, destination: ${destination}';
 
   /**
    *
    */
-  Map toJson() => {Key.ID             : _ID,
-                   Key.state          : _state,
-                   Key.bLeg           : b_Leg,
-                   Key.locked         : locked,
-                   Key.inbound        : inbound,
-                   Key.destination    : destination,
-                   Key.callerID       : callerID,
-                   Key.greetingPlayed : greetingPlayed,
-                   Key.receptionID    : receptionID,
-                   Key.contactID      : contactID,
-                   Key.assignedTo     : assignedTo,
-                   Key.channel        : channel,
-                   Key.arrivalTime    : Util.dateTimeToUnixTimestamp(arrived),
-                   Key.answeredAt     : Util.dateTimeToUnixTimestamp(answeredAt)};
+  Map toJson() => {
+        Key.ID: _ID,
+        PbxKey.state: _state,
+        Key.bLeg: b_Leg,
+        Key.locked: locked,
+        Key.inbound: inbound,
+        Key.destination: destination,
+        Key.callerID: callerID,
+        Key.greetingPlayed: greetingPlayed,
+        Key.receptionID: receptionID,
+        Key.contactID: contactID,
+        Key.assignedTo: assignedTo,
+        Key.channel: channel,
+        Key.arrivalTime: Util.dateTimeToUnixTimestamp(arrived),
+        Key.answeredAt: Util.dateTimeToUnixTimestamp(answeredAt)
+      };
 
   /**
    *
    */
   static void validateID(String callID) {
-    if(callID == null || callID.isEmpty) {
+    if (callID == null || callID.isEmpty) {
       throw new FormatException('Invalid Call ID: ${callID}');
     }
   }
