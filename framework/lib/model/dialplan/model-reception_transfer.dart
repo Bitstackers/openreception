eb94413ef2e4babd455d52425b03c1d286893127
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-12-23 18:46:07 +0100

    Initial Reception Transfer class.

---
+++ framework/lib/model/dialplan/model-reception_transfer.dart
@@ -0,0 +1,57 @@
+/*                  This file is part of OpenReception
+                   Copyright (C) 2015-, BitStackers K/S
+
+  This is free software;  you can redistribute it and/or modify it
+  under terms of the  GNU General Public License  as published by the
+  Free Software  Foundation;  either version 3,  or (at your  option) any
+  later version. This software is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  You should have received a copy of the GNU General Public License along with
+  this program; see the file COPYING3. If not, see http://www.gnu.org/licenses.
+*/
+
+part of openreception.model.dialplan;
+
+/**
+ * ReceptionTransfer action.
+ * TODO: Finish this!
+ */
+class ReceptionTransfer extends Action {
+  final String extension;
+  final String note;
+
+  /**
+   * Default constructor.
+   */
+  const ReceptionTransfer(this.extension, {this.note: ''});
+
+  static Transfer parse(String buffer) {
+    /// Remove leading spaces.
+    buffer = buffer.trimLeft();
+
+    String extension;
+    String note = '';
+
+    buffer = consumeKey(buffer, Key.reception).trimLeft();
+
+    int openBracket = buffer.indexOf('(');
+
+    if (openBracket == -1) {
+      extension = buffer.trim();
+    } else {
+      extension = buffer.substring(0, openBracket).trimRight();
+
+      int closeBracket =
+          buffer.indexOf(')') > 0 ? buffer.indexOf(')') : buffer.length;
+      note = buffer.substring(openBracket + 1, closeBracket);
+    }
+
+    return new Transfer(extension, note: note);
+  }
+
+  String toString() => 'Omstil til $extension ($note)';
+
+  String toJson() => '${Key.reception} $extension'
+      '${note.isNotEmpty ? ' ($note)': ''}';
+}
