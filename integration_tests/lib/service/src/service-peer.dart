2053442820c1c64f4348eb7586c27927b306855d
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-12-28 15:16:01 +0100

    Refined the peer event tests - once again.

--- integration_tests/lib/service/src/service-peer.dart
+++ integration_tests/lib/service/src/service-peer.dart
@@ -1,55 +1,55 @@
 part of or_test_fw;
 
 abstract class Peer {
-
-  static Logger log = new Logger ('Test.Peer');
+  static Logger log = new Logger('Test.Peer');
 
   /**
    * Test for the presence of hangup events when a peer
    * changes registration status.
    */
-  static Future eventPresence(Receptionist receptionist) {
+  static Future eventPresence(Receptionist receptionist) async {
     String peerName = receptionist._phone.defaultAccount.username;
 
-    return
-      new Future.delayed(new Duration(milliseconds : 50))
-      .then((_) => receptionist.eventStack.clear())
-      .then((_) => log.info ('Unregistering peer $peerName to assert state'))
-      .then((_) => receptionist._phone.unregister())
-      .then((_) =>
-          receptionist.waitFor(eventType: Event.Key.peerState)
-          .then((_) {
-            log.info ('Flushing event stack');
-            receptionist.eventStack.clear();
-          }))
-      .then((_) => new Future.delayed(new Duration(milliseconds : 50)))
-      .then((_) => log.info ('Registering peer $peerName'))
-      .then((_) => receptionist._phone.register())
-      .then((_) => log.info ('Waiting for peer state event'))
-      .then((_) =>
-          receptionist.waitFor(eventType: Event.Key.peerState)
-          .then((Event.PeerState peerStateEvent) {
-            log.info ('Got event ${peerStateEvent.asMap}');
-            expect (peerStateEvent.peer.registered, isTrue);
-            expect (peerStateEvent.peer.ID, equals(peerName));
-          }))
-      .then((_) => new Future.delayed(new Duration(milliseconds : 50)))
-      .then((_) => log.info ('Flushing event stack'))
-      .then((_) => receptionist.eventStack.clear())
-      .then((_) => log.info ('Unregistering peer $peerName to complete cycle'))
-      .then((_) => receptionist._phone.unregister())
-      .then((_) => log.info ('Waiting for peer state event'))
-      .then((_) =>
-          receptionist.waitFor(eventType: Event.Key.peerState)
-          .then((Event.PeerState peerStateEvent) {
-            log.info ('Got event ${peerStateEvent.asMap}');
-            expect (peerStateEvent.peer.registered, isFalse);
-            expect (peerStateEvent.peer.ID, equals(peerName));
-          }))
-      .then((_) => log.info ('Test done.'));
+    log.info('Unregistering peer $peerName to assert state');
+
+    Future unregisterEvent = receptionist.notificationSocket.eventStream
+        .firstWhere((Event.Event e) =>
+            e is Event.PeerState && e.peer.ID == peerName && !e.peer.registered)
+        .timeout(new Duration(seconds: 10));
+
+    await receptionist._phone.unregister();
+    await unregisterEvent;
+
+    log.info('Flushing event stack');
+    receptionist.eventStack.clear();
+
+    log.info('Registering peer $peerName');
+    await receptionist._phone.register();
+    log.info('Waiting for peer state event');
+
+    Event.PeerState peerStateEvent =
+        await receptionist.waitFor(eventType: Event.Key.peerState);
+    log.info('Got event ${peerStateEvent.asMap}');
+    expect(peerStateEvent.peer.registered, isTrue);
+    expect(peerStateEvent.peer.ID, equals(peerName));
+
+    log.info('Flushing event stack');
+    receptionist.eventStack.clear();
+
+    log.info('Unregistering peer $peerName to complete cycle');
+    receptionist._phone.unregister();
+    log.info('Waiting for peer state event');
+
+    peerStateEvent = await receptionist.waitFor(eventType: Event.Key.peerState);
+
+    log.info('Got event ${peerStateEvent.asMap}');
+    expect(peerStateEvent.peer.registered, isFalse);
+    expect(peerStateEvent.peer.ID, equals(peerName));
+
+    log.info('Test done.');
   }
 
-  static Future list (Service.CallFlowControl callFlowControl) =>
-    callFlowControl.peerList().then((Iterable<Model.Peer> peers) =>
-      expect (peers.length, isPositive));
+  static Future list(Service.CallFlowControl callFlowControl) => callFlowControl
+      .peerList()
+      .then((Iterable<Model.Peer> peers) => expect(peers.length, isPositive));
 }
