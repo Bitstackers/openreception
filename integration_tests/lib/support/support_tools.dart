d0d1eadb727b3bf98896c1b9f34dc1ae639fd9d1
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-01-06 20:30:50 +0100

    Refactored the setup and teardown of support tools to deploy and delete the peer account of the receptionist actors. Also, autoformatter at work.

--- integration_tests/lib/support/support_tools.dart
+++ integration_tests/lib/support/support_tools.dart
@@ -4,7 +4,7 @@ class SupportTools {
   List<Receptionist> receptionists = [];
   List<Customer> customers = [];
 
-  static final Logger log = new Logger ('Test.SupportTools');
+  static final Logger log = new Logger('Test.SupportTools');
 
   static SupportTools _instance = null;
   Completer _ready = new Completer();
@@ -27,7 +27,11 @@ class SupportTools {
   }
 
   void outputState() {
-    log.shout('Receptionists: ${ReceptionistPool.instance.busy.length}/${ReceptionistPool.instance.available.length}, Customers: ${CustomerPool.instance.busy.length}/${CustomerPool.instance.available.length}');
+    log.shout('Receptionists: '
+        '${ReceptionistPool.instance.busy.length}/'
+        '${ReceptionistPool.instance.available.length}, '
+        'Customers: ${CustomerPool.instance.busy.length}/'
+        '${CustomerPool.instance.available.length}');
   }
 
   /// Maps token to a User object.
@@ -38,95 +42,140 @@ class SupportTools {
 
   Future buildUserMap() {
     Transport.Client transport = new Transport.Client();
-    Service.Authentication authService = new Service.Authentication
-          (Config.authenticationServerUri, Config.serverToken, transport);
+    Service.Authentication authService = new Service.Authentication(
+        Config.authenticationServerUri, Config.serverToken, transport);
 
-    return Future.forEach(Config.authTokens, ((String token) =>
-      authService.userOf(token).then((Model.User user) {
-        tokenMap[token] = user;
-        peerMap[user.peer] = token;
-    })))
-    .whenComplete(() => transport.client.close(force:true));
+    return Future
+        .forEach(
+            Config.authTokens,
+            ((String token) =>
+                authService.userOf(token).then((Model.User user) {
+                  tokenMap[token] = user;
+                  peerMap[user.peer] = token;
+                })))
+        .whenComplete(() => transport.client.close(force: true));
   }
 
+  Future setupReceptionists() => Future.doWhile(() {
+        if (Config.authTokens.isEmpty) {
+          return false;
+        }
 
-  Future setupReceptionists() =>
-    Future.doWhile(() {
-      if (Config.authTokens.isEmpty) {
-        return false;
-      }
+        Phonio.SIPAccount account = ConfigPool.requestLocalSipAccount();
+        String token = peerMap[account.username];
+        Phonio.SIPPhone phone = new Phonio.PJSUAProcess(
+            Config.simpleClientBinaryPath, ConfigPool.requestPjsuaPort());
+        Model.User user = tokenMap[token];
 
-      Phonio.SIPAccount account = ConfigPool.requestLocalSipAccount();
-      String token = peerMap[account.username];
-      Phonio.SIPPhone phone = new Phonio.PJSUAProcess(Config.simpleClientBinaryPath, ConfigPool.requestPjsuaPort());
-      Model.User user = tokenMap[token];
+        phone.addAccount(account);
 
-      phone.addAccount(account);
+        Receptionist receptionist = new Receptionist(phone, token, user);
 
-      Receptionist receptionist = new Receptionist(phone, token, user);
+        receptionists.add(receptionist);
+        return ConfigPool.hasAvailableLocalSipAccount();
+      }).whenComplete(() =>
+          ReceptionistPool.instance = new ReceptionistPool(receptionists));
 
-      receptionists.add(receptionist);
-      return ConfigPool.hasAvailableLocalSipAccount();
+  Future setupCustomers() => Future.doWhile(() {
+        Phonio.SIPAccount account = ConfigPool.requestExternalSIPAccount();
+        Phonio.SIPPhone phone = new Phonio.PJSUAProcess(
+            Config.simpleClientBinaryPath, ConfigPool.requestPjsuaPort());
 
-    })
-    .whenComplete(() =>
-        ReceptionistPool.instance = new ReceptionistPool(receptionists));
+        phone.addAccount(account);
 
-  Future setupCustomers() =>
-    Future.doWhile(() {
+        customers.add(new Customer(phone));
 
-    Phonio.SIPAccount account = ConfigPool.requestExternalSIPAccount();
-    Phonio.SIPPhone phone = new Phonio.PJSUAProcess(Config.simpleClientBinaryPath, ConfigPool.requestPjsuaPort());
+        return ConfigPool.hasAvailableExternalSipAccount();
+      }).whenComplete(
+          () => CustomerPool.instance = new CustomerPool(customers));
 
-      phone.addAccount(account);
+  Future tearDownReceptionists() => Future.wait(receptionists
+      .map((Receptionist receptionist) => receptionist.finalize()));
 
-      customers.add(new Customer(phone));
+  Future tearDownCustomers() =>
+      Future.wait(customers.map((Customer customer) => customer.finalize()));
 
-      return ConfigPool.hasAvailableExternalSipAccount();
-    })
-    .whenComplete(() =>
-        CustomerPool.instance = new CustomerPool(customers));
-
-  Future tearDownReceptionists () => Future.wait
-    (receptionists.map((Receptionist receptionist) => receptionist.finalize()));
-
-  Future tearDownCustomers () => Future.wait
-    (customers.map((Customer customer) => customer.finalize()));
-
-  Future _initialize() =>
-      this.buildUserMap()
+  Future _initialize() => this
+      .buildUserMap()
+      .then((_) => createPeerAccounts())
       .then((_) => this.setupCustomers())
       .then((_) => this.setupReceptionists())
       .then((_) => this._ready.complete())
       .catchError(this._ready.completeError)
-      .whenComplete(() => log.info (this));
+      .whenComplete(() => log.info(this));
 
-  Future tearDown() =>
-      Future.wait([this.tearDownCustomers(),
-                   this.tearDownReceptionists()])
-            .then((_) => ConfigPool.resetCounters());
+  /**
+   * Tear down the state of the support tools.
+   */
+  Future tearDown() async {
+    await Future.wait([
+      tearDownCustomers(),
+      tearDownReceptionists(),
+      removePeerAccounts()]);
+    ConfigPool.resetCounters();
+  }
+
+  /**
+   *
+   */
+  Future createPeerAccounts() {
+    Transport.Client transport = new Transport.Client();
+
+    Service.RESTDialplanStore rdpStore = new Service.RESTDialplanStore(
+        Config.dialplanStoreUri, Config.serverToken, transport);
+
+    Service.PeerAccount paService = new Service.PeerAccount(
+        Config.dialplanStoreUri, Config.serverToken, transport);
+
+    Iterable peerAccounts = Config.localSipAccounts.map(
+        (Phonio.SIPAccount acc) =>
+            new Model.PeerAccount(acc.username, acc.password, 'receptionists'));
+
+    log.info(tokenMap[peerMap[peerAccounts.first.username]].ID);
+
+    return Future.wait(peerAccounts.map((pa) =>
+        paService.deployAccount(pa, tokenMap[peerMap[pa.username]].ID)))
+        .then((_) => rdpStore.reloadConfig())
+        .whenComplete(() => transport.client.close(force: true));
+  }
+
+  /**
+   *
+   */
+  Future removePeerAccounts() {
+    Transport.Client transport = new Transport.Client();
+
+    Service.PeerAccount paService = new Service.PeerAccount(
+        Config.dialplanStoreUri, Config.serverToken, transport);
+
+    Iterable peerAccounts = Config.localSipAccounts.map(
+        (Phonio.SIPAccount acc) =>
+            new Model.PeerAccount(acc.username, acc.password, 'receptionists'));
+
+    log.info(tokenMap[peerMap[peerAccounts.first.username]].ID);
+
+    return Future.wait(peerAccounts.map((pa) =>
+        paService.remove(pa.username)))
+        .whenComplete(() => transport.client.close(force: true));
+  }
 
 
   @override
-  String toString () {
+  String toString() {
     Iterable tokenMappings =
         tokenMap.keys.map((String key) => '$key -> ${tokenMap[key].asSender}');
 
     Iterable peerMappings =
         peerMap.keys.map((String key) => '$key -> ${peerMap[key]}');
 
-    return
-      'Support tools state:\n'
-      ' Token map:\n'
-      '  ${tokenMappings.join('\n  ')}\n'
-      ' Peer map:\n'
-      '  ${peerMappings.join('\n  ')}\n'
-      ' Receptionists:\n'
-      '  ${receptionists.join('\n  ')}\n'
-      ' Customers:\n'
-      '  ${customers.join('\n  ')}\n';
+    return 'Support tools state:\n'
+        ' Token map:\n'
+        '  ${tokenMappings.join('\n  ')}\n'
+        ' Peer map:\n'
+        '  ${peerMappings.join('\n  ')}\n'
+        ' Receptionists:\n'
+        '  ${receptionists.join('\n  ')}\n'
+        ' Customers:\n'
+        '  ${customers.join('\n  ')}\n';
   }
 }
-
-
-
