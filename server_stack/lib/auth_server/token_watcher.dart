469899f37861a338b20e5a0db0339a83993497ea
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-01-10 20:33:17 +0100

    Removed dependency on common.dart.

--- server_stack/lib/auth_server/token_watcher.dart
+++ server_stack/lib/auth_server/token_watcher.dart
@@ -18,11 +18,10 @@ import 'dart:async';
 import 'package:logging/logging.dart';
 import '../configuration.dart';
 import 'token_vault.dart';
-import 'package:openreception_framework/common.dart';
 
 const String libraryName = 'AuthServer.tokenWatch';
 
-final Logger log = new Logger ('$libraryName');
+final Logger log = new Logger('$libraryName');
 
 void setup() {
   int minutes = 10;
@@ -32,21 +31,24 @@ void setup() {
 
 void seen(String token) {
   Map data = vault.getToken(token);
-  data['expiresAt'] = dateTimeToJson
-      (new DateTime.now().add(config.authServer.tokenLifetime));
+  data['expiresAt'] =
+      dateTimeToJson(new DateTime.now().add(config.authServer.tokenLifetime));
   vault.updateToken(token, data);
 }
 
 void _timerTick(Timer timer) {
   Iterable<String> tokens = vault.listUserTokens().toList();
-  for(String token in tokens) {
+  for (String token in tokens) {
     Map data = vault.getToken(token);
     DateTime expiresAt = JsonToDateTime(data['expiresAt']);
 
     int now = new DateTime.now().millisecondsSinceEpoch;
-    if(now > expiresAt.millisecondsSinceEpoch) {
+    if (now > expiresAt.millisecondsSinceEpoch) {
       log.info('This token ${token} expired ${expiresAt} - removing it');
       vault.removeToken(token);
     }
   }
 }
+
+DateTime JsonToDateTime(String timeString) => DateTime.parse(timeString);
+String dateTimeToJson(DateTime time) => time.toString();
