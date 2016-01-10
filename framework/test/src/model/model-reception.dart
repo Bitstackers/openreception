f0d19c0e2f0e266d900c4d1931b28307eb988eb6
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-01-10 22:45:45 +0100

    Removed TODO by actually fixing it.

--- framework/test/src/model/model-reception.dart
+++ framework/test/src/model/model-reception.dart
@@ -70,7 +70,7 @@ abstract class ModelReception {
       expect(() => JSON.encode(buildObject()), returnsNormally);
 
   /**
-   * TODO: Add additional expects.
+   *
    */
   static Model.Reception buildObject() {
     final List<String> addresses = ['Somewhere else'];
@@ -78,6 +78,30 @@ abstract class ModelReception {
     final List<String> alternateNames = ['nice place'];
     final List<String> bankingInformation = ['The vault'];
     final List<String> customerTypes = ['Not defined'];
+    final List<String> emailAddresses = ['me@evil.corp', 'him@good.corp'];
+    final bool enabled = true;
+    final Uri extraData = Uri.parse('http://localhost/test');
+
+    final String name = 'Test test';
+    final String greeting = 'Go away';
+    final List<String> handlingInstructions = ['Hang up'];
+    final int id = 999;
+    final DateTime lastChecked = new DateTime.now();
+    final List<String> openingHours = ['mon-fri 8-17'];
+    final int organizationId = 123;
+    final String otherData = 'Ask Data';
+    final String product = 'Butter';
+    final List<String> salesMarketingHandling = ['Ask them to GTFO'];
+    final String shortGreeting = 'Please go';
+    final List<Model.PhoneNumber> telephoneNumbers = [
+      new Model.PhoneNumber.empty()..value = '56 33 21 44',
+      new Model.PhoneNumber.empty()
+        ..value = '56 33 21 43'
+        ..confidential = true
+        ..description = 'Home phone'
+    ];
+    final List<String> vatNumbers = ['123455'];
+    final List<String> websites = ['www.over-the-rainbow'];
 
     Model.Reception buildObject = new Model.Reception.empty()
       ..addresses = addresses
@@ -85,31 +109,47 @@ abstract class ModelReception {
       ..alternateNames = alternateNames
       ..bankingInformation = bankingInformation
       ..customerTypes = customerTypes
-      ..emailAddresses = []
-      ..enabled = true
-      ..extraData = Uri.parse('http://localhost/test')
-      ..fullName = 'Test test'
-      ..greeting = 'Go away'
-      ..handlingInstructions = ['Hang up']
-      ..ID = 999
-      ..lastChecked = new DateTime.now()
-      ..openingHours = []
-      ..organizationId = 888
-      ..otherData = 'Nope'
-      ..product = 'Butter'
-      ..salesMarketingHandling = []
-      ..shortGreeting = 'Please go'
-      ..telephoneNumbers = [
-        new Model.PhoneNumber.empty()..value = '56 33 21 44'
-      ]
-      ..vatNumbers = []
-      ..websites = [];
+      ..emailAddresses = emailAddresses
+      ..enabled = enabled
+      ..extraData = extraData
+      ..fullName = name
+      ..greeting = greeting
+      ..handlingInstructions = handlingInstructions
+      ..ID = id
+      ..lastChecked = lastChecked
+      ..openingHours = openingHours
+      ..organizationId = organizationId
+      ..otherData = otherData
+      ..product = product
+      ..salesMarketingHandling = salesMarketingHandling
+      ..shortGreeting = shortGreeting
+      ..telephoneNumbers = telephoneNumbers
+      ..vatNumbers = vatNumbers
+      ..websites = websites;
 
     expect(buildObject.addresses, equals(addresses));
     expect(buildObject.dialplan, equals(dialplan));
+    expect(buildObject.enabled, equals(enabled));
+    expect(buildObject.extraData, equals(extraData));
+    expect(buildObject.emailAddresses, equals(emailAddresses));
     expect(buildObject.alternateNames, equals(alternateNames));
     expect(buildObject.bankingInformation, equals(bankingInformation));
     expect(buildObject.customerTypes, equals(customerTypes));
+
+    expect(buildObject.fullName, equals(name));
+    expect(buildObject.greeting, equals(greeting));
+    expect(buildObject.handlingInstructions, equals(handlingInstructions));
+    expect(buildObject.ID, equals(id));
+    expect(buildObject.lastChecked, equals(lastChecked));
+    expect(buildObject.openingHours, equals(openingHours));
+    expect(buildObject.organizationId, equals(organizationId));
+    expect(buildObject.otherData, equals(otherData));
+    expect(buildObject.product, equals(product));
+    expect(buildObject.salesMarketingHandling, equals(salesMarketingHandling));
+    expect(buildObject.shortGreeting, equals(shortGreeting));
+    expect(buildObject.telephoneNumbers, equals(telephoneNumbers));
+    expect(buildObject.vatNumbers, equals(vatNumbers));
+    expect(buildObject.websites, equals(websites));
 
     return buildObject;
   }
