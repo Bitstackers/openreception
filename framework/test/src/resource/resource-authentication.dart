75aca5dd1d112af9f9bec45b3ea3bfaf4685060d
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-01-10 22:46:26 +0100

    dartfmt

--- framework/test/src/resource/resource-authentication.dart
+++ framework/test/src/resource/resource-authentication.dart
@@ -23,11 +23,11 @@ void testResourceAuthentication() {
 abstract class ResourceAuthentication {
   static final Uri authServer = Uri.parse('http://localhost:4050');
 
-  static void userOf () =>
-      expect(Resource.Authentication.tokenToUser(authServer, 'testtest'),
-        equals(Uri.parse('${authServer}/token/testtest')));
+  static void userOf() => expect(
+      Resource.Authentication.tokenToUser(authServer, 'testtest'),
+      equals(Uri.parse('${authServer}/token/testtest')));
 
-  static void validate () =>
-      expect(Resource.Authentication.validate(authServer, 'testtest'),
-        equals(Uri.parse('${authServer}/token/testtest/validate')));
+  static void validate() => expect(
+      Resource.Authentication.validate(authServer, 'testtest'),
+      equals(Uri.parse('${authServer}/token/testtest/validate')));
 }
