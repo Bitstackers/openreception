c252b1bcdce65c49ecabac7a0818e38c326e8585
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-01-15 09:55:01 +0100

    dartfmt.

--- integration_tests/lib/service/src/storage-message.dart
+++ integration_tests/lib/service/src/storage-message.dart
@@ -1,8 +1,7 @@
 part of or_test_fw;
 
 abstract class MessageStore {
-
-  static final Logger log = new Logger ('$libraryName.MessageStore');
+  static final Logger log = new Logger('$libraryName.MessageStore');
 
   /**
    * Test server behaviour when trying to retrieve a non-filtered list of
@@ -11,41 +10,41 @@ abstract class MessageStore {
    * The expected behaviour is that the server should return a list
    * of message objects.
    */
-  static Future list (Storage.Message messageStore) {
-
+  static Future list(Storage.Message messageStore) {
     log.info('Listing messages non-filtered.');
 
-    return messageStore.list()
-      .then((Iterable<Model.Message> messages) {
-        expect(messages.length, greaterThan(0));
-        expect(messages.every((msg) => msg is Model.Message), isTrue);
+    return messageStore.list().then((Iterable<Model.Message> messages) {
+      expect(messages.length, greaterThan(0));
+      expect(messages.every((msg) => msg is Model.Message), isTrue);
     });
   }
 
   /**
    *
    */
-  static Future<Model.Message> _createMessage(Storage.Message messageStore,
-                                Storage.Contact contactStore,
-                                Storage.Reception receptionStore,
-                                Receptionist sender) async {
-
+  static Future<Model.Message> _createMessage(
+      Storage.Message messageStore,
+      Storage.Contact contactStore,
+      Storage.Reception receptionStore,
+      Receptionist sender) async {
     int receptionID = 1;
     int contactID = 4;
-    Model.Contact contact = await contactStore.getByReception(contactID, receptionID);
+    Model.Contact contact =
+        await contactStore.getByReception(contactID, receptionID);
     Model.Reception reception = await receptionStore.get(receptionID);
     Set<Model.MessageRecipient> recipients = new Set();
 
-    await Future.forEach(contact.distributionList, (Model.DistributionListEntry de) =>
-      contactStore.getByReception(de.contactID, de.receptionID)
-        .then((Model.Contact c) {
-      recipients.addAll(c.endpoints.map((Model.MessageEndpoint mep) =>
-            new Model.MessageRecipient(mep, de)));
-    }));
+    await Future.forEach(
+        contact.distributionList,
+        (Model.DistributionListEntry de) => contactStore
+                .getByReception(de.contactID, de.receptionID)
+                .then((Model.Contact c) {
+              recipients.addAll(c.endpoints.map((Model.MessageEndpoint mep) =>
+                  new Model.MessageRecipient(mep, de)));
+            }));
 
     Model.Message newMessage = Randomizer.randomMessage()
-      ..context = new Model.MessageContext.fromContact
-                    (contact, reception)
+      ..context = new Model.MessageContext.fromContact(contact, reception)
       ..recipients = recipients
       ..senderId = sender.user.ID;
 
@@ -55,12 +54,13 @@ abstract class MessageStore {
   /**
    *
    */
-  static Future create(Storage.Message messageStore,
-                         Storage.Contact contactStore,
-                         Storage.Reception receptionStore,
-                         Receptionist sender) async {
-    Model.Message createdMessage = await
-        _createMessage (messageStore, contactStore, receptionStore, sender);
+  static Future create(
+      Storage.Message messageStore,
+      Storage.Contact contactStore,
+      Storage.Reception receptionStore,
+      Receptionist sender) async {
+    Model.Message createdMessage = await _createMessage(
+        messageStore, contactStore, receptionStore, sender);
 
     Model.Message fetchedMessage = await messageStore.get(createdMessage.ID);
 
@@ -71,18 +71,19 @@ abstract class MessageStore {
     expect(createdMessage.sent, isFalse);
     expect(createdMessage.senderId, sender.user.ID);
 
-    return  messageStore.remove(createdMessage.ID);
+    return messageStore.remove(createdMessage.ID);
   }
 
   /**
    *
    */
-  static Future remove(Storage.Message messageStore,
-                         Storage.Contact contactStore,
-                         Storage.Reception receptionStore,
-                         Receptionist sender) async {
-    Model.Message createdMessage = await
-        _createMessage (messageStore, contactStore, receptionStore, sender);
+  static Future remove(
+      Storage.Message messageStore,
+      Storage.Contact contactStore,
+      Storage.Reception receptionStore,
+      Receptionist sender) async {
+    Model.Message createdMessage = await _createMessage(
+        messageStore, contactStore, receptionStore, sender);
 
     await messageStore.remove(createdMessage.ID);
 
@@ -90,15 +91,15 @@ abstract class MessageStore {
         throwsA(new isInstanceOf<Storage.NotFound>()));
   }
 
-  static Future enqueue (Storage.Message messageStore,
-                             Storage.Contact contactStore,
-                             Storage.Reception receptionStore,
-                             Receptionist sender) async {
-      Model.Message createdMessage = await
-          _createMessage(messageStore, contactStore, receptionStore, sender);
+  static Future enqueue(
+      Storage.Message messageStore,
+      Storage.Contact contactStore,
+      Storage.Reception receptionStore,
+      Receptionist sender) async {
+    Model.Message createdMessage = await _createMessage(
+        messageStore, contactStore, receptionStore, sender);
 
-      return messageStore.enqueue(createdMessage);
-
+    return messageStore.enqueue(createdMessage);
   }
 
   /**
@@ -108,49 +109,51 @@ abstract class MessageStore {
    * The expected behaviour is that the server should return the
    * Reception object.
    */
-  static Future<Model.Message> get (Storage.Message messageStore) {
+  static Future<Model.Message> get(Storage.Message messageStore) {
     int existingMessageID = 1;
 
     log.info('Checking server behaviour on an existing message.');
 
-    return messageStore.get(existingMessageID)
-      .then ((Model.Message message) {
-        expect (message.ID, equals(existingMessageID));
+    return messageStore.get(existingMessageID).then((Model.Message message) {
+      expect(message.ID, equals(existingMessageID));
     });
   }
-
 
   /**
    *
    */
-  static Future update(Storage.Message messageStore,
-                         Storage.Contact contactStore,
-                         Storage.Reception receptionStore,
-                         Receptionist sender) {
-
-      return  _createMessage (messageStore, contactStore, receptionStore, sender)
+  static Future update(
+      Storage.Message messageStore,
+      Storage.Contact contactStore,
+      Storage.Reception receptionStore,
+      Receptionist sender) {
+    return _createMessage(messageStore, contactStore, receptionStore, sender)
         .then((Model.Message createdMessage) {
-        {
-          Model.Message randMsg = Randomizer.randomMessage();
-          randMsg.ID = createdMessage.ID;
-          randMsg.context = createdMessage.context;
-          randMsg.senderId = createdMessage.senderId;
-          createdMessage = randMsg;
-        }
+      {
+        Model.Message randMsg = Randomizer.randomMessage();
+        randMsg.ID = createdMessage.ID;
+        randMsg.context = createdMessage.context;
+        randMsg.senderId = createdMessage.senderId;
+        createdMessage = randMsg;
+      }
 
-        return messageStore.update(createdMessage)
-            .then((Model.Message updatedMessage) {
-          expect(createdMessage.ID, equals(updatedMessage.ID));
-          expect(createdMessage.asMap, equals(updatedMessage.asMap));
-          expect(createdMessage.body, equals(updatedMessage.body));
-          expect(createdMessage.callerInfo.asMap, equals(updatedMessage.callerInfo.asMap));
-          expect(createdMessage.closed, equals(updatedMessage.closed));
-          expect(createdMessage.flag.toJson(), equals(updatedMessage.flag.toJson()));
-          expect(createdMessage.hasRecpients, equals(updatedMessage.hasRecpients));
-          expect(createdMessage.recipients, equals(updatedMessage.recipients));
-          expect(createdMessage.senderId, equals(updatedMessage.senderId));
-          expect(createdMessage.sent, equals(updatedMessage.sent));
-        });
+      return messageStore
+          .update(createdMessage)
+          .then((Model.Message updatedMessage) {
+        expect(createdMessage.ID, equals(updatedMessage.ID));
+        expect(createdMessage.asMap, equals(updatedMessage.asMap));
+        expect(createdMessage.body, equals(updatedMessage.body));
+        expect(createdMessage.callerInfo.asMap,
+            equals(updatedMessage.callerInfo.asMap));
+        expect(createdMessage.closed, equals(updatedMessage.closed));
+        expect(
+            createdMessage.flag.toJson(), equals(updatedMessage.flag.toJson()));
+        expect(
+            createdMessage.hasRecpients, equals(updatedMessage.hasRecpients));
+        expect(createdMessage.recipients, equals(updatedMessage.recipients));
+        expect(createdMessage.senderId, equals(updatedMessage.senderId));
+        expect(createdMessage.sent, equals(updatedMessage.sent));
       });
-    }
+    });
+  }
 }
