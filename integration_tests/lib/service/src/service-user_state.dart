0b61e94cbcd1893cc86adaa3459025062fa07d1a
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-01-19 10:12:52 +0100

    Rewrote to async.

--- integration_tests/lib/service/src/service-user_state.dart
+++ integration_tests/lib/service/src/service-user_state.dart
@@ -4,54 +4,44 @@ abstract class UserState {
   static Logger log = new Logger('$libraryName.CallFlowControl.UserState');
 
   static Future originateForbidden(
-      Receptionist receptionist, Customer caller, Customer callee) {
+      Receptionist receptionist, Customer caller, Customer callee) async {
     String receptionNumber = '12340005';
 
-    Model.Call firstCall;
     final Model.OriginationContext context = new Model.OriginationContext()
       ..contactId = 4
       ..dialplan = '12340001'
       ..receptionId = 1;
 
-    return Future.wait([])
-        .then((_) => log.info('Caller dials the reception at $receptionNumber'))
-        .then((_) => caller.dial(receptionNumber))
-        .then((_) => log.info('Receptionist tries to hunt down the next call'))
-        .then((_) => receptionist.huntNextCall().then((Model.Call call) {
-              firstCall = call;
-              expect(firstCall, isNotNull);
-              expect(firstCall.ID, isNot(Model.Call.noID));
-            }))
-        .then((_) =>
-            log.info('Receptionist tries to orignate a new call to $callee'))
-        .then((_) => expect(receptionist.originate(callee.extension, context),
-            throwsA(new isInstanceOf<Storage.ClientError>())));
+    log.info('Caller dials the reception at $receptionNumber');
+    await caller.dial(receptionNumber);
+    log.info('Receptionist tries to hunt down the next call');
+    final Model.Call firstCall = await receptionist.huntNextCall();
+    expect(firstCall, isNotNull);
+    expect(firstCall.ID, isNot(Model.Call.noID));
+
+    log.info('Receptionist tries to orignate a new call to $callee');
+    expect(receptionist.originate(callee.extension, context),
+        throwsA(new isInstanceOf<Storage.ClientError>()));
   }
 
-  static Future pickupForbidden(
-      Receptionist receptionist, Customer caller, Customer second_caller) {
+  static Future pickupForbidden(Receptionist receptionist, Customer caller,
+      Customer second_caller) async {
     String receptionNumber = '12340005';
 
-    Model.Call secondCall;
+    log.info('Caller dials the reception at $receptionNumber');
+    await caller.dial(receptionNumber);
+    log.info('Receptionist tries to hunt down the next call');
+    final Model.Call firstCall = await receptionist.huntNextCall();
+    log.info('$receptionist got $firstCall');
+    receptionist.eventStack.clear();
+    log.info('Second caller dials the reception at $receptionNumber');
+    await second_caller.dial(receptionNumber);
 
-    return Future.wait([])
-        .then((_) => log.info('Caller dials the reception at $receptionNumber'))
-        .then((_) => caller.dial(receptionNumber))
-        .then((_) => log.info('Receptionist tries to hunt down the next call'))
-        .then((_) => receptionist
-            .huntNextCall()
-            .then((Model.Call call) => log.info('$receptionist got $call')))
-        .then((_) => receptionist.eventStack.clear())
-        .then((_) =>
-            log.info('Second caller dials the reception at $receptionNumber'))
-        .then((_) => second_caller.dial(receptionNumber))
-        .then((_) => log.info('Receptionist waits second call'))
-        .then((_) => receptionist
-            .waitForCall()
-            .then((Model.Call call) => secondCall = call))
-        .then((_) =>
-            log.info('Receptionist tries to pick up second call $secondCall'))
-        .then((_) => expect(receptionist.pickup(secondCall),
-            throwsA(new isInstanceOf<Storage.ClientError>())));
+    log.info('Receptionist waits second call');
+    final Model.Call secondCall = await receptionist.waitForCall();
+
+    log.info('Receptionist tries to pick up second call $secondCall');
+    expect(receptionist.pickup(secondCall),
+        throwsA(new isInstanceOf<Storage.ClientError>()));
   }
 }
