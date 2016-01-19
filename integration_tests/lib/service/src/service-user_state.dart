4c9a5d987da263811380babfe48507ca40df3bb5
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-01-19 09:23:40 +0100

    Autoformatting and minor fixes.

--- integration_tests/lib/service/src/service-user_state.dart
+++ integration_tests/lib/service/src/service-user_state.dart
@@ -1,12 +1,10 @@
 part of or_test_fw;
 
 abstract class UserState {
-
   static Logger log = new Logger('$libraryName.CallFlowControl.UserState');
 
-  static Future originateForbidden(Receptionist receptionist,
-                                Customer caller,
-                                Customer callee) {
+  static Future originateForbidden(
+      Receptionist receptionist, Customer caller, Customer callee) {
     String receptionNumber = '12340005';
 
     Model.Call firstCall;
@@ -16,44 +14,44 @@ abstract class UserState {
       ..receptionId = 1;
 
     return Future.wait([])
-      .then((_) => log.info ('Caller dials the reception at $receptionNumber'))
-      .then((_) => caller.dial(receptionNumber))
-      .then((_) => log.info ('Receptionist tries to hunt down the next call'))
-      .then((_) => receptionist.huntNextCall()
-          .then((Model.Call call) {
-            firstCall = call;
-            expect(firstCall, isNotNull);
-            expect(firstCall.ID, isNot(Model.Call.noID));
-          }))
-      .then((_) => log.info ('Receptionist tries to orignate a new call to $callee'))
-      .then((_) =>
-        expect (receptionist.originate(callee.extension,context),
-          throwsA(new isInstanceOf<Storage.ClientError>())));
+        .then((_) => log.info('Caller dials the reception at $receptionNumber'))
+        .then((_) => caller.dial(receptionNumber))
+        .then((_) => log.info('Receptionist tries to hunt down the next call'))
+        .then((_) => receptionist.huntNextCall().then((Model.Call call) {
+              firstCall = call;
+              expect(firstCall, isNotNull);
+              expect(firstCall.ID, isNot(Model.Call.noID));
+            }))
+        .then((_) =>
+            log.info('Receptionist tries to orignate a new call to $callee'))
+        .then((_) => expect(receptionist.originate(callee.extension, context),
+            throwsA(new isInstanceOf<Storage.ClientError>())));
   }
 
-  static Future pickupForbidden(Receptionist receptionist,
-                                Customer caller,
-                                Customer second_caller) {
+  static Future pickupForbidden(
+      Receptionist receptionist, Customer caller, Customer second_caller) {
     String receptionNumber = '12340005';
 
-    Model.Call firstCall;
     Model.Call secondCall;
 
     return Future.wait([])
-      .then((_) => log.info ('Caller dials the reception at $receptionNumber'))
-      .then((_) => caller.dial(receptionNumber))
-      .then((_) => log.info ('Receptionist tries to hunt down the next call'))
-      .then((_) => receptionist.huntNextCall()
-        .then((Model.Call call) => firstCall = call))
-      .then((_) => receptionist.eventStack.clear())
-      .then((_) => log.info ('Second caller dials the reception at $receptionNumber'))
-      .then((_) => second_caller.dial(receptionNumber))
-      .then((_) => log.info ('Receptionist waits second call'))
-      .then((_) => receptionist.waitForCall()
-        .then((Model.Call call) => secondCall = call))
-      .then((_) => log.info ('Receptionist tries to pick up second call $secondCall'))
-      .then((_) =>
-        expect (receptionist.pickup(secondCall),
-          throwsA(new isInstanceOf<Storage.ClientError>())));
+        .then((_) => log.info('Caller dials the reception at $receptionNumber'))
+        .then((_) => caller.dial(receptionNumber))
+        .then((_) => log.info('Receptionist tries to hunt down the next call'))
+        .then((_) => receptionist
+            .huntNextCall()
+            .then((Model.Call call) => log.info('$receptionist got $call')))
+        .then((_) => receptionist.eventStack.clear())
+        .then((_) =>
+            log.info('Second caller dials the reception at $receptionNumber'))
+        .then((_) => second_caller.dial(receptionNumber))
+        .then((_) => log.info('Receptionist waits second call'))
+        .then((_) => receptionist
+            .waitForCall()
+            .then((Model.Call call) => secondCall = call))
+        .then((_) =>
+            log.info('Receptionist tries to pick up second call $secondCall'))
+        .then((_) => expect(receptionist.pickup(secondCall),
+            throwsA(new isInstanceOf<Storage.ClientError>())));
   }
 }
