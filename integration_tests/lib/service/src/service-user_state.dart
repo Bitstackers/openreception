c0aa5d16d52a0eb3e8a2e7302f6054916b285598
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-01-19 20:27:12 +0100

    Additional steps to perform in order to assert that server-side state is sane.

--- integration_tests/lib/service/src/service-user_state.dart
+++ integration_tests/lib/service/src/service-user_state.dart
@@ -3,6 +3,10 @@ part of or_test_fw;
 abstract class UserState {
   static Logger log = new Logger('$libraryName.CallFlowControl.UserState');
 
+  /**
+  * Tests if the receptionist receives an error when trying originate
+  * a new call while in call.
+   */
   static Future originateForbidden(
       Receptionist receptionist, Customer caller, Customer callee) async {
     String receptionNumber = '12340005';
@@ -22,8 +26,20 @@ abstract class UserState {
     log.info('Receptionist tries to orignate a new call to $callee');
     expect(receptionist.originate(callee.extension, context),
         throwsA(new isInstanceOf<Storage.ClientError>()));
+    await receptionist.hangUp(firstCall);
+    await receptionist.waitForPhoneHangup();
+
+    log.info('$receptionist tries to orignate a new call to $callee again');
+    final Model.Call outboundCall =
+        await receptionist.originate(callee.extension, context);
+    await receptionist.hangUp(outboundCall);
+    await receptionist.waitForPhoneHangup();
   }
 
+  /**
+   * Tests if the receptionist receives an error when trying to pick
+   * up a second call while in call.
+   */
   static Future pickupForbidden(Receptionist receptionist, Customer caller,
       Customer second_caller) async {
     String receptionNumber = '12340005';
@@ -43,5 +59,11 @@ abstract class UserState {
     log.info('Receptionist tries to pick up second call $secondCall');
     expect(receptionist.pickup(secondCall),
         throwsA(new isInstanceOf<Storage.ClientError>()));
+
+    await receptionist.hangUp(firstCall);
+    await receptionist.waitForPhoneHangup();
+    await receptionist.pickup(secondCall, waitForEvent: true);
+    await receptionist.hangUp(secondCall);
+    await receptionist.waitForPhoneHangup();
   }
 }
