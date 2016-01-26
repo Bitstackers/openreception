01d0234a57b78fcdfe37ed0cc3356e95e3ffaa3d
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-01-26 14:01:20 +0100

    Refactored bus test into a type-safe(er) implementation.

--- framework/test/src/bus.dart
+++ framework/test/src/bus.dart
@@ -14,24 +14,19 @@
 part of openreception.test;
 
 void testBus() {
-  test('async openreception.bus test', () {
+  test('async openreception.bus test', () async {
     final String testEvent = 'Foo!';
     Bus bus = new Bus<String>();
-    Stream<String> stream = bus.stream;
+    Stream stream = bus.stream;
     Timer timer;
-
-    timer = new Timer(new Duration(seconds: 1), () {
-      fail('testEvent not fired or caught within 1 second');
-    });
-
-    stream.listen(expectAsync((String value) {
-      expect(value, equals(testEvent));
-
-      if (timer != null) {
-        timer.cancel();
-      }
-    }));
-
+    Future expectTestEvent =
+        stream.firstWhere((String value) => value == testEvent);
     bus.fire(testEvent);
+
+    try {
+      await expectTestEvent.timeout(new Duration(seconds: 1));
+    } on TimeoutException {
+      fail('Did not receive event within 1 second');
+    }
   });
 }
