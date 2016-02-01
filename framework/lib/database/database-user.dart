5752e7bee1f50d02f84f95a55a5995f60c611b22
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-02-01 23:06:52 +0100

    Cleaned out and cleaned up the user database wrapper. Removed the inlined groups and identities which now have to be fetched after the user object.

--- framework/lib/database/database-user.dart
+++ framework/lib/database/database-user.dart
@@ -17,10 +17,6 @@ part of openreception.database;
  * Database realization of the [User] [Storage] layer.
  */
 class User implements Storage.User {
-  static const String className = '${libraryName}.User';
-
-  static final Logger log = new Logger(className);
-
   final Connection _connection;
 
   /**
@@ -32,363 +28,415 @@ class User implements Storage.User {
   /**
    * Create a single user in the database.
    */
-  Future<Model.User> create(Model.User user) {
-    String sql = '''
-    INSERT INTO users (name, extension, send_from)
-    VALUES (@name, @extension, @sendfrom)
-    RETURNING id;
-  ''';
+  Future<Model.User> create(Model.User user) async {
+    final String sql = '''
+iNSERT INTO
+  users
+    (name, extension, send_from, google_username, google_appcode, enabled)
+VALUES
+    (@name, @extension, @sendfrom, @googleUsername, @googleAppcode, @enabled)
+RETURNING
+  id''';
 
-    Map parameters = {
+    final Map parameters = {
       'name': user.name,
       'extension': user.peer,
-      'sendfrom': user.address
+      'sendfrom': user.address,
+      'googleUsername': user.googleUsername,
+      'googleAppcode': user.googleAppcode,
+      'enabled': user.enabled,
     };
 
-    return _connection
-        .query(sql, parameters)
-        .then((Iterable rows) => rows.isEmpty
-            ? throw new Storage.ServerError('User not created')
-            : user..ID = rows.first.id)
-        .catchError((error, stackTrace) {
-      log.severe('sql:$sql \nparameters:$parameters', error, stackTrace);
+    try {
+      final rows = await _connection.query(sql, parameters);
 
-      return new Future.error(error, stackTrace);
-    });
+      if (rows.isEmpty) {
+        throw new Storage.ServerError('User not created');
+      }
+
+      user.id = rows.first.id;
+      return user;
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
   }
 
   /**
    * Remove the user with id [userId] from the database
    */
-  Future remove(int userId) {
+  Future remove(int userId) async {
     String sql = '''
-      DELETE FROM users
-      WHERE id=@id;
-    ''';
+DELETE FROM
+  users
+WHERE
+  id=@id''';
 
-    Map parameters = {'id': userId};
-    return _connection.execute(sql, parameters)
-        .catchError((error, stackTrace) {
-      log.severe('sql:$sql :: parameters:$parameters', error, stackTrace);
+    final Map parameters = {'id': userId};
 
-      return new Future.error(error, stackTrace);
-    });
+    try {
+      final int rowsAffected = await _connection.execute(sql, parameters);
+
+      if (rowsAffected == 0) {
+        throw new Storage.NotFound('No user with uid: $userId');
+      }
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
+  }
+
+  /**
+   *
+   */
+  Future<Model.User> getByIdentity(String identity) async {
+    String sql = '''
+SELECT
+  u.id,
+  u.name,
+  u.extension,
+  u.google_username,
+  u.google_appcode,
+  u.send_from,
+  u.enabled
+FROM auth_identities JOIN users u ON auth_identities.user_id = u.id
+WHERE identity = @identity''';
+
+    final Map parameters = {'identity': identity};
+
+    try {
+      Iterable rows = await _connection.query(sql, parameters);
+
+      if (rows.isEmpty) {
+        throw new Storage.NotFound('No user with identity: $identity');
+      }
+
+      return _rowToUser(rows.first);
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
   }
 
   /**
    * Retrive a single user from the database.
    */
-  Future<Model.User> get(int userID) {
+  Future<Model.User> get(int userID) async {
     String sql = '''
 SELECT
-  outer_user.id,
-  outer_user.name,
-  outer_user.send_from,
-  outer_user.enabled,
-  outer_user.extension,
-  COALESCE((SELECT array_to_json(array_agg(row_to_json(tmp_groups)))
-   FROM 
-   (SELECT 
-     groups.id, groups.name
-    FROM
-     users 
-    JOIN
-     user_groups ON user_groups.user_id = users.id
-    JOIN groups ON user_groups.group_id = groups.id
-    WHERE user_groups.user_id = outer_user.id
-   ) tmp_groups
-  ),'[]') AS groups,
-
-  COALESCE((SELECT array_to_json(array_agg(row_to_json(tmp_iden)))
-   FROM 
-   (SELECT 
-     users.id as user_id, 
-     auth_identities.identity
-    FROM
-     users 
-    JOIN
-      auth_identities ON auth_identities.user_id = users.id
-    WHERE auth_identities.user_id = outer_user.id
-   ) tmp_iden
-  ),'[]') AS identities
+  id,
+  name,
+  send_from,
+  enabled,
+  google_username,
+  google_appcode,
+  extension
 FROM
-  users as outer_user
-WHERE 
+  users
+WHERE
   id = @id''';
 
-    Map parameters = {'id': userID};
+    final Map parameters = {'id': userID};
 
-    return _connection
-        .query(sql, parameters)
-        .then((Iterable rows) => rows.isEmpty
-            ? new Future.error(new Storage.NotFound('No user with ID $userID'))
-            : _rowToUser(rows.first))
-        .catchError((error, stackTrace) {
-      if (error is! Storage.NotFound) {
-        log.severe(error);
-        log.severe(stackTrace);
-        log.severe('sql:$sql :: parameters:$parameters', error, stackTrace);
+    try {
+      Iterable rows = await _connection.query(sql, parameters);
+
+      if (rows.isEmpty) {
+        throw new Storage.NotFound('No user with uid: $userID');
       }
 
-      return new Future.error(error, stackTrace);
-    });
+      return _rowToUser(rows.first);
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
   }
 
   /**
    * Update an exiting user with the data stored in object [user] passed.
    */
-  Future<Model.User> update(Model.User user) {
+  Future<Model.User> update(Model.User user) async {
     String sql = '''
-    UPDATE users
-    SET name=@name, extension=@extension, send_from=@sendfrom
-    WHERE id=@id;
+UPDATE
+  users
+SET
+  name=@name,
+  extension=@extension,
+  send_from=@sendfrom,
+  google_username=@googleUsername,
+  google_appcode=@googleAppcode,
+  enabled=@enabled
+WHERE id=@id;
   ''';
 
-    Map parameters = {
+    final Map parameters = {
       'name': user.name,
       'extension': user.peer,
       'sendfrom': user.address,
-      'id': user.ID
+      'googleUsername': user.googleUsername,
+      'googleAppcode': user.googleAppcode,
+      'enabled': user.enabled,
+      'id': user.id
     };
 
-    return _connection
-        .execute(sql, parameters)
-        .then((_) => user)
-        .catchError((error, stackTrace) {
-      log.severe('sql:$sql :: parameters:$parameters', error, stackTrace);
+    try {
+      final affectedRows = await _connection.execute(sql, parameters);
 
-      return new Future.error(error, stackTrace);
-    });
+      if (affectedRows == 0) {
+        throw new Storage.ServerError('User not updated');
+      }
+
+      return user;
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
   }
 
   /**
    * Returns the groups of user with id [userId].
    */
-  Future<Iterable<Model.UserGroup>> userGroups(int userId) {
+  Future<Iterable<Model.UserGroup>> userGroups(int userId) async {
     String sql = '''
-    SELECT id, name
-    FROM user_groups 
-     JOIN groups on user_groups.group_id = groups.id
-    WHERE user_groups.user_id = @userid
-  ''';
+SELECT
+  id,
+  name
+FROM
+  user_groups
+JOIN
+  groups ON
+    user_groups.group_id = groups.id
+WHERE
+  user_groups.user_id = @userid''';
 
-    Map parameters = {'userid': userId};
+    final Map parameters = {'userid': userId};
 
-    return _connection.query(sql, parameters).then((Iterable rows) {
-      List<Model.UserGroup> userGroups = [];
-      for (var row in rows) {
-        userGroups.add(new Model.UserGroup.empty()
-          ..id = row.id
-          ..name = row.name);
+    try {
+      return (await _connection.query(sql, parameters)).map(_rowToUserGroup);
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
+  }
+
+  /**
+   * Returns group with id [groupId] in the system.
+   */
+  Future<Model.UserGroup> group(int groupId) async {
+    String sql = '''
+SELECT
+  id,
+  name
+FROM
+  groups
+WHERE
+  id=@gid''';
+
+    final Map parameters = {'gid': groupId};
+
+    try {
+      Iterable rows = await _connection.query(sql, parameters);
+
+      if (rows.isEmpty) {
+        throw new Storage.NotFound('No group with gid: $groupId');
       }
-      return userGroups;
-    }).catchError((error, stackTrace) {
-      log.severe('sql:$sql :: parameters:$parameters', error, stackTrace);
 
-      return new Future.error(error, stackTrace);
-    });
+      return _rowToUserGroup(rows.first);
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
   }
 
   /**
    * Returns all available groups in the system.
    */
-  Future<Iterable<Model.UserGroup>> groups() {
+  Future<Iterable<Model.UserGroup>> groups() async {
     String sql = '''
-    SELECT id, name
-    FROM groups
-  ''';
+SELECT
+  id,
+  name
+FROM
+  groups''';
 
-    return _connection.query(sql).then((List rows) {
-      List<Model.UserGroup> userGroups = new List<Model.UserGroup>();
-      for (var row in rows) {
-        userGroups.add(new Model.UserGroup.empty()
-          ..id = row.id
-          ..name = row.name);
-      }
-      return userGroups;
-    }).catchError((error, stackTrace) {
-      log.severe('sql:$sql', error, stackTrace);
-
-      return new Future.error(error, stackTrace);
-    });
+    try {
+      return (await _connection.query(sql)).map(_rowToUserGroup);
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
   }
 
   /**
    * Create a new group.
    */
-  Future createGroup(String name) {
+  Future createGroup(String name) async {
     String sql = '''
-    INSERT INTO groups (name)
-    VALUES (@name)
-    RETURNING id
-  ''';
+INSERT INTO
+  groups
+    (name)
+VALUES
+    (@name)
+RETURNING id''';
 
-    Map parameters = {'name': name};
+    final Map parameters = {'name': name};
 
-    return _connection
-        .query(sql, parameters)
-        .then((row) => new Model.UserGroup.empty()
-          ..id = row.id
-          ..name = name)
-        .catchError((error, stackTrace) {
-      log.severe('sql:$sql :: parameters:$parameters', error, stackTrace);
+    try {
+      final rows = await _connection.query(sql, parameters);
 
-      return new Future.error(error, stackTrace);
-    });
+      if (rows.isEmpty) {
+        throw new Storage.ServerError('Group not created');
+      }
+
+      return await group(rows.first.id);
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
   }
 
   /**
    * Adds user with id [userId] to group with id [groupId].
    */
-  Future joinGroup(int userId, int groupId) {
+  Future joinGroup(int userId, int groupId) async {
     String sql = '''
-    INSERT INTO user_groups (user_id, group_id)
-    VALUES (@userid, @groupid);
-  ''';
+INSERT INTO
+  user_groups
+    (user_id, group_id)
+VALUES
+    (@userid, @groupid)''';
 
-    Map parameters = {'userid': userId, 'groupid': groupId};
+    final Map parameters = {'userid': userId, 'groupid': groupId};
 
-    return _connection.execute(sql, parameters).catchError((error, stackTrace) {
-      log.severe('sql:$sql :: parameters:$parameters', error, stackTrace);
+    try {
+      final int rowsAffected = await _connection.execute(sql, parameters);
 
-      return new Future.error(error, stackTrace);
-    });
+      if (rowsAffected == 0) {
+        throw new Storage.ServerError(
+            'User with uid: $userId failed to join group $groupId');
+      }
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
   }
 
   /**
    * Removes user with id [userId] from group with id [groupId].
    */
-  Future leaveGroup(int userId, int groupId) {
+  Future leaveGroup(int userId, int groupId) async {
     String sql = '''
-    DELETE FROM user_groups
-    WHERE user_id = @userid AND group_id = @groupid;
-  ''';
+DELETE FROM
+  user_groups
+WHERE
+  user_id = @userid
+AND
+  group_id = @groupid''';
 
-    Map parameters = {'userid': userId, 'groupid': groupId};
+    final Map parameters = {'userid': userId, 'groupid': groupId};
 
-    return _connection.execute(sql, parameters).catchError((error, stackTrace) {
-      log.severe('sql:$sql :: parameters:$parameters', error, stackTrace);
+    try {
+      final int rowsAffected = await _connection.execute(sql, parameters);
 
-      return new Future.error(error, stackTrace);
-    });
+      if (rowsAffected == 0) {
+        throw new Storage.ServerError(
+            'User with uid: $userId failed to leave group $groupId');
+      }
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
   }
 
   /**
    * Returns the identities of user with id [userId].
    */
-  Future<Iterable<Model.UserIdentity>> identities(int userId) {
+  Future<Iterable<Model.UserIdentity>> identities(int userId) async {
     String sql = '''
-    SELECT identity, user_id
-    FROM auth_identities
-    WHERE user_id = @userid
-  ''';
+SELECT
+  identity,
+  user_id
+FROM
+  auth_identities
+WHERE
+  user_id = @userid''';
 
-    Map parameters = {'userid': userId};
+    final Map parameters = {'userid': userId};
 
-    return _connection.query(sql, parameters).then((List rows) {
-      List<Model.UserIdentity> userIdentities = new List<Model.UserIdentity>();
-      for (var row in rows) {
-        userIdentities.add(new Model.UserIdentity.empty()
-          ..identity = row.identity
-          ..userId = row.user_id);
-      }
-      return userIdentities;
-    }).catchError((error, stackTrace) {
-      log.severe('sql:$sql :: parameters:$parameters', error, stackTrace);
-
-      return new Future.error(error, stackTrace);
-    });
+    try {
+      return (await _connection.query(sql, parameters)).map(_rowToUserIdentity);
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
   }
 
   /**
    * Add a new [UserIndentity] to the database.
    */
-  Future<Model.UserIdentity> addIdentity(Model.UserIdentity identity) {
+  Future<Model.UserIdentity> addIdentity(Model.UserIdentity identity) async {
     String sql = '''
     INSERT INTO auth_identities (identity, user_id)
     VALUES (@identity, @userid)
     RETURNING identity;
   ''';
 
-    Map parameters = {'identity': identity.identity, 'userid': identity.userId};
+    final Map parameters = {
+      'identity': identity.identity,
+      'userid': identity.userId
+    };
 
-    return _connection
-        .query(sql, parameters)
-        .then((rows) => new Model.UserIdentity.empty()
-          ..identity = rows.first.identity
-          ..userId = identity.userId)
-        .catchError((error, stackTrace) {
-      log.severe('sql:$sql :: parameters:$parameters', error, stackTrace);
+    try {
+      final int rowsAffected = await _connection.execute(sql, parameters);
 
-      return new Future.error(error, stackTrace);
-    });
+      if (rowsAffected == 0) {
+        throw new Storage.ServerError(
+            'User with uid: ${identity.userId} failed to associate '
+            'with identity ${identity.identity}');
+      }
+
+      return identity;
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
   }
 
   /**
    * Remove a [UserIndentity] from the database.
    */
-  Future removeIdentity(Model.UserIdentity identity) {
+  Future removeIdentity(Model.UserIdentity identity) async {
     String sql = '''
       DELETE FROM auth_identities
       WHERE user_id=@userid AND identity=@identity;
     ''';
 
-    Map parameters = {'userid': identity.userId, 'identity': identity.identity};
+    final Map parameters = {
+      'userid': identity.userId,
+      'identity': identity.identity
+    };
 
-    return _connection.execute(sql, parameters).catchError((error, stackTrace) {
-      log.severe('sql:$sql :: parameters:$parameters', error, stackTrace);
+    try {
+      final int rowsAffected = await _connection.execute(sql, parameters);
 
-      return new Future.error(error, stackTrace);
-    });
+      if (rowsAffected == 0) {
+        throw new Storage.ServerError(
+            'User with uid: ${identity.userId} failed to un-associate '
+            'with identity ${identity.identity}');
+      }
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
   }
 
   /**
    * List every user in the database.
    */
-  Future<Iterable<Model.User>> list() {
+  Future<Iterable<Model.User>> list() async {
     final String sql = '''
 SELECT
-  outer_user.id,
-  outer_user.name,
-  outer_user.send_from,
-  outer_user.enabled,
-  outer_user.extension,
-  COALESCE((SELECT array_to_json(array_agg(row_to_json(tmp_groups)))
-   FROM 
-   (SELECT 
-     groups.id, groups.name
-    FROM
-     users 
-    JOIN
-     user_groups ON user_groups.user_id = users.id
-    JOIN groups ON user_groups.group_id = groups.id
-    WHERE user_groups.user_id = outer_user.id
-   ) tmp_groups
-  ),'[]') AS groups,
-
-  COALESCE((SELECT array_to_json(array_agg(row_to_json(tmp_iden)))
-   FROM 
-   (SELECT 
-     users.id as user_id, 
-     auth_identities.identity
-    FROM
-     users 
-    JOIN
-      auth_identities ON auth_identities.user_id = users.id
-    WHERE auth_identities.user_id = outer_user.id
-   ) tmp_iden
-  ),'[]') AS identities
+  id,
+  name,
+  send_from,
+  enabled,
+  google_username,
+  google_appcode,
+  extension
 FROM
-  users as outer_user
-WHERE enabled
-''';
+  users
+WHERE enabled''';
 
-    return _connection
-        .query(sql)
-        .then((Iterable rows) => rows.map(_rowToUser))
-        .catchError((error, stackTrace) {
-      log.severe('$sql', error, stackTrace);
-      return new Future.error(error, stackTrace);
-    });
+    try {
+      return (await _connection.query(sql)).map(_rowToUser);
+    } on Storage.SqlError catch (error) {
+      throw new Storage.ServerError(error.toString());
+    }
   }
 }
