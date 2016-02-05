1f529841c372af7f585aff41cfebd161d8a4b06e
Thomas Løcke <thomas@responsum.dk>
2016-02-05 15:39:14 +0100

    dartfmt

--- receptionist_client/web/view/view-contact-calendar.dart
+++ receptionist_client/web/view/view-contact-calendar.dart
@@ -68,11 +68,8 @@ class ContactCalendar extends ViewWidget {
    * Fetch all calendar entries for [contact].
    */
   void _fetchCalendar(ORModel.Contact contact) {
-    _calendarController
-        .contactCalendar(contact)
-        .then((Iterable<ORModel.CalendarEntry> entries) {
-      _ui.calendarEntries = entries.toList()
-        ..sort((a, b) => a.start.compareTo(b.start));
+    _calendarController.contactCalendar(contact).then((Iterable<ORModel.CalendarEntry> entries) {
+      _ui.calendarEntries = entries.toList()..sort((a, b) => a.start.compareTo(b.start));
     });
   }
 
@@ -100,8 +97,7 @@ class ContactCalendar extends ViewWidget {
 
     _notification.onCalendarChange.listen(_updateOnChange);
 
-    _contactSelector.onSelect
-        .listen((Model.ContactWithFilterContext c) => _render(c.contact));
+    _contactSelector.onSelect.listen((Model.ContactWithFilterContext c) => _render(c.contact));
 
     _receptionSelector.onSelect.listen(_clear);
   }
