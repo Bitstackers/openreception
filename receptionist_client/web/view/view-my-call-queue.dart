1edfc7c69338a9118cf785bad87a16c891bf8d93
Thomas Løcke <thomas@responsum.dk>
2016-02-13 11:21:40 +0100

    dartfmt

--- receptionist_client/web/view/view-my-call-queue.dart
+++ receptionist_client/web/view/view-my-call-queue.dart
@@ -89,9 +89,7 @@ class MyCallQueue extends ViewWidget {
     _busyCallController();
     try {
       ORModel.Call newCall = await _callController.dial(
-          phoneNumber,
-          _receptionSelector.selectedReception,
-          _contactSelector.selectedContact,
+          phoneNumber, _receptionSelector.selectedReception, _contactSelector.selectedContact,
           contextCallId: contextCallId);
       if (markTransfer || parkAndMarkTransfer) {
         _ui.markForTransfer(newCall);
