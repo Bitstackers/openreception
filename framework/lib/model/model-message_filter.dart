2b56be2ed4a57f6ea559131e8ff6b2f55c824178
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-03-07 17:33:30 +0100

    Removed (future) unsupported state filtering and cleaned out unsupported SQL-functionality.

--- framework/lib/model/model-message_filter.dart
+++ framework/lib/model/model-message_filter.dart
@@ -13,42 +13,11 @@
 
 part of openreception.model;
 
-class InvalidState implements Exception {
-  String _state;
-  InvalidState(this._state);
-}
-
-/**
- * Message state 'enum'.
- */
-abstract class MessageState {
-  static const Saved = 'saved';
-  static const Sent = 'sent';
-  static const Pending = 'pending';
-  static const NotSaved = 'notsaved';
-
-  static List<String> validStates = [Saved, Sent, Pending, NotSaved];
-
-  static String ofMessage(Message message) {
-    if (message.enqueued) {
-      return Pending;
-    } else if (message.sent) {
-      return Sent;
-    } else if (!message.sent && !message.enqueued) {
-      return Saved;
-    } else {
-      return '';
-    }
-  }
-}
-
 /**
  * Message filter model class. Meant for transmitting a filter 'function' from
  * a client to a server.
  */
 class MessageFilter {
-  String _messageState = '';
-
   int userId = User.noId;
   int receptionId = Reception.noId;
   int contactId = BaseContact.noId;
@@ -65,8 +34,6 @@ class MessageFilter {
   MessageFilter.fromMap(Map map) {
     userId = map.containsKey(Key.userId) ? map[Key.userId] : userId;
 
-    messageState = map.containsKey(Key.state) ? map[Key.state] : messageState;
-
     receptionId =
         map.containsKey(Key.receptionId) ? map[Key.receptionId] : receptionId;
 
@@ -76,44 +43,20 @@ class MessageFilter {
   }
 
   /**
-   * Current message state.
-   */
-  String get messageState => this._messageState;
-  void set messageState(String newState) {
-    if (newState == null) {
-      throw new ArgumentError.notNull(newState);
-    }
-
-    if (newState.isNotEmpty) {
-      if (!MessageState.validStates.contains(newState.toLowerCase())) {
-        throw new ArgumentError.value(newState, 'newState',
-            'State must one of: ${MessageState.validStates}');
-      }
-
-      _messageState = newState.toLowerCase();
-    } else {
-      _messageState = '';
-    }
-  }
-
-  /**
    * Check if this filter is active (any field is set).
    */
   bool get active =>
       userId != User.noId ||
       receptionId != Reception.noId ||
-      contactId != ReceptionAttributes.noId ||
-      messageState.isNotEmpty;
+      contactId != BaseContact.noId;
 
   /**
    * Check if the filter applies to [message].
    */
   bool appliesTo(Message message) =>
-      [message.context.contactId, ReceptionAttributes.noId]
-          .contains(contactId) &&
-      [message.context.receptionId, Reception.noId].contains(receptionId) &&
-      [message.senderId, User.noId].contains(userId) &&
-      [MessageState.ofMessage(message), Message.noId].contains(contactId);
+      [message.context.cid, BaseContact.noId].contains(contactId) &&
+      [message.context.rid, Reception.noId].contains(receptionId) &&
+      [message.sender.id, User.noId].contains(userId);
 
   /**
    * Filters [messages] using this filter.
@@ -128,7 +71,6 @@ class MessageFilter {
   @override
   bool operator ==(MessageFilter other) =>
       limitCount == other.limitCount &&
-      messageState == other.messageState &&
       userId == other.userId &&
       receptionId == other.receptionId &&
       contactId == other.contactId;
@@ -143,15 +85,11 @@ class MessageFilter {
       retval[Key.userId] = userId;
     }
 
-    if (messageState != Message.noId) {
-      retval[Key.state] = messageState;
-    }
-
     if (receptionId != Reception.noId) {
       retval[Key.receptionId] = receptionId;
     }
 
-    if (contactId != ReceptionAttributes.noId) {
+    if (contactId != BaseContact.noId) {
       retval[Key.contactId] = contactId;
     }
 
@@ -161,50 +99,7 @@ class MessageFilter {
   }
 
   /**
-   * Returns a list of active limit fields used in SQL filter function.
-   */
-  List<String> get _activeFields {
-    List<String> retval = [];
-
-    if (userId != User.noId) {
-      retval.add('taken_by_agent = ${this.userId}');
-    }
-
-    if (receptionId != Reception.noId) {
-      retval.add('context_reception_id = ${this.receptionId}');
-    }
-
-    if (contactId != ReceptionAttributes.noId) {
-      retval.add('context_contact_id = ${this.contactId}');
-    }
-
-    switch (this._messageState) {
-      case MessageState.Pending:
-        retval.add('enqueued');
-        break;
-
-      case MessageState.Sent:
-        retval.add('sent');
-        break;
-
-      case MessageState.Saved:
-        retval.add('(NOT enqueued AND NOT sent)');
-        break;
-
-      case MessageState.NotSaved:
-        retval.add('(enqueued OR sent)');
-        break;
-    }
-
-    return retval;
-  }
-
-  /**
-   * Return this filter as an SQL 'Where' clause.
    *
-   * FIXME: This does not perform any form of expression checking and is, thus,
-   * vulnerable to SQL injection.
    */
-  String get asSQL =>
-      (this.active ? 'WHERE ${this._activeFields.join(' AND ')}' : '');
+  String toString() => toJson().toString();
 }
