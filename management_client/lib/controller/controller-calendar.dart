ae28f0dccfcd08790dd70b359a68e4a886e0dcfb
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-03-13 11:23:38 +0100

    Use an internal user reference instead of upplying it on every modifying controller call.

--- management_client/lib/controller/controller-calendar.dart
+++ management_client/lib/controller/controller-calendar.dart
@@ -2,33 +2,28 @@ part of management_tool.controller;
 
 class Calendar {
   final service.RESTCalendarStore _calendarService;
+  final model.User _appUser;
 
-  Calendar(this._calendarService);
+  Calendar(this._calendarService, this._appUser);
 
   Future<Iterable<model.CalendarEntryChange>> changes(int entryId) =>
       _calendarService.changes(entryId);
 
-  Future get(model.CalendarEntry entry, model.User user) =>
-      _calendarService.remove(entry.id, user.id);
+  Future get(model.CalendarEntry entry) =>
+      _calendarService.remove(entry.id, _appUser);
 
-  Future<Iterable<model.CalendarEntry>> listContact(int contactId,
-          {bool deleted: false}) =>
-      _calendarService.list(new model.OwningContact(contactId),
-          deleted: deleted);
+  Future<Iterable<model.CalendarEntry>> listContact(int contactId) =>
+      _calendarService.list(new model.OwningContact(contactId));
 
-  Future<Iterable<model.CalendarEntry>> listReception(int receptionId,
-          {bool deleted: false}) =>
-      _calendarService.list(new model.OwningReception(receptionId),
-          deleted: deleted);
+  Future<Iterable<model.CalendarEntry>> listReception(int receptionId) =>
+      _calendarService.list(new model.OwningReception(receptionId));
 
-  Future<model.CalendarEntry> create(
-          model.CalendarEntry entry, model.User user) =>
-      _calendarService.create(entry, user);
+  Future<model.CalendarEntry> create(model.CalendarEntry entry) =>
+      _calendarService.create(entry, _appUser);
 
-  Future<model.CalendarEntry> update(
-          model.CalendarEntry entry, model.User user) =>
-      _calendarService.update(entry, user.id);
+  Future<model.CalendarEntry> update(model.CalendarEntry entry) =>
+      _calendarService.update(entry, _appUser);
 
-  Future remove(model.CalendarEntry entry, model.User user) =>
-      _calendarService.remove(entry.id, user.id);
+  Future remove(model.CalendarEntry entry) =>
+      _calendarService.remove(entry.id, _appUser);
 }
