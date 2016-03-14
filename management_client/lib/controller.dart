2bdb47622f3d19ace4ec2576e415de5a57b852c9
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-03-14 19:13:27 +0100

    Notification controller.

--- management_client/lib/controller.dart
+++ management_client/lib/controller.dart
@@ -3,6 +3,10 @@ library management_tool.controller;
 import 'dart:async';
 import 'dart:html' as html;
 
+import 'package:logging/logging.dart';
+
+import 'package:openreception_framework/bus.dart';
+import 'package:openreception_framework/event.dart' as event;
 import 'package:openreception_framework/service.dart' as service;
 import 'package:openreception_framework/model.dart' as model;
 
@@ -12,10 +16,13 @@ part 'controller/controller-contact.dart';
 part 'controller/controller-dialplan.dart';
 part 'controller/controller-ivr.dart';
 part 'controller/controller-message.dart';
+part 'controller/controller-notification.dart';
 part 'controller/controller-popup.dart';
 part 'controller/controller-organization.dart';
 part 'controller/controller-reception.dart';
 part 'controller/controller-user.dart';
+
+const String _libraryName = 'controller';
 
 final Popup popup = new Popup(
     new Uri.file('/image/popup_error.png'),
