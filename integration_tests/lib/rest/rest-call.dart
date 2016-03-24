6dd6350e6f114c3306dc872f2a044b29c4251b00
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-03-24 13:52:55 +0100

    Simplified call tests by letting the test-environment functionality handle the complexity.

--- integration_tests/lib/rest/rest-call.dart
+++ integration_tests/lib/rest/rest-call.dart
@@ -1,8 +1,8 @@
 part of openreception_tests.rest;
 
 void _runCallTests() {
-  _runCallHangupTests();
-  _callFlowControlList();
+  //_runCallHangupTests();
+  //_callFlowControlList();
   _callFlowControlPickup();
 }
 
@@ -12,14 +12,6 @@ void _runCallHangupTests() {
 
     ServiceAgent sa;
     TestEnvironment env;
-    process.CallFlowControl cProcess;
-    process.DialplanServer dProcess;
-    process.AuthServer aProcess;
-    process.NotificationServer nProcess;
-    process.FreeSwitch fsProcess;
-    process.UserServer uProcess;
-    service.Client client;
-    AuthToken authToken;
     Receptionist r;
     Customer c;
 
@@ -29,44 +21,6 @@ void _runCallHangupTests() {
     setUp(() async {
       env = new TestEnvironment();
       sa = await env.createsServiceAgent();
-      client = new service.Client();
-      authToken = new AuthToken(sa.user);
-      sa.authToken = authToken.tokenName;
-
-      nProcess = new process.NotificationServer(
-          Config.serverStackPath, env.runpath.path);
-
-      aProcess = new process.AuthServer(
-          Config.serverStackPath, env.runpath.path,
-          intialTokens: [authToken]);
-
-      fsProcess = await env.freeswitchProcess;
-
-      uProcess =
-          new process.UserServer(Config.serverStackPath, env.runpath.path);
-
-      dProcess = new process.DialplanServer(
-          Config.serverStackPath, env.runpath.path, fsProcess.confPath);
-
-      cProcess =
-          new process.CallFlowControl(Config.serverStackPath, env.runpath.path);
-
-      sa.callflow = new service.CallFlowControl(
-          Config.CallFlowControlUri, authToken.tokenName, client);
-
-      await new Future.delayed(new Duration(seconds: 3));
-      await Future.wait([
-        aProcess.whenReady,
-        nProcess.whenReady,
-        uProcess.whenReady,
-        cProcess.whenReady,
-        dProcess.whenReady
-      ]);
-
-      sa.paService = new service.PeerAccount(
-          Config.dialplanStoreUri, sa.authToken, client);
-      sa.dpService = new service.RESTDialplanStore(
-          Config.dialplanStoreUri, sa.authToken, client);
 
       final org = await sa.createsOrganization();
       final rec = await sa.createsReception(org);
@@ -75,53 +29,33 @@ void _runCallHangupTests() {
 
       r = await sa.createsReceptionist();
       c = await sa.spawnCustomer();
-      log.finest('Restarting authserver');
-      await aProcess.terminate();
-      aProcess = new process.AuthServer(
-          Config.serverStackPath, env.runpath.path,
-          intialTokens: [authToken, new AuthToken(r.user)]);
-      await aProcess.whenReady;
-
-      final registerEvent = sa.notificationSocket.eventStream.firstWhere((e) =>
-          e is event.PeerState &&
-          e.peer.name == r.user.peer &&
-          e.peer.registered);
-
-      await r.initialize();
-
-      await registerEvent;
-      //log.finest((await callflow.peerList()).map((p) => p.toJson()).join('\n'));
     });
 
     tearDown(() async {
-      await Future.wait([
-        dProcess.terminate(),
-        cProcess.terminate(),
-        uProcess.terminate(),
-        aProcess.terminate(),
-        nProcess.terminate(),
-        fsProcess.cleanConfig()
-      ]);
-      log.finest('FSLOG:\n ${fsProcess.latestLog.readAsStringSync()}');
+      //log.finest('FSLOG:\n ${fsProcess.latestLog.readAsStringSync()}');
       await env.clear();
-      client.client.close();
     });
 
     test(
         'CORS headers present (existingUri)',
-        () => isCORSHeadersPresent(
-            resource.CallFlowControl.list(Config.CallFlowControlUri), log));
+        () async => isCORSHeadersPresent(
+            resource.CallFlowControl
+                .list((await env.requestCallFlowProcess()).uri),
+            log));
 
     test(
         'CORS headers present (non-existingUri)',
-        () => isCORSHeadersPresent(
-            Uri.parse('${Config.CallFlowControlUri}/nonexistingpath'), log));
+        () async => isCORSHeadersPresent(
+            Uri.parse(
+                '${(await env.requestCallFlowProcess()).uri}/nonexistingpath'),
+            log));
 
     test(
         'Non-existing path',
-        () => nonExistingPath(
-            Uri.parse('${Config.CallFlowControlUri}/nonexistingpath'
-                '?token=${authToken.tokenName}'),
+        () async => nonExistingPath(
+            Uri.parse(
+                '${(await env.requestCallFlowProcess()).uri}/nonexistingpath'
+                '?token=${sa.authToken.tokenName}'),
             log));
 
     test('interfaceCallNotFound',
@@ -145,62 +79,16 @@ void _callFlowControlList() {
 
     ServiceAgent sa;
     TestEnvironment env;
-    process.CallFlowControl cProcess;
-    process.DialplanServer dProcess;
-    process.AuthServer aProcess;
-    process.NotificationServer nProcess;
-    process.FreeSwitch fsProcess;
-    process.UserServer uProcess;
-    service.Client client;
-    AuthToken authToken;
     Receptionist r;
     Customer c;
-    model.Reception rec;
 
     /// Transient object
     model.ReceptionDialplan rdp;
+    model.Reception rec;
 
     setUp(() async {
       env = new TestEnvironment();
       sa = await env.createsServiceAgent();
-      client = new service.Client();
-      authToken = new AuthToken(sa.user);
-      sa.authToken = authToken.tokenName;
-
-      nProcess = new process.NotificationServer(
-          Config.serverStackPath, env.runpath.path);
-
-      aProcess = new process.AuthServer(
-          Config.serverStackPath, env.runpath.path,
-          intialTokens: [authToken]);
-
-      fsProcess = await env.freeswitchProcess;
-
-      uProcess =
-          new process.UserServer(Config.serverStackPath, env.runpath.path);
-
-      dProcess = new process.DialplanServer(
-          Config.serverStackPath, env.runpath.path, fsProcess.confPath);
-
-      cProcess =
-          new process.CallFlowControl(Config.serverStackPath, env.runpath.path);
-
-      sa.callflow = new service.CallFlowControl(
-          Config.CallFlowControlUri, authToken.tokenName, client);
-
-      await new Future.delayed(new Duration(seconds: 3));
-      await Future.wait([
-        aProcess.whenReady,
-        nProcess.whenReady,
-        uProcess.whenReady,
-        cProcess.whenReady,
-        dProcess.whenReady
-      ]);
-
-      sa.paService = new service.PeerAccount(
-          Config.dialplanStoreUri, sa.authToken, client);
-      sa.dpService = new service.RESTDialplanStore(
-          Config.dialplanStoreUri, sa.authToken, client);
 
       final org = await sa.createsOrganization();
       rec = await sa.createsReception(org);
@@ -209,36 +97,11 @@ void _callFlowControlList() {
 
       r = await sa.createsReceptionist();
       c = await sa.spawnCustomer();
-      log.finest('Restarting authserver');
-      await aProcess.terminate();
-      aProcess = new process.AuthServer(
-          Config.serverStackPath, env.runpath.path,
-          intialTokens: [authToken, new AuthToken(r.user)]);
-      await aProcess.whenReady;
-
-      final registerEvent = sa.notificationSocket.eventStream.firstWhere((e) =>
-          e is event.PeerState &&
-          e.peer.name == r.user.peer &&
-          e.peer.registered);
-
-      await r.initialize();
-
-      await registerEvent;
-      //log.finest((await callflow.peerList()).map((p) => p.toJson()).join('\n'));
     });
 
     tearDown(() async {
-      await Future.wait([
-        dProcess.terminate(),
-        cProcess.terminate(),
-        uProcess.terminate(),
-        aProcess.terminate(),
-        nProcess.terminate(),
-        fsProcess.cleanConfig()
-      ]);
-      log.finest('FSLOG:\n ${fsProcess.latestLog.readAsStringSync()}');
+      //log.finest('FSLOG:\n ${fsProcess.latestLog.readAsStringSync()}');
       await env.clear();
-      client.client.close();
     });
 
     test('callDataOK', () => serviceTest.CallList.callDataOK(rec, rdp, r, c));
@@ -259,67 +122,19 @@ void _callFlowControlList() {
  */
 void _callFlowControlPickup() {
   group('$_namespace.Call', () {
-    Logger log = new Logger('$_namespace.Call');
-
     ServiceAgent sa;
     TestEnvironment env;
-    process.CallFlowControl cProcess;
-    process.DialplanServer dProcess;
-    process.AuthServer aProcess;
-    process.NotificationServer nProcess;
-    process.FreeSwitch fsProcess;
-    process.UserServer uProcess;
-    service.Client client;
-    AuthToken authToken;
     Receptionist r;
     Receptionist r2;
     Customer c;
-    model.Reception rec;
 
     /// Transient object
     model.ReceptionDialplan rdp;
+    model.Reception rec;
 
     setUp(() async {
       env = new TestEnvironment();
       sa = await env.createsServiceAgent();
-      client = new service.Client();
-      authToken = new AuthToken(sa.user);
-      sa.authToken = authToken.tokenName;
-
-      nProcess = new process.NotificationServer(
-          Config.serverStackPath, env.runpath.path);
-
-      aProcess = new process.AuthServer(
-          Config.serverStackPath, env.runpath.path,
-          intialTokens: [authToken]);
-
-      fsProcess = await env.freeswitchProcess;
-
-      uProcess =
-          new process.UserServer(Config.serverStackPath, env.runpath.path);
-
-      dProcess = new process.DialplanServer(
-          Config.serverStackPath, env.runpath.path, fsProcess.confPath);
-
-      cProcess =
-          new process.CallFlowControl(Config.serverStackPath, env.runpath.path);
-
-      sa.callflow = new service.CallFlowControl(
-          Config.CallFlowControlUri, authToken.tokenName, client);
-
-      await new Future.delayed(new Duration(seconds: 3));
-      await Future.wait([
-        aProcess.whenReady,
-        nProcess.whenReady,
-        uProcess.whenReady,
-        cProcess.whenReady,
-        dProcess.whenReady
-      ]);
-
-      sa.paService = new service.PeerAccount(
-          Config.dialplanStoreUri, sa.authToken, client);
-      sa.dpService = new service.RESTDialplanStore(
-          Config.dialplanStoreUri, sa.authToken, client);
 
       final org = await sa.createsOrganization();
       rec = await sa.createsReception(org);
@@ -327,37 +142,13 @@ void _callFlowControlPickup() {
       await sa.deploysDialplan(rdp, rec);
 
       r = await sa.createsReceptionist();
+      r2 = await sa.createsReceptionist();
       c = await sa.spawnCustomer();
-      log.finest('Restarting authserver');
-      await aProcess.terminate();
-      aProcess = new process.AuthServer(
-          Config.serverStackPath, env.runpath.path,
-          intialTokens: [authToken, new AuthToken(r.user)]);
-      await aProcess.whenReady;
-
-      final registerEvent = sa.notificationSocket.eventStream.firstWhere((e) =>
-          e is event.PeerState &&
-          e.peer.name == r.user.peer &&
-          e.peer.registered);
-
-      await r.initialize();
-
-      await registerEvent;
-      //log.finest((await callflow.peerList()).map((p) => p.toJson()).join('\n'));
     });
 
     tearDown(() async {
-      await Future.wait([
-        dProcess.terminate(),
-        cProcess.terminate(),
-        uProcess.terminate(),
-        aProcess.terminate(),
-        nProcess.terminate(),
-        fsProcess.cleanConfig()
-      ]);
-      log.finest('FSLOG:\n ${fsProcess.latestLog.readAsStringSync()}');
+      //log.finest('FSLOG:\n ${fsProcess.latestLog.readAsStringSync()}');
       await env.clear();
-      client.client.close();
     });
 
     /* Perform test. */
@@ -387,91 +178,6 @@ void _callFlowControlPickup() {
               ..dialplan = rdp.extension,
             r,
             c));
-
-    setUp(() async {
-      env = new TestEnvironment();
-      sa = await env.createsServiceAgent();
-      client = new service.Client();
-      authToken = new AuthToken(sa.user);
-      sa.authToken = authToken.tokenName;
-
-      nProcess = new process.NotificationServer(
-          Config.serverStackPath, env.runpath.path);
-
-      aProcess = new process.AuthServer(
-          Config.serverStackPath, env.runpath.path,
-          intialTokens: [authToken]);
-
-      fsProcess = await env.freeswitchProcess;
-
-      uProcess =
-          new process.UserServer(Config.serverStackPath, env.runpath.path);
-
-      dProcess = new process.DialplanServer(
-          Config.serverStackPath, env.runpath.path, fsProcess.confPath);
-
-      cProcess =
-          new process.CallFlowControl(Config.serverStackPath, env.runpath.path);
-
-      sa.callflow = new service.CallFlowControl(
-          Config.CallFlowControlUri, authToken.tokenName, client);
-
-      await new Future.delayed(new Duration(seconds: 3));
-      await Future.wait([
-        aProcess.whenReady,
-        nProcess.whenReady,
-        uProcess.whenReady,
-        cProcess.whenReady,
-        dProcess.whenReady
-      ]);
-
-      sa.paService = new service.PeerAccount(
-          Config.dialplanStoreUri, sa.authToken, client);
-      sa.dpService = new service.RESTDialplanStore(
-          Config.dialplanStoreUri, sa.authToken, client);
-
-      final org = await sa.createsOrganization();
-      rec = await sa.createsReception(org);
-      rdp = await sa.createsDialplan();
-      await sa.deploysDialplan(rdp, rec);
-
-      r = await sa.createsReceptionist();
-      r2 = await sa.createsReceptionist();
-      c = await sa.spawnCustomer();
-      log.finest('Restarting authserver');
-      await aProcess.terminate();
-      aProcess = new process.AuthServer(
-          Config.serverStackPath, env.runpath.path, intialTokens: [
-        authToken,
-        new AuthToken(r.user),
-        new AuthToken(r2.user)
-      ]);
-      await aProcess.whenReady;
-
-      final registerEvent = sa.notificationSocket.eventStream.firstWhere((e) =>
-          e is event.PeerState &&
-          e.peer.name == r.user.peer &&
-          e.peer.registered);
-
-      await r.initialize();
-
-      await registerEvent;
-      //log.finest((await callflow.peerList()).map((p) => p.toJson()).join('\n'));
-    });
-
-    tearDown(() async {
-      await Future.wait([
-        dProcess.terminate(),
-        cProcess.terminate(),
-        uProcess.terminate(),
-        aProcess.terminate(),
-        nProcess.terminate(),
-        fsProcess.cleanConfig()
-      ]);
-      log.finest('FSLOG:\n ${fsProcess.latestLog.readAsStringSync()}');
-      await env.clear();
-      client.client.close();
-    });
 
     test('pickupAllocatedCall',
         () => serviceTest.Pickup.pickupAllocatedCall(rdp, r, r2, c));
