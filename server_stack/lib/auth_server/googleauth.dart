e23c99a2d72971878f0a9b182e3722dda2dd1f73
Thomas Løcke <thomas@monadic.dk>
2016-04-06 13:52:28 +0200

    Fixed #60

--- server_stack/lib/auth_server/googleauth.dart
+++ server_stack/lib/auth_server/googleauth.dart
@@ -16,21 +16,22 @@ library openreception.authentication_server.google_auth;
 import 'package:oauth2/oauth2.dart' as oauth2;
 import 'package:crypto/crypto.dart';
 
-final Uri authorizationEndpoint = Uri.parse("https://accounts.google.com/o/oauth2/auth");
-final Uri tokenEndpoint = Uri.parse("https://accounts.google.com/o/oauth2/token");
+final Uri authorizationEndpoint =
+    Uri.parse("https://accounts.google.com/o/oauth2/auth");
+final Uri tokenEndpoint =
+    Uri.parse("https://accounts.google.com/o/oauth2/token");
 final List<String> _scopes = ['profile', 'email'];
 
 Uri _AuthorizationUrl;
 
 Uri googleAuthUrl(String identifier, String secret, Uri redirectUrl) {
   if (_AuthorizationUrl == null) {
-    oauth2.AuthorizationCodeGrant grant =
-        new oauth2.AuthorizationCodeGrant(identifier, secret, authorizationEndpoint, tokenEndpoint);
+    oauth2.AuthorizationCodeGrant grant = new oauth2.AuthorizationCodeGrant(
+        identifier, secret, authorizationEndpoint, tokenEndpoint);
     _AuthorizationUrl = grant.getAuthorizationUrl(redirectUrl, scopes: _scopes);
   }
 
   return _AuthorizationUrl;
 }
 
-String Sha256Token(String token) =>
-    CryptoUtils.bytesToHex((new SHA256()..add(token.codeUnits)).close());
+String Sha256Token(String token) => sha256.convert(token.codeUnits).toString();
