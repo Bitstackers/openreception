7dc8242e75b502adc8ba21f1c87a01119f1012be
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-04-10 19:12:02 +0200

    Ivr menu object change listing test.

--- integration_tests/lib/storage/storage-ivr.dart
+++ integration_tests/lib/storage/storage-ivr.dart
@@ -66,7 +66,102 @@ abstract class Ivr {
     }
 
     await ivrStore.update(createdMenu, user);
+  }
 
-    //await ivrStore.remove(createdMenu.name, user);
+  /**
+     *
+     */
+  static Future changeOnCreate(ServiceAgent sa) async {
+    final model.IvrMenu created = await sa.createsIvrMenu();
+
+    Iterable<model.Commit> commits = await sa.ivrStore.changes(created.name);
+
+    expect(commits.length, equals(1));
+    expect(commits.first.changedAt.millisecondsSinceEpoch,
+        lessThan(new DateTime.now().millisecondsSinceEpoch));
+    expect(commits.first.authorIdentity, equals(sa.user.address));
+    expect(commits.first.uid, equals(sa.user.id));
+
+    expect(commits.first.changes.length, equals(1));
+    final change = commits.first.changes.first;
+
+    expect(change.changeType, model.ChangeType.add);
+    expect(change.menuName, created.name);
+  }
+
+  /**
+     *
+     */
+  static Future changeOnUpdate(ServiceAgent sa) async {
+    final model.BaseContact created = await sa.createsContact();
+
+    await sa.updatesContact(created);
+
+    Iterable<model.Commit> commits = await sa.contactStore.changes(created.id);
+
+    expect(commits.length, equals(2));
+
+    expect(commits.first.changedAt.millisecondsSinceEpoch,
+        lessThan(new DateTime.now().millisecondsSinceEpoch));
+    expect(commits.first.authorIdentity, equals(sa.user.address));
+
+    expect(commits.last.changedAt.millisecondsSinceEpoch,
+        lessThan(new DateTime.now().millisecondsSinceEpoch));
+    expect(commits.last.authorIdentity, equals(sa.user.address));
+
+    expect(commits.length, equals(2));
+    expect(commits.first.changedAt.millisecondsSinceEpoch,
+        lessThan(new DateTime.now().millisecondsSinceEpoch));
+    expect(commits.first.authorIdentity, equals(sa.user.address));
+    expect(commits.first.uid, equals(sa.user.id));
+
+    expect(commits.first.changes.length, equals(1));
+    expect(commits.last.changes.length, equals(1));
+    final latestChange = commits.first.changes.first;
+    final oldestChange = commits.last.changes.first;
+
+    expect(latestChange.changeType, model.ChangeType.modify);
+    expect(latestChange.cid, created.id);
+
+    expect(oldestChange.changeType, model.ChangeType.add);
+    expect(oldestChange.cid, created.id);
+  }
+
+  /**
+     *
+     */
+  static Future changeOnRemove(ServiceAgent sa) async {
+    final model.BaseContact created = await sa.createsContact();
+
+    await sa.removesContact(created);
+
+    Iterable<model.Commit> commits = await sa.contactStore.changes(created.id);
+
+    expect(commits.length, equals(2));
+
+    expect(commits.first.changedAt.millisecondsSinceEpoch,
+        lessThan(new DateTime.now().millisecondsSinceEpoch));
+    expect(commits.first.authorIdentity, equals(sa.user.address));
+
+    expect(commits.last.changedAt.millisecondsSinceEpoch,
+        lessThan(new DateTime.now().millisecondsSinceEpoch));
+    expect(commits.last.authorIdentity, equals(sa.user.address));
+
+    expect(commits.length, equals(2));
+    expect(commits.first.changedAt.millisecondsSinceEpoch,
+        lessThan(new DateTime.now().millisecondsSinceEpoch));
+    expect(commits.first.authorIdentity, equals(sa.user.address));
+    expect(commits.first.uid, equals(sa.user.id));
+
+    expect(commits.first.changes.length, equals(1));
+    expect(commits.last.changes.length, equals(1));
+    final latestChange = commits.first.changes.first;
+    final oldestChange = commits.last.changes.first;
+
+    expect(latestChange.changeType, model.ChangeType.delete);
+    expect(latestChange.cid, created.id);
+
+    expect(oldestChange.changeType, model.ChangeType.add);
+    expect(oldestChange.cid, created.id);
   }
 }
