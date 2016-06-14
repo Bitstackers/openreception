fd7ab258a2a695325f3cef9aef273bedeb0c4a93
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-06-14 15:20:22 +0200

    Dialplan and Ivr changes added to event subsystem.

---
+++ framework/lib/event/event-dialplan.dart
@@ -0,0 +1,80 @@
+/*                  This file is part of OpenReception
+                   Copyright (C) 2014-, BitStackers K/S
+
+  This is free software;  you can redistribute it and/or modify it
+  under terms of the  GNU General Public License  as published by the
+  Free Software  Foundation;  either version 3,  or (at your  option) any
+  later version. This software is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  You should have received a copy of the GNU General Public License along with
+  this program; see the file COPYING3. If not, see http://www.gnu.org/licenses.
+*/
+
+part of openreception.framework.event;
+
+/**
+ *
+ */
+class DialplanChange implements Event {
+  final DateTime timestamp;
+
+  String get eventName => Key._dialplanChange;
+
+  bool get isCreate => state == Change.created;
+  bool get isUpdate => state == Change.updated;
+  bool get isDelete => state == Change.deleted;
+
+  final String extension;
+  final int modifierUid;
+  final String state;
+
+  /**
+   *
+   */
+  DialplanChange._internal(this.extension, this.modifierUid, this.state)
+      : timestamp = new DateTime.now();
+
+  /*
+   *
+   */
+  factory DialplanChange.create(String extension, int modifierUid) =>
+      new DialplanChange._internal(extension, modifierUid, Change.created);
+
+  /**
+   *
+   */
+  factory DialplanChange.update(String extension, int modifierUid) =>
+      new DialplanChange._internal(extension, modifierUid, Change.updated);
+
+  /**
+   *
+   */
+  factory DialplanChange.delete(String extension, int modifierUid) =>
+      new DialplanChange._internal(extension, modifierUid, Change.deleted);
+
+  /**
+   *
+   */
+  String toString() => toJson().toString();
+
+  /**
+   *
+   */
+  Map toJson() => {
+        Key.event: eventName,
+        Key.timestamp: util.dateTimeToUnixTimestamp(timestamp),
+        Key.modifierUid: modifierUid,
+        Key._extension: extension,
+        Key.state: this.state
+      };
+
+  /**
+   *
+   */
+  DialplanChange.fromMap(Map map)
+      : modifierUid = map[Key.modifierUid],
+        extension = map[Key._extension],
+        state = map[Key.state],
+        timestamp = util.unixTimestampToDateTime(map[Key.timestamp]);
+}
