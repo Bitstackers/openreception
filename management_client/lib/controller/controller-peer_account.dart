07b6a983b640326f2d1e8df6431857337f654f65
Kim Rostgaard Christensen <krc@retrospekt.dk>
2016-06-14 10:00:01 +0200

    Peer account controller.

---
+++ management_client/lib/controller/controller-peer_account.dart
@@ -0,0 +1,19 @@
+part of management_tool.controller;
+
+class PeerAccount {
+  final service.PeerAccount _paService;
+
+  PeerAccount(this._paService);
+
+  Future<Iterable<model.PeerAccount>> list() =>
+      _paService.list().catchError(_handleError);
+
+  Future<model.PeerAccount> get(String accountName) =>
+      _paService.get(accountName).catchError(_handleError);
+
+  Future<Iterable<String>> deploy(model.PeerAccount account, int uid) =>
+      _paService.deployAccount(account, uid).catchError(_handleError);
+
+  Future remove(String accountName) =>
+      _paService.remove(accountName).catchError(_handleError);
+}
