d2685c31d4aff75d578de03d6e80bb8420b6b4e7
Thomas Løcke <thomas@monadic.dk>
2016-11-14 16:40:05 +0100

    Fixed bug where tags did not properly update in ContactData on filtering

--- receptionist_client/lib/model/ui/model-ui-contact-selector.dart
+++ receptionist_client/lib/model/ui/model-ui-contact-selector.dart
@@ -229,10 +229,15 @@ class UIContactSelector extends UIModel {
     }
 
     final List<Element> visible = _list.querySelectorAll('li:not(.hide)');
-    if (_list.children.isNotEmpty &&
-        !visible.any((Element li) => li.classes.contains('selected'))) {
-      _markSelected(_scanForwardForVisibleElement(_list.children.first),
-          alwaysFire: true);
+
+    if (_list.children.isNotEmpty) {
+      if (!visible.any((Element li) => li.classes.contains('selected'))) {
+        _markSelected(_scanForwardForVisibleElement(_list.children.first),
+            alwaysFire: true);
+      } else {
+        final LIElement selected = _list.querySelector('.selected');
+        _contactSelectCallback(selected);
+      }
     }
 
     final LIElement selected = _list.querySelector('.selected');
